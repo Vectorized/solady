05769504316bc377ddbb63fdbf0d26b13a6d7993
Vectorized <webby1111@hotmail.com>
2022-07-25 12:56:17 +0000

    Multicallable optimizations

--- src/utils/Multicallable.sol
+++ src/utils/Multicallable.sol
@@ -7,45 +7,51 @@ pragma solidity ^0.8.4;
 abstract contract Multicallable {
     function multicall(bytes[] calldata data) public payable returns (bytes[] memory results) {
         assembly {
-            results := mload(0x40)
-            mstore(results, data.length)
+            if data.length {
+                results := mload(0x40) // Point `results` to start of free memory.
+                mstore(results, data.length) // Store `data.length` into `results`.
+                results := add(results, 0x20)
 
-            // The slot of the end of the offsets in `data`.
-            // `shl` 5 is equivalent to multiplying by 0x20.
-            let end := add(data.offset, shl(5, data.length))
-            // Pointer to the array of pointers to each of the the returned data in `results`.
-            let resultsPtrs := add(results, 0x20)
-            // Pointer to the top of the memory (i.e. start of the free memory).
-            // `shl` 5 is equivalent to multiplying by 0x20.
-            let memPtr := add(resultsPtrs, shl(5, data.length))
+                // `shl` 5 is equivalent to multiplying by 0x20.
+                let end := shl(5, data.length)
+                // Copy the offsets from calldata into memory.
+                calldatacopy(results, data.offset, end)
+                // Pointer to the top of the memory (i.e. start of the free memory).
+                // `shl` 5 is equivalent to multiplying by 0x20.
+                let memPtr := add(results, end)
+                end := add(results, end)
 
-            // prettier-ignore
-            for { let i := data.offset } iszero(eq(i, end)) { i := add(i, 0x20) } {
-                // The offset of the current bytes in the calldata.
-                let o := add(data.offset, calldataload(i))
-                // Copy the current bytes from calldata to the memory.
-                calldatacopy(
-                    memPtr,
-                    add(o, 0x20), // The offset of the current bytes' bytes.
-                    calldataload(o) // The length of the current bytes.
-                )
-                if iszero(delegatecall(gas(), address(), memPtr, calldataload(o), 0x00, 0x00)) {
-                    // Bubble up the revert if the delegatecall reverts.
-                    returndatacopy(0x00, 0x00, returndatasize())
-                    revert(0x00, returndatasize())
+                // prettier-ignore
+                for {} 1 {} {
+                    // The offset of the current bytes in the calldata.
+                    let o := add(data.offset, mload(results))
+                    // Copy the current bytes from calldata to the memory.
+                    calldatacopy(
+                        memPtr,
+                        add(o, 0x20), // The offset of the current bytes' bytes.
+                        calldataload(o) // The length of the current bytes.
+                    )
+                    if iszero(delegatecall(gas(), address(), memPtr, calldataload(o), 0x00, 0x00)) {
+                        // Bubble up the revert if the delegatecall reverts.
+                        returndatacopy(0x00, 0x00, returndatasize())
+                        revert(0x00, returndatasize())
+                    }
+                    // Append the current `memPtr` into `results`.
+                    mstore(results, memPtr)
+                    results := add(results, 0x20)
+                    // Append the `returndatasize()`, and the return data.
+                    mstore(memPtr, returndatasize())
+                    returndatacopy(add(memPtr, 0x20), 0x00, returndatasize())
+                    // Advance the `memPtr` by `returndatasize() + 0x20`,
+                    // rounded up to the next multiple of 32.
+                    memPtr := and(add(add(memPtr, returndatasize()), 0x3f), 0xffffffffffffffe0)
+                    // prettier-ignore
+                    if iszero(lt(results, end)) { break }
                 }
-                // Append the current `memPtr` into `resultsPtrs`.
-                mstore(resultsPtrs, memPtr)
-                resultsPtrs := add(resultsPtrs, 0x20)
-
-                // Append the `returndatasize()`, and the return data.
-                mstore(memPtr, returndatasize())
-                returndatacopy(add(memPtr, 0x20), 0x00, returndatasize())
-                // Advance the `memPtr` by `returndatasize() + 0x20`,
-                // rounded up to the next multiple of 32.
-                memPtr := and(add(add(memPtr, returndatasize()), 0x3f), 0xffffffffffffffe0)
+                // Restore and allocate memory for `results`.
+                results := mload(0x40)
+                mstore(0x40, memPtr)
             }
-            mstore(0x40, memPtr)
         }
     }
 }
