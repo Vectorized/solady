5bb8062ba7790aff4d65ac027096d9080d14752f
Vectorized <webby1111@hotmail.com>
2022-08-04 00:48:33 +0000

    LibBitmap improvements

--- test/LibBitmap.t.sol
+++ test/LibBitmap.t.sol
@@ -2,13 +2,52 @@
 pragma solidity ^0.8.4;
 
 import "forge-std/Test.sol";
-import {MockLibBitmap} from "./utils/mocks/MockLibBitmap.sol";
+import {LibBitmap} from "../src/utils/LibBitmap.sol";
 
 contract LibBitmapTest is Test {
-    MockLibBitmap mockLibBitmap;
+    using LibBitmap for LibBitmap.Bitmap;
+
+    error AlreadyClaimed();
+
+    mapping(uint256 => LibBitmap.Bitmap) bitmaps;
+
+    uint256 currentBitmapIndex;
 
     function setUp() public {
-        mockLibBitmap = new MockLibBitmap();
+        ++currentBitmapIndex;
+    }
+
+    function get(uint256 index) public view returns (bool result) {
+        result = bitmaps[currentBitmapIndex].get(index);
+    }
+
+    function set(uint256 index) public {
+        bitmaps[currentBitmapIndex].set(index);
+    }
+
+    function unset(uint256 index) public {
+        bitmaps[currentBitmapIndex].unset(index);
+    }
+
+    function toggle(uint256 index) public {
+        bitmaps[currentBitmapIndex].toggle(index);
+    }
+
+    function setTo(uint256 index, bool shouldSet) public {
+        bitmaps[currentBitmapIndex].setTo(index, shouldSet);
+    }
+
+    function claimWithGetSet(uint256 index) public {
+        if (bitmaps[currentBitmapIndex].get(index)) {
+            revert AlreadyClaimed();
+        }
+        bitmaps[currentBitmapIndex].set(index);
+    }
+
+    function claimWithToggle(uint256 index) public {
+        if (bitmaps[currentBitmapIndex].toggle(index) == false) {
+            revert AlreadyClaimed();
+        }
     }
 
     function testBitmapGet() public {
@@ -16,7 +55,7 @@ contract LibBitmapTest is Test {
     }
 
     function testBitmapGet(uint256 index) public {
-        assertFalse(mockLibBitmap.get(index));
+        assertFalse(get(index));
     }
 
     function testBitmapSet() public {
@@ -24,8 +63,8 @@ contract LibBitmapTest is Test {
     }
 
     function testBitmapSet(uint256 index) public {
-        mockLibBitmap.set(index);
-        assertTrue(mockLibBitmap.get(index));
+        set(index);
+        assertTrue(get(index));
     }
 
     function testBitmapUnset() public {
@@ -33,20 +72,24 @@ contract LibBitmapTest is Test {
     }
 
     function testBitmapUnset(uint256 index) public {
-        mockLibBitmap.set(index);
-        assertTrue(mockLibBitmap.get(index));
-        mockLibBitmap.unset(index);
-        assertFalse(mockLibBitmap.get(index));
+        set(index);
+        assertTrue(get(index));
+        unset(index);
+        assertFalse(get(index));
     }
 
     function testBitmapSetTo() public {
-        testBitmapSetTo(123, true);
-        testBitmapSetTo(123, false);
+        testBitmapSetTo(123, true, 0);
+        testBitmapSetTo(123, false, 0);
     }
 
-    function testBitmapSetTo(uint256 index, bool shouldSet) public {
-        mockLibBitmap.setTo(index, shouldSet);
-        assertEq(mockLibBitmap.get(index), shouldSet);
+    function testBitmapSetTo(uint256 index, bool shouldSet, uint256 brutalizer) public {
+        bool shouldSetBrutalized;
+        assembly {
+            shouldSetBrutalized := shl(and(brutalizer, 0xff), shouldSet)
+        }
+        setTo(index, shouldSetBrutalized);
+        assertEq(get(index), shouldSet);
     }
 
     function testBitmapToggle() public {
@@ -55,9 +98,23 @@ contract LibBitmapTest is Test {
     }
 
     function testBitmapToggle(uint256 index, bool initialValue) public {
-        mockLibBitmap.setTo(index, initialValue);
-        assertEq(mockLibBitmap.get(index), initialValue);
-        mockLibBitmap.toggle(index);
-        assertEq(mockLibBitmap.get(index), !initialValue);
+        setTo(index, initialValue);
+        assertEq(get(index), initialValue);
+        toggle(index);
+        assertEq(get(index), !initialValue);
+    }
+
+    function testBitmapClaimWithGetSet() public {
+        uint256 index = 123;
+        this.claimWithGetSet(index);
+        vm.expectRevert(AlreadyClaimed.selector);
+        this.claimWithGetSet(index);
+    }
+
+    function testBitmapClaimWithToggle() public {
+        uint256 index = 123;
+        this.claimWithToggle(index);
+        vm.expectRevert(AlreadyClaimed.selector);
+        this.claimWithToggle(index);
     }
 }
