735578e8a3bb257b7c2d6aaf331c5f7c4d2aff4a
0xphaze <0xphaze@gmail.com>
2022-08-05 17:41:40 +0200

    returndata-encoding

--- src/utils/Multicallable.sol
+++ src/utils/Multicallable.sol
@@ -7,25 +7,30 @@ pragma solidity ^0.8.4;
 /// @dev Note that combining Multicallable with msg.value can cause double-spend issues
 /// (https://www.paradigm.xyz/2021/08/two-rights-might-make-a-wrong/)
 abstract contract Multicallable {
-    function multicall(bytes[] calldata data) public payable returns (bytes[] memory results) {
+    function multicall(bytes[] calldata data) public payable returns (bytes[] memory) {
         assembly {
             if data.length {
-                results := mload(0x40) // Point `results` to start of free memory.
-                mstore(results, data.length) // Store `data.length` into `results`.
-                let resultPtr := add(results, 0x20) // Pointer to current result.
+                let returndata := mload(0x40) // Point `results` to start of free memory
+                let results := add(returndata, 0x20) // Pointer to `results`.
 
-                // `shl` 5 is equivalent to multiplying by 0x20.
-                let end := shl(5, data.length)
-                // Copy the offsets from calldata into memory.
-                calldatacopy(resultPtr, data.offset, end)
+                mstore(returndata, 0x20) // Store offset to `results` in returndata.
+                mstore(results, data.length) // Store `data.length` into `results`.
+
+                let currentResultPtr := add(results, 0x20) // Pointer to current result.
+                let dataLengthBytes := shl(5, data.length) // `shl` 5 is equivalent to multiplying by 0x20.
+
+                calldatacopy(currentResultPtr, data.offset, dataLengthBytes) // Copy the offsets from calldata into memory.
+
                 // Pointer to the top of the memory (i.e. start of the free memory).
-                let memPtr := add(resultPtr, end)
-                end := memPtr
+                let memPtr := add(currentResultPtr, dataLengthBytes)
+                let end := memPtr
+
+                let resultOffset := dataLengthBytes // Pointer to offset of `result` in returndata.
 
                 // prettier-ignore
                 for {} 1 {} {
                     // The offset of the current bytes in the calldata.
-                    let o := add(data.offset, mload(resultPtr))
+                    let o := add(data.offset, mload(currentResultPtr))
                     // Copy the current bytes from calldata to the memory.
                     calldatacopy(
                         memPtr,
@@ -37,21 +42,29 @@ abstract contract Multicallable {
                         returndatacopy(0x00, 0x00, returndatasize())
                         revert(0x00, returndatasize())
                     }
-                    // Append the current `memPtr` into `results`.
-                    mstore(resultPtr, memPtr)
-                    resultPtr := add(resultPtr, 0x20)
+                    // Append the current `resultOffset` into `results`.
+                    mstore(currentResultPtr, resultOffset)
+                    currentResultPtr := add(currentResultPtr, 0x20)
                     // Append the `returndatasize()`, and the return data.
                     mstore(memPtr, returndatasize())
                     returndatacopy(add(memPtr, 0x20), 0x00, returndatasize())
                     // Advance the `memPtr` by `returndatasize() + 0x20`,
                     // rounded up to the next multiple of 32.
-                    memPtr := and(add(add(memPtr, returndatasize()), 0x3f), 0xffffffffffffffe0)
+                    let advancePtr := shl(5, shr(5, add(returndatasize(), 0x3f)))
+
+                    memPtr := add(memPtr, advancePtr)
+                    resultOffset := add(resultOffset, advancePtr)
+
                     // prettier-ignore
-                    if eq(resultPtr, end) { break }
+                    if eq(currentResultPtr, end) { break }
                 }
-                // Allocate memory for `results`.
-                mstore(0x40, memPtr)
+
+                return(returndata, sub(memPtr, returndata))
             }
+
+            mstore(0x00, 0x20)
+            mstore(0x20, 0x00)
+            return(0x00, 0x40)
         }
     }
 }
