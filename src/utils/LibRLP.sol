c3e3ac4d95e2a5e43929433a6189f17bff3c7223
Vectorized <webby1111@hotmail.com>
2022-08-16 19:35:38 +0000

    Edit LibRLP comments

--- src/utils/LibRLP.sol
+++ src/utils/LibRLP.sol
@@ -9,19 +9,16 @@ library LibRLP {
     function computeAddress(address deployer, uint256 nonce) internal pure returns (address deployed) {
         // @dev The theoretical allowed limit, based on EIP-2681,
         // for an account nonce is 2**64-2: https://eips.ethereum.org/EIPS/eip-2681.
-
-        // Caution! This function will NOT check that the nonce is within the theoretical range.
-        // It is the user's responsibility to clean and validate the nonce.
         assembly {
             // prettier-ignore
             for {} 1 {} {
                 // The integer zero is treated as an empty byte string,
                 // and as a result it only has a length prefix, 0x80,
-                // computed via 0x80 + 0.
-                //
+                // computed via `0x80 + 0`.
+
                 // A one-byte integer in the [0x00, 0x7f] range uses its 
                 // own value as a length prefix,
-                // there is no additional "0x80 + length" prefix that precedes it.
+                // there is no additional `0x80 + length` prefix that precedes it.
                 if iszero(gt(nonce, 0x7f)) {
                     mstore(0x00, deployer)
                     // Using `mstore8` instead of `or` naturally cleans
