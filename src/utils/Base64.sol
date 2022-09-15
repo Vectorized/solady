1631b6a130871deb6db252f922c0785debd4b0b1
Vectorized <webby1111@hotmail.com>
2022-09-15 02:50:29 +0000

    Cleanup

--- src/utils/Base64.sol
+++ src/utils/Base64.sol
@@ -132,9 +132,6 @@ library Base64 {
                 // Skip the first slot, which stores the length.
                 let ptr := add(result, 0x20)
 
-                // Cache the slot.
-                // let m0x80 := mload(0x80)
-
                 // Load the table into the scratch space.
                 mstore(0x20, 0x0000000000000000000000f8fcf800fcd0d4d8dce0e4e8ecf0f4000000000000)
                 mstore(0x40, 0x000004080c1014181c2024282c3034383c4044484c5054585c606400000000fc)
