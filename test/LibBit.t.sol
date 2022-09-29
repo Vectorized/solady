a8800bd0345f736400908366acd983a0ff2e9c6d
Vectorized <webby1111@hotmail.com>
2022-09-29 16:30:57 +0800

    ✨ Add isPo2 to LibBit (#112)

--- test/LibBit.t.sol
+++ test/LibBit.t.sol
@@ -43,6 +43,43 @@ contract LibBitTest is Test {
     }
 
     function testPopCount() public {
-        assertEq(LibBit.popCount((1 << 255) | 1), 2);
+        unchecked {
+            for (uint256 i = 1; i < 256; ++i) {
+                assertEq(LibBit.popCount((1 << i) | 1), 2);
+            }
+        }
+    }
+
+    function testFuzzIsPo2(uint8 a, uint8 b) public {
+        unchecked {
+            uint256 x = (1 << uint256(a)) | (1 << uint256(b));
+            if (a == b) {
+                assertTrue(LibBit.isPo2(x));
+            } else {
+                assertFalse(LibBit.isPo2(x));
+            }
+        }
+    }
+
+    function testFuzzIsPo2(uint256 x) public {
+        uint256 c;
+        unchecked {
+            for (uint256 t = x; t != 0; c++) {
+                t &= t - 1;
+            }
+        }
+        assertEq(LibBit.isPo2(x), c == 1);
+    }
+
+    function testIsPo2() public {
+        assertFalse(LibBit.isPo2(0));
+        assertFalse(LibBit.isPo2(type(uint256).max));
+        unchecked {
+            for (uint256 i; i < 256; ++i) {
+                uint256 x = 1 << i;
+                assertTrue(LibBit.isPo2(x));
+                assertFalse(LibBit.isPo2(~x));
+            }
+        }
     }
 }
