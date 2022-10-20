fa4f58bedcef08e3a710425393f4ae46b1729a1f
Vectorized <webby1111@hotmail.com>
2022-10-21 05:31:54 +0800

    ✨ New LibBitmap functions (batchSet, batchUnset, popCount, findLastSet) (#167)
    Co-authored-by: 0xEstarriol <76832066+estarriolvetch@users.noreply.github.com>

--- src/utils/LibBitmap.sol
+++ src/utils/LibBitmap.sol
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "./LibBit.sol";
+
 /// @notice Efficient bitmap library for mapping integers to single bit booleans.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibBitmap.sol)
 /// @author Modified from Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/LibBitmap.sol)
+/// @author Modified from Solidity-Bits (https://github.com/estarriolvetch/solidity-bits/blob/main/contracts/BitMaps.sol)
 library LibBitmap {
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         CONSTANTS                          */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev The constant returned when a bitmap scan does not find a result.
+    uint256 internal constant NOT_FOUND = type(uint256).max;
+
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                          STRUCTS                           */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
@@ -78,6 +88,118 @@ library LibBitmap {
                 // Unsets the bit at `shift` via `and`, then sets its new value via `or`.
                 or(and(storageValue, not(shl(shift, 1))), shl(shift, iszero(iszero(shouldSet))))
             )
+        }
+    }
+
+    /// @dev Consecutively sets `amount` of bits starting from the bit at `start`.
+    function setBatch(
+        Bitmap storage bitmap,
+        uint256 start,
+        uint256 amount
+    ) internal {
+        assembly {
+            let max := not(0)
+            let shift := and(start, 0xff)
+            mstore(0x20, bitmap.slot)
+            mstore(0x00, shr(8, start))
+            if iszero(lt(add(shift, amount), 257)) {
+                let storageSlot := keccak256(0x00, 0x40)
+                sstore(storageSlot, or(sload(storageSlot), shl(shift, max)))
+                let bucket := add(mload(0x00), 1)
+                let bucketEnd := add(mload(0x00), shr(8, add(amount, shift)))
+                amount := and(add(amount, shift), 0xff)
+                shift := 0
+                // prettier-ignore
+                for {} iszero(eq(bucket, bucketEnd)) { bucket := add(bucket, 1) } {
+                    mstore(0x00, bucket)
+                    sstore(keccak256(0x00, 0x40), max)
+                }
+                mstore(0x00, bucket)
+            }
+            let storageSlot := keccak256(0x00, 0x40)
+            sstore(storageSlot, or(sload(storageSlot), shl(shift, shr(sub(256, amount), max))))
+        }
+    }
+
+    /// @dev Consecutively unsets `amount` of bits starting from the bit at `start`.
+    function unsetBatch(
+        Bitmap storage bitmap,
+        uint256 start,
+        uint256 amount
+    ) internal {
+        assembly {
+            let shift := and(start, 0xff)
+            mstore(0x20, bitmap.slot)
+            mstore(0x00, shr(8, start))
+            if iszero(lt(add(shift, amount), 257)) {
+                let storageSlot := keccak256(0x00, 0x40)
+                sstore(storageSlot, and(sload(storageSlot), not(shl(shift, not(0)))))
+                let bucket := add(mload(0x00), 1)
+                let bucketEnd := add(mload(0x00), shr(8, add(amount, shift)))
+                amount := and(add(amount, shift), 0xff)
+                shift := 0
+                // prettier-ignore
+                for {} iszero(eq(bucket, bucketEnd)) { bucket := add(bucket, 1) } {
+                    mstore(0x00, bucket)
+                    sstore(keccak256(0x00, 0x40), 0)
+                }
+                mstore(0x00, bucket)
+            }
+            let storageSlot := keccak256(0x00, 0x40)
+            sstore(storageSlot, and(sload(storageSlot), not(shl(shift, shr(sub(256, amount), not(0))))))
+        }
+    }
+
+    /// @dev Returns number of set bits within a range.
+    function popCount(
+        Bitmap storage bitmap,
+        uint256 start,
+        uint256 amount
+    ) internal view returns (uint256 count) {
+        unchecked {
+            uint256 bucket = start >> 8;
+            uint256 shift = start & 0xff;
+            if (!(amount + shift < 257)) {
+                count = LibBit.popCount(bitmap.map[bucket] >> shift);
+                uint256 bucketEnd = bucket + ((amount + shift) >> 8);
+                amount = (amount + shift) & 0xff;
+                shift = 0;
+                for (++bucket; bucket != bucketEnd; ++bucket) {
+                    count += LibBit.popCount(bitmap.map[bucket]);
+                }
+            }
+            count += LibBit.popCount((bitmap.map[bucket] >> shift) << (256 - amount));
+        }
+    }
+
+    /// @dev Returns the index of the most significant set bit smaller than `before`.
+    /// If no set bit is found, returns `NOT_FOUND`.
+    function findLastSet(Bitmap storage bitmap, uint256 before) internal view returns (uint256 setBitIndex) {
+        uint256 bucket;
+        uint256 bucketBits;
+        assembly {
+            setBitIndex := not(0)
+            bucket := shr(8, before)
+            mstore(0x00, bucket)
+            mstore(0x20, bitmap.slot)
+            let offset := xor(0xff, and(0xff, before)) // `256 - (255 & before) - 1`.
+            bucketBits := shr(offset, shl(offset, sload(keccak256(0x00, 0x40))))
+            if iszero(bucketBits) {
+                // prettier-ignore
+                for {} bucket {} {
+                    bucket := sub(bucket, 1)
+                    mstore(0x00, bucket)
+                    bucketBits := sload(keccak256(0x00, 0x40))
+                    // prettier-ignore
+                    if bucketBits { break }
+                }
+            }
+        }
+        if (bucketBits != 0) {
+            setBitIndex = (bucket << 8) | LibBit.fls(bucketBits);
+            assembly {
+                setBitIndex := or(setBitIndex, sub(0, gt(setBitIndex, before)))
+            }
         }
     }
 }
--- test/LibBitmap.t.sol
+++ test/LibBitmap.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.4;
 
 import "forge-std/Test.sol";
 import {LibBitmap} from "../src/utils/LibBitmap.sol";
+import {LibBit} from "../src/utils/LibBit.sol";
 
 contract LibBitmapTest is Test {
     using LibBitmap for LibBitmap.Bitmap;
@@ -140,5 +141,234 @@ contract LibBitmapTest is Test {
         this.claimWithToggle(index);
         vm.expectRevert(AlreadyClaimed.selector);
         this.claimWithToggle(index);
+    }
+
+    function testBitmapSetBatchWithinSingleBucket() public {
+        _testBitmapSetBatch(257, 30);
+    }
+
+    function testBitmapSetBatchAcrossMultipleBuckets() public {
+        _testBitmapSetBatch(10, 512);
+    }
+
+    function testBitmapSetBatch() public {
+        unchecked {
+            uint256 lcg = 123;
+            for (uint256 i; i < 8; ++i) {
+                lcg = _stepLCG(lcg);
+                uint256 start = lcg;
+                lcg = _stepLCG(lcg);
+                uint256 amount = lcg;
+                _testBitmapSetBatch(start, amount);
+            }
+        }
+    }
+
+    function testBitmapUnsetBatchWithinSingleBucket() public {
+        _testBitmapUnsetBatch(257, 30);
+    }
+
+    function testBitmapUnsetBatchAcrossMultipleBuckets() public {
+        _testBitmapUnsetBatch(10, 512);
+    }
+
+    function testBitmapUnsetBatch() public {
+        unchecked {
+            uint256 lcg = 123;
+            for (uint256 i; i < 8; ++i) {
+                lcg = _stepLCG(lcg);
+                uint256 start = lcg;
+                lcg = _stepLCG(lcg);
+                uint256 amount = lcg;
+                _testBitmapUnsetBatch(start, amount);
+            }
+        }
+    }
+
+    function testBitmapPopCountWithinSingleBucket() public {
+        _testBitmapPopCount(1, 150);
+    }
+
+    function testBitmapPopCountAcrossMultipleBuckets() public {
+        _testBitmapPopCount(10, 512);
+    }
+
+    function testBitmapPopCount(
+        uint256 start,
+        uint256 amount,
+        uint256 lcg
+    ) public {
+        unchecked {
+            uint256 n = 1000;
+            uint256 expectedCount;
+            _resetBitmap(0, n / 256 + 1);
+
+            (start, amount) = _boundStartAndAmount(start, amount, n);
+
+            uint256 jPrev = 0xff + 1;
+            uint256 j = lcg & 0xff;
+            while (true) {
+                bitmap.set(j);
+                if (j != jPrev && start <= j && j < start + amount) {
+                    expectedCount += 1;
+                }
+                if (start + amount <= j && lcg & 7 == 0) break;
+                lcg = _stepLCG(lcg);
+                jPrev = j;
+                j += lcg & 0xff;
+            }
+            assertEq(bitmap.popCount(start, amount), expectedCount);
+        }
+    }
+
+    function testBitmapPopCount() public {
+        unchecked {
+            uint256 lcg = 123;
+            for (uint256 i; i < 8; ++i) {
+                lcg = _stepLCG(lcg);
+                uint256 start = lcg;
+                lcg = _stepLCG(lcg);
+                uint256 amount = lcg;
+                lcg = _stepLCG(lcg);
+                testBitmapPopCount(start, amount, lcg);
+            }
+        }
+    }
+
+    function testBitmapFindLastSet() public {
+        unchecked {
+            bitmap.unsetBatch(0, 2000);
+            bitmap.set(1000);
+            for (uint256 i = 0; i < 1000; ++i) {
+                assertEq(bitmap.findLastSet(i), LibBitmap.NOT_FOUND);
+            }
+            bitmap.set(100);
+            bitmap.set(10);
+            for (uint256 i = 0; i < 10; ++i) {
+                assertEq(bitmap.findLastSet(i), LibBitmap.NOT_FOUND);
+            }
+            for (uint256 i = 10; i < 100; ++i) {
+                assertEq(bitmap.findLastSet(i), 10);
+            }
+            for (uint256 i = 100; i < 600; ++i) {
+                assertEq(bitmap.findLastSet(i), 100);
+            }
+            for (uint256 i = 1000; i < 1100; ++i) {
+                assertEq(bitmap.findLastSet(i), 1000);
+            }
+            bitmap.set(0);
+            for (uint256 i = 0; i < 10; ++i) {
+                assertEq(bitmap.findLastSet(i), 0);
+            }
+        }
+    }
+
+    function testBitmapFindLastSet(uint256 before, uint256 lcg) public {
+        uint256 n = 1000;
+        unchecked {
+            _resetBitmap(0, n / 256 + 1);
+            before = before % n;
+            lcg = lcg % n;
+        }
+        bitmap.set(lcg);
+        if (lcg <= before) {
+            assertEq(bitmap.findLastSet(before), lcg);
+            uint256 nextLcg = _stepLCG(lcg);
+            bitmap.set(nextLcg);
+            if (nextLcg <= before) {
+                assertEq(bitmap.findLastSet(before), (lcg < nextLcg ? nextLcg : lcg));
+            }
+        } else {
+            assertEq(bitmap.findLastSet(before), LibBitmap.NOT_FOUND);
+            uint256 nextLcg = _stepLCG(lcg);
+            bitmap.set(nextLcg);
+            if (nextLcg <= before) {
+                assertEq(bitmap.findLastSet(before), nextLcg);
+            } else {
+                assertEq(bitmap.findLastSet(before), LibBitmap.NOT_FOUND);
+            }
+        }
+    }
+
+    function _testBitmapSetBatch(uint256 start, uint256 amount) internal {
+        uint256 n = 1000;
+        (start, amount) = _boundStartAndAmount(start, amount, n);
+
+        unchecked {
+            _resetBitmap(0, n / 256 + 1);
+            bitmap.setBatch(start, amount);
+            for (uint256 i; i < n; ++i) {
+                if (i < start) {
+                    assertFalse(bitmap.get(i));
+                } else if (i < start + amount) {
+                    assertTrue(bitmap.get(i));
+                } else {
+                    assertFalse(bitmap.get(i));
+                }
+            }
+        }
+    }
+
+    function _testBitmapUnsetBatch(uint256 start, uint256 amount) internal {
+        uint256 n = 1000;
+        (start, amount) = _boundStartAndAmount(start, amount, n);
+
+        unchecked {
+            _resetBitmap(type(uint256).max, n / 256 + 1);
+            bitmap.unsetBatch(start, amount);
+            for (uint256 i; i < n; ++i) {
+                if (i < start) {
+                    assertTrue(bitmap.get(i));
+                } else if (i < start + amount) {
+                    assertFalse(bitmap.get(i));
+                } else {
+                    assertTrue(bitmap.get(i));
+                }
+            }
+        }
+    }
+
+    function _testBitmapPopCount(uint256 start, uint256 amount) internal {
+        uint256 n = 1000;
+        (start, amount) = _boundStartAndAmount(start, amount, n);
+
+        unchecked {
+            _resetBitmap(0, n / 256 + 1);
+            bitmap.setBatch(start, amount);
+            assertEq(bitmap.popCount(0, n), amount);
+            if (start > 0) {
+                assertEq(bitmap.popCount(0, start - 1), 0);
+            }
+            if (start + amount < n) {
+                assertEq(bitmap.popCount(start + amount, n - (start + amount)), 0);
+            }
+        }
+    }
+
+    function _boundStartAndAmount(
+        uint256 start,
+        uint256 amount,
+        uint256 n
+    ) private pure returns (uint256 boundedStart, uint256 boundedAmount) {
+        unchecked {
+            boundedStart = start % n;
+            uint256 end = boundedStart + (amount % n);
+            if (end > n) end = n;
+            boundedAmount = end - boundedStart;
+        }
+    }
+
+    function _resetBitmap(uint256 bucketValue, uint256 bucketEnd) private {
+        unchecked {
+            for (uint256 i; i < bucketEnd; ++i) {
+                bitmap.map[i] = bucketValue;
+            }
+        }
+    }
+
+    function _stepLCG(uint256 input) private pure returns (uint256 output) {
+        unchecked {
+            output = (input * 1664525 + 1013904223) & 0xFFFFFFFF;
+        }
     }
 }
