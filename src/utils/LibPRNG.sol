7e3083a22d658da343556857ab94aa66475c1cd0
Vectorized <webby1111@hotmail.com>
2022-10-23 05:42:08 +0800

    ✨ LibPRNG new functions (#176)
    * Refactor
    
    * Add shuffle

--- src/utils/LibPRNG.sol
+++ src/utils/LibPRNG.sol
@@ -4,10 +4,29 @@ pragma solidity ^0.8.4;
 /// @notice Library for generating psuedorandom numbers.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibPRNG.sol)
 library LibPRNG {
-    /// @dev Returns the next psuedorandom state.
-    /// The input `state` can be any uint256 number, including 0.
-    /// All bits of the returned state pass the NIST Statistical Test Suite.
-    function next(uint256 state) internal pure returns (uint256 randomness) {
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                          STRUCTS                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev A psuedorandom number state in memory.
+    struct PRNG {
+        uint256 state;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         OPERATIONS                         */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Seeds the `prng` with `state`.
+    function seed(PRNG memory prng, uint256 state) internal pure {
+        assembly {
+            mstore(prng, state)
+        }
+    }
+
+    /// @dev Returns the next psuedorandom uint256.
+    /// All bits of the returned uint256 pass the NIST Statistical Test Suite.
+    function next(PRNG memory prng) internal pure returns (uint256 result) {
         // We simply use `keccak256` for a great balance between
         // runtime gas costs, bytecode size, and statistical properties.
         //
@@ -17,10 +36,53 @@ library LibPRNG {
         // when this function is inlined.
         //
         // Using this method is about 2x more efficient than
-        // `uint256(keccak256(abi.encode(state)))`.
+        // `nextRandomness = uint256(keccak256(abi.encode(randomness)))`.
         assembly {
-            mstore(0x00, state)
-            randomness := keccak256(0x00, 0x20)
+            result := keccak256(prng, 0x20)
+            mstore(prng, result)
+        }
+    }
+
+    /// @dev Returns a psuedorandom uint256, uniformly distributed
+    /// between 0 (inclusive) and `upper` (exclusive).
+    /// This method is recommended for uniform sampling to avoid modulo bias.
+    /// For uniform sampling across all uint256 values, use `next` instead.
+    function uniform(PRNG memory prng, uint256 upper) internal pure returns (uint256 result) {
+        assembly {
+            // prettier-ignore
+            for { let min := mod(sub(0, upper), upper) } 1 {} {
+                result := keccak256(prng, 0x20)
+                mstore(prng, result) 
+                // prettier-ignore
+                if iszero(lt(result, min)) { break }
+            }
+            result := mod(result, upper)
+        }
+    }
+
+    /// @dev Shuffles the array in-place with Fisher-Yates shuffle.
+    function shuffle(PRNG memory prng, uint256[] memory a) internal pure {
+        assembly {
+            let n := mload(a)
+            let o := add(a, 0x20)
+            // prettier-ignore
+            for { let j := 0 } iszero(lt(n, 2)) {} {
+                // prettier-ignore
+                for {} 1 {} {
+                    // We can just directly use `keccak256`, cuz
+                    // the other approaches don't save much.
+                    j := keccak256(prng, 0x20)
+                    mstore(prng, j)
+                    // prettier-ignore
+                    if iszero(lt(j, mod(sub(0, n), n))) { break }
+                }
+                j := add(o, shl(5, mod(j, n)))
+                n := sub(n, 1)
+                let i := add(o, shl(5, n))
+                let t := mload(i)
+                mstore(i, mload(j))
+                mstore(j, t)
+            }
         }
     }
 }
--- test/LibPRNG.t.sol
+++ test/LibPRNG.t.sol
@@ -3,8 +3,88 @@ pragma solidity ^0.8.4;
 
 import "./utils/TestPlus.sol";
 import {LibPRNG} from "../src/utils/LibPRNG.sol";
+import {LibSort} from "../src/utils/LibSort.sol";
 
 contract LibPRNGTest is TestPlus {
+    using LibPRNG for *;
+
+    function testPRNGNext() public {
+        unchecked {
+            // Super unlikely to fail.
+            for (uint256 i; i < 32; ++i) {
+                LibPRNG.PRNG memory prng;
+                prng.seed(i);
+                uint256 r0 = prng.next();
+                uint256 r1 = prng.next();
+                uint256 r2 = prng.next();
+                assertTrue(r0 != r1);
+                assertTrue(r1 != r2);
+                prng.seed(i * 2);
+                uint256 r3 = prng.next();
+                assertTrue(r2 != r3);
+            }
+        }
+    }
+
+    function testPRNGUniform() public {
+        unchecked {
+            LibPRNG.PRNG memory prng;
+            for (uint256 i = 1; i < 32; ++i) {
+                for (uint256 j; j < 32; ++j) {
+                    assertTrue(prng.uniform(i) < i);
+                }
+            }
+            for (uint256 i; i < 32; ++i) {
+                assertTrue(prng.uniform(0) == 0);
+            }
+            // Super unlikely to fail.
+            uint256 previous;
+            for (uint256 i = 128; i < 256; ++i) {
+                uint256 n = 1 << i;
+                for (uint256 j; j < 8; ++j) {
+                    uint256 r = prng.uniform(n);
+                    assertTrue(r < n);
+                    assertTrue(r != previous);
+                    previous = r;
+                }
+            }
+        }
+    }
+
+    function testPRNGShuffleGas() public pure {
+        unchecked {
+            uint256[] memory a = new uint256[](10000);
+            LibPRNG.PRNG memory prng;
+            prng.shuffle(a);
+        }
+    }
+
+    function testPRNGShuffle() public {
+        unchecked {
+            for (uint256 s = 1; s < 9; ++s) {
+                uint256 n = 1 << s; // 2, 4, 8, 16, ...
+                uint256[] memory a = new uint256[](n);
+                for (uint256 i; i < n; ++i) {
+                    a[i] = i;
+                }
+                bytes32 hashBefore = keccak256(abi.encode(a));
+                uint256 numDifferent;
+                LibPRNG.PRNG memory prng;
+                for (uint256 i; i < 30; ++i) {
+                    prng.shuffle(a);
+                    bytes32 hashAfterShuffle = keccak256(abi.encode(a));
+                    if (hashBefore != hashAfterShuffle) {
+                        numDifferent++;
+                    }
+                    LibSort.sort(a);
+                    bytes32 hashAfterSort = keccak256(abi.encode(a));
+                    assertTrue(hashBefore == hashAfterSort);
+                }
+                assertTrue(numDifferent > 1);
+            }
+        }
+    }
+
     function testLCGGas() public {
         unchecked {
             uint256 randomness;
@@ -17,9 +97,10 @@ contract LibPRNGTest is TestPlus {
 
     function testPRNGGas() public {
         unchecked {
+            LibPRNG.PRNG memory prng;
             uint256 randomness;
             for (uint256 i; i < 256; i++) {
-                randomness = LibPRNG.next(randomness);
+                randomness = prng.next();
             }
             assertTrue(randomness != 0);
         }
