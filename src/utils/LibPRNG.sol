5c6dc87862f33defa334e62e276fd19300bc4ad7
Vectorized <webby1111@hotmail.com>
2022-10-24 01:41:48 +0000

    Add comments

--- src/utils/LibPRNG.sol
+++ src/utils/LibPRNG.sol
@@ -45,8 +45,11 @@ library LibPRNG {
 
     /// @dev Returns a psuedorandom uint256, uniformly distributed
     /// between 0 (inclusive) and `upper` (exclusive).
-    /// This method is recommended for uniform sampling to avoid modulo bias.
-    /// For uniform sampling across all uint256 values, use `next` instead.
+    /// If your modulus is big, this method is recommended
+    /// for uniform sampling to avoid modulo bias.
+    /// For uniform sampling across all uint256 values,
+    /// or for small enough moduli such that the bias is neligible,
+    /// use `next` instead.
     function uniform(PRNG memory prng, uint256 upper) internal pure returns (uint256 result) {
         assembly {
             // prettier-ignore
@@ -73,6 +76,10 @@ library LibPRNG {
                     let r := keccak256(prng, 0x20)
                     mstore(prng, r)
 
+                    // Note that there will be a very tiny modulo bias
+                    // if the length of the array is not a power of 2.
+                    // For all practical purposes, it is neligible
+                    // and will not be a fairness or security concern.
                     {
                         let j := add(a, shl(5, mod(shr(128, r), n)))
                         n := sub(n, 1)
