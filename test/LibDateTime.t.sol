b7c666ea8556a390d6fc0c674e9028ee89101644
Atarpara <akpatel0618@gmail.com>
2022-11-06 15:32:40 +0530

    🐞 Test Fixed

--- test/LibDateTime.t.sol
+++ test/LibDateTime.t.sol
@@ -84,8 +84,8 @@ contract LibDateTimeTest is TestPlus {
     ) public {
         // MAX POSSIBLE DAY = 115792089237316195423570985008687907853269984665640564039457584007913128920467
         // MAX DATE = 317027972476686572410305440929486321699336700043506886628630523577932824465 - 12 - 03
-        _y = _bound(_y,1970,3669305236998687180674831492239425019668248843096144521164705134005821);
-        _m = _bound(_m,1,12);
+        _y = _bound(_y, 1970, 3669305236998687180674831492239425019668248843096144521164705134005821);
+        _m = _bound(_m, 1, 12);
         uint256 md = LibDateTime.getDaysInMonth(_y,_m);
         _d = _bound(_d,1,md);
         uint256 day = LibDateTime.daysFromDate(_y, _m, _d);
@@ -206,7 +206,7 @@ contract LibDateTimeTest is TestPlus {
         uint256 m,
         uint256 d
     ) public {
-        if (y > 1969 && y < 317027972476686572410305440929486321699336700043506886628630523577932824464) {
+        if (y > 1969 && y < 3669305236998687180674831492239425019668248843096144521164705134005822) {
             if (m > 0 && m < 13 && d > 0 && d < LibDateTime.getDaysInMonth(y, m)) {
                 assertTrue(LibDateTime.isValidDate(y, m, d));
             }
