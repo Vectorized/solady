83ffe1093bac4d1c903434966f0ba7062f47e28b
Vectorized <webby1111@hotmail.com>
2022-11-22 16:04:45 +0000

    Fmt

--- src/utils/LibRLP.sol
+++ src/utils/LibRLP.sol
@@ -31,13 +31,12 @@ library LibRLP {
     {
         /// @solidity memory-safe-assembly
         assembly {
-            // forgefmt: disable-next-item
             for {} 1 {} {
                 // The integer zero is treated as an empty byte string,
                 // and as a result it only has a length prefix, 0x80,
                 // computed via `0x80 + 0`.
 
-                // A one-byte integer in the [0x00, 0x7f] range uses its 
+                // A one-byte integer in the [0x00, 0x7f] range uses its
                 // own value as a length prefix,
                 // there is no additional `0x80 + length` prefix that precedes it.
                 if iszero(gt(nonce, 0x7f)) {
@@ -53,7 +52,6 @@ library LibRLP {
                 }
                 let i := 8
                 // Just use a loop to generalize all the way with minimal bytecode size.
-                // forgefmt: disable-next-item
                 for {} shr(i, nonce) { i := add(i, 8) } {}
                 // `shr` 3 is equivalent to dividing by 8.
                 i := shr(3, i)
