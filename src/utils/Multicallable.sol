465dbe1f4f15b7dab5ca9ef0685f58197789b904
Vectorized <webby1111@hotmail.com>
2023-01-06 23:20:28 +0800

    ⚡️ Optimize Multicallable (#260)
    * Optimize multicall
    
    * Strengthen tests

--- src/utils/Multicallable.sol
+++ src/utils/Multicallable.sol
@@ -14,50 +14,49 @@ abstract contract Multicallable {
     /// and store the `abi.encode` formatted results of each `DELEGATECALL` into `results`.
     /// If any of the `DELEGATECALL`s reverts, the entire transaction is reverted,
     /// and the error is bubbled up.
-    function multicall(bytes[] calldata data) public payable returns (bytes[] memory results) {
+    function multicall(bytes[] calldata data) external payable returns (bytes[] memory) {
         assembly {
-            if data.length {
-                results := mload(0x40) // Point `results` to start of free memory.
-                mstore(results, data.length) // Store `data.length` into `results`.
-                results := add(results, 0x20)
+            mstore(0x00, 0x20)
+            mstore(0x20, data.length) // Store `data.length` into `results`.
+            // Early return if no data.
+            if iszero(data.length) { return(0x00, 0x40) }
 
-                // `shl` 5 is equivalent to multiplying by 0x20.
-                let end := shl(5, data.length)
-                // Copy the offsets from calldata into memory.
-                calldatacopy(results, data.offset, end)
-                // Pointer to the top of the memory (i.e. start of the free memory).
-                let memPtr := add(results, end)
-                end := add(results, end)
+            let results := 0x40
+            // `shl` 5 is equivalent to multiplying by 0x20.
+            let end := shl(5, data.length)
+            // Copy the offsets from calldata into memory.
+            calldatacopy(0x40, data.offset, end)
+            // Pointer to the top of the memory (i.e. start of the free memory).
+            let resultsOffset := end
 
-                for {} 1 {} {
-                    // The offset of the current bytes in the calldata.
-                    let o := add(data.offset, mload(results))
-                    // Copy the current bytes from calldata to the memory.
-                    calldatacopy(
-                        memPtr,
-                        add(o, 0x20), // The offset of the current bytes' bytes.
-                        calldataload(o) // The length of the current bytes.
-                    )
-                    if iszero(delegatecall(gas(), address(), memPtr, calldataload(o), 0x00, 0x00)) {
-                        // Bubble up the revert if the delegatecall reverts.
-                        returndatacopy(0x00, 0x00, returndatasize())
-                        revert(0x00, returndatasize())
-                    }
-                    // Append the current `memPtr` into `results`.
-                    mstore(results, memPtr)
-                    results := add(results, 0x20)
-                    // Append the `returndatasize()`, and the return data.
-                    mstore(memPtr, returndatasize())
-                    returndatacopy(add(memPtr, 0x20), 0x00, returndatasize())
-                    // Advance the `memPtr` by `returndatasize() + 0x20`,
-                    // rounded up to the next multiple of 32.
-                    memPtr := and(add(add(memPtr, returndatasize()), 0x3f), 0xffffffffffffffe0)
-                    if iszero(lt(results, end)) { break }
+            for { end := add(results, end) } 1 {} {
+                // The offset of the current bytes in the calldata.
+                let o := add(data.offset, mload(results))
+                let memPtr := add(resultsOffset, 0x40)
+                // Copy the current bytes from calldata to the memory.
+                calldatacopy(
+                    memPtr,
+                    add(o, 0x20), // The offset of the current bytes' bytes.
+                    calldataload(o) // The length of the current bytes.
+                )
+                if iszero(delegatecall(gas(), address(), memPtr, calldataload(o), 0x00, 0x00)) {
+                    // Bubble up the revert if the delegatecall reverts.
+                    returndatacopy(0x00, 0x00, returndatasize())
+                    revert(0x00, returndatasize())
                 }
-                // Restore `results` and allocate memory for it.
-                results := mload(0x40)
-                mstore(0x40, memPtr)
+                // Append the current `resultsOffset` into `results`.
+                mstore(results, resultsOffset)
+                results := add(results, 0x20)
+                // Append the `returndatasize()`, and the return data.
+                mstore(memPtr, returndatasize())
+                returndatacopy(add(memPtr, 0x20), 0x00, returndatasize())
+                // Advance the `resultsOffset` by `returndatasize() + 0x20`,
+                // rounded up to the next multiple of 32.
+                resultsOffset :=
+                    and(add(add(resultsOffset, returndatasize()), 0x3f), 0xffffffffffffffe0)
+                if iszero(lt(results, end)) { break }
             }
+            return(0x00, add(resultsOffset, 0x40))
         }
     }
 }
