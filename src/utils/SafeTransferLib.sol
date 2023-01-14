a214cf0a3302dc19ab22768f7aace7766fa8baff
Vectorized <webby1111@hotmail.com>
2023-01-14 17:29:11 +0000

    Add transfer all functions

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -164,9 +164,6 @@ library SafeTransferLib {
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
                     or(eq(mload(0x00), 1), iszero(returndatasize())),
-                    // We use 0x64 because that's the total length of our calldata (0x04 + 0x20 * 3)
-                    // Counterintuitively, this call() must be positioned after the or() in the
-                    // surrounding and() because and() evaluates its arguments from right to left.
                     call(gas(), token, 0, 0x1c, 0x64, 0x00, 0x20)
                 )
             ) {
@@ -175,6 +172,55 @@ library SafeTransferLib {
                 // Revert with (offset, size).
                 revert(0x1c, 0x04)
             }
+
+            mstore(0x60, 0) // Restore the zero slot to zero.
+            mstore(0x40, m) // Restore the free memory pointer.
+        }
+    }
+
+    /// @dev Sends all of ERC20 `token` from `from` to `to`.
+    /// Reverts upon failure.
+    ///
+    /// The `from` account must have at least `amount` approved for
+    /// the current contract to manage.
+    function safeTransferAllFrom(address token, address from, address to)
+        internal
+        returns (uint256 amount)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let m := mload(0x40) // Cache the free memory pointer.
+
+            mstore(0x00, 0x70a08231) // Store the function selector of `balanceOf(address)`.
+            mstore(0x20, from) // Store the `from` argument.
+            if iszero(
+                and(eq(returndatasize(), 0x20), staticcall(gas(), token, 0x1c, 0x24, 0x60, 0x20))
+            ) {
+                // Store the function selector of `TransferFromFailed()`.
+                mstore(0x00, 0x7939f424)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+
+            // Store the function selector of `transferFrom(address,address,uint256)`.
+            mstore(0x00, 0x23b872dd)
+            mstore(0x40, to) // Store the `to` argument.
+
+            if iszero(
+                and(
+                    // Set success to whether the call reverted, if not we check it either
+                    // returned exactly 1 (can't just be non-zero data), or had no return data.
+                    or(eq(mload(0x00), 1), iszero(returndatasize())),
+                    call(gas(), token, 0, 0x1c, 0x64, 0x00, 0x20)
+                )
+            ) {
+                // Store the function selector of `TransferFromFailed()`.
+                mstore(0x00, 0x7939f424)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+            // The `amount` argument is already written to the memory word at 0x6a.
+            amount := mload(0x60)
 
             mstore(0x60, 0) // Restore the zero slot to zero.
             mstore(0x40, m) // Restore the free memory pointer.
@@ -198,9 +244,6 @@ library SafeTransferLib {
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
                     or(eq(mload(0x00), 1), iszero(returndatasize())),
-                    // We use 0x44 because that's the total length of our calldata (0x04 + 0x20 * 2)
-                    // Counterintuitively, this call() must be positioned after the or() in the
-                    // surrounding and() because and() evaluates its arguments from right to left.
                     call(gas(), token, 0, 0x16, 0x44, 0x00, 0x20)
                 )
             ) {
@@ -209,6 +252,49 @@ library SafeTransferLib {
                 // Revert with (offset, size).
                 revert(0x1c, 0x04)
             }
+            // Restore the part of the free memory pointer that was overwritten,
+            // which is guaranteed to be zero, if less than 8tb of memory is used.
+            mstore(0x3a, 0)
+        }
+    }
+
+    /// @dev Sends all of ERC20 `token` from the current contract to `to`.
+    /// Reverts upon failure.
+    function safeTransferAll(address token, address to) internal returns (uint256 amount) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x00, 0x70a08231) // Store the function selector of `balanceOf(address)`.
+            mstore(0x20, address()) // Store the address of the current contract.
+            if iszero(
+                and(eq(returndatasize(), 0x20), staticcall(gas(), token, 0x1c, 0x24, 0x3a, 0x20))
+            ) {
+                // Store the function selector of `TransferFailed()`.
+                mstore(0x00, 0x90b8ec18)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+
+            mstore(0x1a, to) // Store the `to` argument.
+            // Store the function selector of `transfer(address,uint256)`,
+            // left by 6 bytes (enough for 8tb of memory represented by the free memory pointer).
+            // We waste 6-3 = 3 bytes to save on 6 runtime gas (PUSH1 0x224 SHL).
+            mstore(0x00, 0xa9059cbb000000000000)
+
+            if iszero(
+                and(
+                    // Set success to whether the call reverted, if not we check it either
+                    // returned exactly 1 (can't just be non-zero data), or had no return data.
+                    or(eq(mload(0x00), 1), iszero(returndatasize())),
+                    call(gas(), token, 0, 0x16, 0x44, 0x00, 0x20)
+                )
+            ) {
+                // Store the function selector of `TransferFailed()`.
+                mstore(0x00, 0x90b8ec18)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+            // The `amount` argument is already written to the memory word at 0x3a.
+            amount := mload(0x3a)
             // Restore the part of the free memory pointer that was overwritten,
             // which is guaranteed to be zero, if less than 8tb of memory is used.
             mstore(0x3a, 0)
@@ -232,9 +318,6 @@ library SafeTransferLib {
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
                     or(eq(mload(0x00), 1), iszero(returndatasize())),
-                    // We use 0x44 because that's the total length of our calldata (0x04 + 0x20 * 2)
-                    // Counterintuitively, this call() must be positioned after the or() in the
-                    // surrounding and() because and() evaluates its arguments from right to left.
                     call(gas(), token, 0, 0x16, 0x44, 0x00, 0x20)
                 )
             ) {
@@ -246,6 +329,21 @@ library SafeTransferLib {
             // Restore the part of the free memory pointer that was overwritten,
             // which is guaranteed to be zero, if less than 8tb of memory is used.
             mstore(0x3a, 0)
+        }
+    }
+
+    /// @dev Returns the amount of ERC20 `token` owned by `account`.
+    /// Returns zero if the `token` does not exist.
+    function balanceOf(address token, address account) internal view returns (uint256 amount) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x00, 0x70a08231) // Store the function selector of `balanceOf(address)`.
+            mstore(0x20, account) // Store the `account` argument.
+            amount :=
+                mul(
+                    mload(0x20),
+                    and(eq(returndatasize(), 0x20), staticcall(gas(), token, 0x1c, 0x24, 0x20, 0x20))
+                )
         }
     }
 }
