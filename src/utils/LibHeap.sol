4e5b32121eaed4f51216ae08640f116c6260db14
Vectorized <webby1111@hotmail.com>
2023-01-17 00:23:53 +0000

    Simplify

--- src/utils/LibHeap.sol
+++ src/utils/LibHeap.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-/// @notice Library for managing a min or max heap in storage.
+/// @notice Library for managing a minimum heap in storage.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibHeap.sol)
 library LibHeap {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -24,9 +24,7 @@ library LibHeap {
     /*                         OPERATIONS                         */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    /// @dev Returns the root of the heap.
-    /// If the heap is a min-heap, the returned value is the minimum.
-    /// Otherwise, the heap is a max-heap, and the returned value is the maximum.
+    /// @dev Returns the minimum value of the heap.
     /// Reverts if the heap is empty.
     function root(Heap storage heap) internal view returns (uint256 result) {
         /// @solidity memory-safe-assembly
@@ -46,13 +44,124 @@ library LibHeap {
     }
 
     /// @dev Pushes the `value` onto the min-heap.
-    function pushMin(Heap storage heap, uint256 value) internal {
+    function push(Heap storage heap, uint256 value) internal {
+        _set(heap, value, 0, 4);
+    }
+
+    /// @dev Pops the minimum value from the min-heap.
+    /// Reverts if the heap is empty.
+    function pop(Heap storage heap) internal returns (uint256 popped) {
+        (,, popped) = _set(heap, 0, 0, 0);
+    }
+
+    /// @dev Pops the minimum value, and pushes the new `value` onto the min-heap.
+    /// Reverts if the heap is empty.
+    function replace(Heap storage heap, uint256 value) internal returns (uint256 popped) {
+        (,, popped) = _set(heap, value, 0, 1);
+    }
+
+    /// @dev Pushes the `value` onto the min-heap, and pops the minimum value.
+    function pushPop(Heap storage heap, uint256 value) internal returns (uint256 popped) {
+        (,, popped) = _set(heap, value, 0, 2);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                      PRIVATE HELPERS                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function _set(Heap storage heap, uint256 value, uint256 maxLength, uint256 mode)
+        private
+        returns (bool success, bool hasPopped, uint256 popped)
+    {
         /// @solidity memory-safe-assembly
         assembly {
-            let pos := sload(heap.slot)
-            sstore(heap.slot, add(pos, 1))
+            let n := sload(heap.slot)
+            // Compute the array storage slot offset.
             mstore(0x00, heap.slot)
             let sOffset := keccak256(0x00, 0x20)
+
+            let pos := 0
+            let childPos := not(0)
+            for {} 1 {
+                mstore(0x00, 0xa6ca772e) // Store the function selector of `HeapIsEmpty()`.
+                revert(0x1c, 0x04) // Revert with (offset, size).
+            } {
+                if eq(mode, 3) {
+                    // Enqueue Min.
+                    if iszero(maxLength) { continue }
+                    if iszero(eq(n, maxLength)) {
+                        // If queue is not full.
+                        success := 1
+                        // Increment and update the length.
+                        pos := n
+                        sstore(heap.slot, add(pos, 1))
+                        childPos := add(childPos, childPos)
+                        break
+                    }
+                    let r := sload(sOffset)
+                    if lt(r, value) {
+                        success := 1
+                        hasPopped := 1
+                        popped := r
+                        childPos := 1
+                        break
+                    }
+                    break
+                }
+                if eq(mode, 4) {
+                    // Push.
+                    // Increment and update the length.
+                    pos := n
+                    sstore(heap.slot, add(pos, 1))
+                    childPos := add(childPos, childPos)
+                    break
+                }
+                if eq(mode, 1) {
+                    // Replace.
+                    if iszero(n) { continue }
+                    popped := sload(sOffset)
+                    childPos := 1
+                    break
+                }
+                if iszero(mode) {
+                    // Pop.
+                    if iszero(n) { continue }
+                    // Decrement and update the length.
+                    n := sub(n, 1)
+                    sstore(heap.slot, n)
+                    // Set the `value` to the last item.
+                    value := sload(add(sOffset, n))
+                    popped := value
+                    if n {
+                        popped := sload(sOffset)
+                        childPos := 1
+                    }
+                    break
+                }
+                // Push pop.
+                popped := value
+                if n {
+                    let r := sload(sOffset)
+                    if lt(r, value) {
+                        popped := r
+                        childPos := 1
+                    }
+                }
+                break
+            }
+
+            for {} lt(childPos, n) {} {
+                let child := sload(add(sOffset, childPos))
+                let rightPos := add(childPos, 1)
+                let right := sload(add(sOffset, rightPos))
+                if iszero(and(lt(rightPos, n), iszero(lt(child, right)))) {
+                    right := child
+                    rightPos := childPos
+                }
+                sstore(add(sOffset, pos), right)
+                pos := rightPos
+                childPos := add(shl(1, pos), 1)
+            }
 
             for {} pos {} {
                 let parentPos := shr(1, sub(pos, 1))
@@ -61,160 +170,8 @@ library LibHeap {
                 sstore(add(sOffset, pos), parent)
                 pos := parentPos
             }
-            sstore(add(sOffset, pos), value)
-        }
-    }
-
-    /// @dev Pops the minimum value from the min-heap.
-    /// Reverts if the heap is empty.
-    function popMin(Heap storage heap) internal returns (uint256 popped) {
-        popped = _setMin(heap, 0, 0);
-    }
-
-    /// @dev Pops the minimum value, and pushes the new `value` onto the min-heap.
-    /// Reverts if the heap is empty.
-    function replaceMin(Heap storage heap, uint256 value) internal returns (uint256 popped) {
-        _setMin(heap, value, 1);
-    }
-
-    /// @dev Pushes the `value` onto the min-heap, and pops the minimum value.
-    function pushPopMin(Heap storage heap, uint256 value) internal returns (uint256 popped) {
-        popped = _setMin(heap, value, 2);
-    }
-
-    /// @dev Pushes the `value` onto the min-heap, and pops the minimum value
-    /// if the length of the heap exceeds `maxLength`.
-    ///
-    /// Reverts if `maxLength` is zero.
-    ///
-    /// - If `value` is not greater than the minimum value on the heap and queue is not full:
-    ///   (`success` = true, `hasPopped` = false, `popped` = 0)
-    /// - If `value` is not greater than the minimum value on the heap and queue is full:
-    ///   (`success` = false, `hasPopped` = false, `popped` = 0)
-    /// - If `value` is greater than the minimum value on the heap and queue is full:
-    ///   (`success` = true, `hasPopped` = true, `popped` = <minimum value>)
-    ///
-    /// Useful for implementing a bounded priority queue.
-    function enqueueMin(Heap storage heap, uint256 value, uint256 maxLength)
-        internal
-        returns (bool success, bool hasPopped, uint256 popped)
-    {}
-
-    /// @dev Pushes the `value` onto the max-heap.
-    function pushMax(Heap storage heap, uint256 value) internal {}
-
-    /// @dev Pops the maximum value from the max-heap.
-    /// Reverts if the heap is empty.
-    function popMax(Heap storage heap) internal returns (uint256 popped) {}
-
-    /// @dev Pops the maximum value, and pushes the new `value` onto the max-heap.
-    /// Reverts if the heap is empty.
-    function replaceMax(Heap storage heap, uint256 value) internal returns (uint256 popped) {}
-
-    /// @dev Pushes the `value` onto the max-heap, and pops the maximum value.
-    function pushPopMax(Heap storage heap, uint256 value) internal returns (uint256 popped) {}
-
-    /// @dev Pushes the `value` onto the max-heap, and pops the maximum value
-    /// if the length of the heap exceeds `maxLength`.
-    ///
-    /// Reverts if `maxLength` is zero.
-    ///
-    /// - If `value` is not less than the maximum value on the heap and queue is not full:
-    ///   (`success` = true, `hasPopped` = false, `popped` = 0)
-    /// - If `value` is not less than the maximum value on the heap and queue is full:
-    ///   (`success` = false, `hasPopped` = false, `popped` = 0)
-    /// - If `value` is less than the maximum value on the heap and queue is full:
-    ///   (`success` = true, `hasPopped` = true, `popped` = <maximum value>)
-    ///
-    /// Useful for implementing a bounded priority queue.
-    function enqueueMax(Heap storage heap, uint256 value, uint256 maxLength)
-        internal
-        returns (bool success, bool hasPopped, uint256 popped)
-    {}
-
-    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
-    /*                      PRIVATE HELPERS                       */
-    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
-
-    function _setMin(Heap storage heap, uint256 value, uint256 mode)
-        private
-        returns (uint256 popped)
-    {
-        /// @solidity memory-safe-assembly
-        assembly {
-            let n := sload(heap.slot)
-
-            mstore(0x00, heap.slot)
-            let sOffset := keccak256(0x00, 0x20)
-
-            let needsSift := 0
-            switch mode
-            case 0 {
-                // Pop.
-                if iszero(n) {
-                    // Revert with `HeapIsEmpty()` if heap is empty.
-                    mstore(0x00, 0xa6ca772e)
-                    revert(0x1c, 0x04)
-                }
-                // Decrement and update the length.
-                n := sub(n, 1)
-                sstore(heap.slot, n)
-                // Set the `value` to the last item.
-                value := sload(add(sOffset, n))
-                popped := value
-                if n {
-                    popped := sload(sOffset)
-                    needsSift := 1
-                }
-            }
-            case 1 {
-                // Replace.
-                if iszero(n) {
-                    // Revert with `HeapIsEmpty()` if heap is empty.
-                    mstore(0x00, 0xa6ca772e)
-                    revert(0x1c, 0x04)
-                }
-                popped := sload(sOffset)
-                needsSift := 1
-            }
-            case 2 {
-                // Push Pop.
-                if n {
-                    popped := sload(sOffset)
-                    needsSift := 1
-                    if lt(value, popped) {
-                        popped := value
-                        needsSift := 0
-                    }
-                }
-            }
-
-            if needsSift {
-                let pos := 0
-
-                for { let childPos := 1 } 1 {} {
-                    let child := sload(add(sOffset, childPos))
-                    let rightPos := add(childPos, 1)
-                    let right := sload(add(sOffset, rightPos))
-                    if iszero(and(lt(rightPos, n), iszero(lt(child, right)))) {
-                        right := child
-                        rightPos := childPos
-                    }
-                    sstore(add(sOffset, pos), right)
-                    pos := rightPos
-                    childPos := add(shl(1, pos), 1)
-                    if iszero(lt(childPos, n)) { break }
-                }
-
-                for {} pos {} {
-                    let parentPos := shr(1, sub(pos, 1))
-                    let parent := sload(add(sOffset, parentPos))
-                    if iszero(lt(value, parent)) { break }
-                    sstore(add(sOffset, pos), parent)
-                    pos := parentPos
-                }
-                sstore(add(sOffset, pos), value)
-            }
+            // If `childPos` is not `not(0)`.
+            if add(childPos, 1) { sstore(add(sOffset, pos), value) }
         }
     }
 }
--- test/LibHeap.t.sol
+++ test/LibHeap.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 import "./utils/TestPlus.sol";
 import {LibHeap} from "../src/utils/LibHeap.sol";
 import {LibSort} from "../src/utils/LibSort.sol";
+import {LibString} from "../src/utils/LibString.sol";
 
 contract LibHeapTest is TestPlus {
     using LibHeap for LibHeap.Heap;
@@ -20,55 +21,40 @@ contract LibHeapTest is TestPlus {
         assertEq(heap0.root(), x);
     }
 
-    function testHeapPushAndPopMin(uint256) public {
+    function testHeapPushAndPop(uint256) public {
         unchecked {
             uint256 n = _random() % 32;
             uint256[] memory a = new uint256[](n);
+
             for (uint256 i; i < n; ++i) {
                 uint256 r = _random();
                 a[i] = r;
-                heap0.pushMin(r);
+                heap0.push(r);
             }
             LibSort.sort(a);
             for (uint256 i; i < n; ++i) {
-                assertEq(heap0.popMin(), a[i]);
+                assertEq(heap0.pop(), a[i]);
             }
+            assertEq(heap0.length(), 0);
         }
     }
 
-    function _checkMinHeapInvariant(LibHeap.Heap storage heap) private view {
+    function testHeapPushPop(uint256) public {
         unchecked {
-            uint256 n = heap.data.length;
-            for (uint256 i = 1; i < n; ++i) {
-                uint256 parentPos = (i - 1) >> 1;
-                uint256 item = heap.data[i];
-                if (heap.data[parentPos] > item) {
-                    revert("Min heap invariant violated.");
-                }
+            uint256 n = _random() % 32;
+            for (uint256 i; i < n; ++i) {
+                uint256 r = _random();
+                heap0.push(r);
+                heap1.push(r);
+            }
+            n = _random() % 32;
+            for (uint256 i; i < 2; ++i) {
+                uint256 r = _random();
+                uint256 popped0 = heap0.pushPop(r);
+                heap1.push(r);
+                uint256 popped1 = heap1.pop();
+                assertEq(popped0, popped1);
             }
         }
-    }
-
-    function _checkMinHeapInvariant() private view {
-        _checkMinHeapInvariant(heap0);
-        _checkMinHeapInvariant(heap1);
-    }
-
-    function _checkMaxHeapInvariant(LibHeap.Heap storage heap) private view {
-        unchecked {
-            uint256 n = heap.data.length;
-            for (uint256 i = 1; i < n; ++i) {
-                uint256 parentPos = (i - 1) >> 1;
-                uint256 item = heap.data[i];
-                if (heap.data[parentPos] < item) {
-                    revert("Max heap invariant violated.");
-                }
-            }
-        }
-    }
-
-    function _checkMaxHeapInvariant() private view {
-        _checkMinHeapInvariant(heap0);
-        _checkMinHeapInvariant(heap1);
     }
 }
