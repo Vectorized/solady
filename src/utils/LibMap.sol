7eec9ef4bf71774154e1953217d0ac3cf64ae386
Vectorized <webby1111@hotmail.com>
2023-01-17 22:12:56 +0000

    Update README

--- src/utils/LibMap.sol
+++ src/utils/LibMap.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-/// @notice Library for mapping integers to packed integers.
+/// @notice Library for storage of packed unsigned integers.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibMap.sol)
 library LibMap {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
