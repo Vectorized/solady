6bc8362db0a375e77de2308d4f2ec1d718e5dfd3
Vectorized <webby1111@hotmail.com>
2023-02-09 09:02:33 +0000

    Improve test randomness

--- test/utils/TestPlus.sol
+++ test/utils/TestPlus.sol
@@ -88,18 +88,22 @@ contract TestPlus is Test {
                     r := and(r, 3)
                     break
                 }
-                // With a 15/256 chance, set `r` to near a random power of 2.
-                if iszero(gt(d, 16)) {
-                    let u := and(0x80, r) // With a 1/2 chance, negate `r`.
-                    let t := 1
-                    // If the 1st byte of `r` is not greater than 32, set `t` to `not(0)`.
-                    if iszero(gt(byte(1, r), 32)) { t := not(0) }
-                    // If the 2nd byte of `r` is not greater than 128, set `t` to `xor(sValue, r)`.
-                    if iszero(gt(byte(2, r), 128)) { t := xor(sValue, r) }
+                // With a 1/2 chance, set `r` to near a random power of 2.
+                if iszero(and(2, d)) {
+                    // Set `t` either `not(0)` or `xor(sValue, r)`.
+                    let t := xor(not(0), mul(iszero(and(4, d)), not(xor(sValue, r))))
                     // Set `r` to `t` shifted left or right by a random multiple of 8.
-                    r := sub(shl(shl(3, and(byte(3, r), 31)), t), and(r, 3))
-                    // Negate `r` if `u` is zero.
-                    if iszero(u) { r := not(r) }
+                    switch and(8, d)
+                    case 0 {
+                        if iszero(and(16, d)) { t := 1 }
+                        r := add(shl(shl(3, and(byte(3, r), 31)), t), sub(and(r, 7), 3))
+                    }
+                    default {
+                        if iszero(and(16, d)) { t := shl(255, 1) }
+                        r := add(shr(shl(3, and(byte(3, r), 31)), t), sub(and(r, 7), 3))
+                    }
+                    // With a 1/2 chance, negate `r`.
+                    if iszero(and(32, d)) { r := not(r) }
                     break
                 }
                 // Otherwise, just set `r` to `xor(sValue, r)`.
