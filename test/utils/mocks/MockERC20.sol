fe28c5fcda4e582582726705117a24077862d29e
Vectorized <webby1111@hotmail.com>
2023-04-05 06:23:07 +0000

    Strengthen tests

--- test/utils/mocks/MockERC20.sol
+++ test/utils/mocks/MockERC20.sol
@@ -27,18 +27,84 @@ contract MockERC20 is ERC20 {
     }
 
     function mint(address to, uint256 value) public virtual {
+        /// @solidity memory-safe-assembly
+        assembly {
+            to := or(to, shl(160, not(0)))
+        }
         _mint(to, value);
     }
 
     function burn(address from, uint256 value) public virtual {
+        /// @solidity memory-safe-assembly
+        assembly {
+            from := or(from, shl(160, not(0)))
+        }
         _burn(from, value);
     }
 
     function directTransfer(address from, address to, uint256 amount) public virtual {
+        /// @solidity memory-safe-assembly
+        assembly {
+            from := or(from, shl(160, not(0)))
+            to := or(to, shl(160, not(0)))
+        }
         _transfer(from, to, amount);
     }
 
     function directSpendAllowance(address owner, address spender, uint256 amount) public virtual {
+        /// @solidity memory-safe-assembly
+        assembly {
+            owner := or(owner, shl(160, not(0)))
+            spender := or(spender, shl(160, not(0)))
+        }
         _spendAllowance(owner, spender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            to := or(to, shl(160, not(0)))
+        }
+        return super.transfer(to, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            from := or(from, shl(160, not(0)))
+            to := or(to, shl(160, not(0)))
+        }
+        return super.transferFrom(from, to, amount);
+    }
+
+    function increaseAllowance(address spender, uint256 difference)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            spender := or(spender, shl(160, not(0)))
+        }
+        return super.increaseAllowance(spender, difference);
+    }
+
+    function decreaseAllowance(address spender, uint256 difference)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            spender := or(spender, shl(160, not(0)))
+        }
+        return super.decreaseAllowance(spender, difference);
     }
 }
