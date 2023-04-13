a81f7b0f1c3c4e37906a3e78660710ee6a0872d1
Vectorized <webby1111@hotmail.com>
2023-04-13 12:59:04 +0000

    Add _getArgUint all the way

--- src/utils/Clone.sol
+++ src/utils/Clone.sol
@@ -31,16 +31,7 @@ abstract contract Clone {
         uint256 offset = _getImmutableArgsOffset();
         /// @solidity memory-safe-assembly
         assembly {
-            arg := shr(0x60, calldataload(add(offset, argOffset)))
-        }
-    }
-
-    /// @dev Reads an immutable arg with type uint256
-    function _getArgUint256(uint256 argOffset) internal pure returns (uint256 arg) {
-        uint256 offset = _getImmutableArgsOffset();
-        /// @solidity memory-safe-assembly
-        assembly {
-            arg := calldataload(add(offset, argOffset))
+            arg := shr(96, calldataload(add(offset, argOffset)))
         }
     }
 
@@ -64,12 +55,282 @@ abstract contract Clone {
         }
     }
 
-    /// @dev Reads an immutable arg with type uint64.
+    /// @dev Reads an immutable arg with type uint256.
+    function _getArgUint256(uint256 argOffset) internal pure returns (uint256 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := calldataload(add(offset, argOffset))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint248(uint256 argOffset) internal pure returns (uint248 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(8, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint240(uint256 argOffset) internal pure returns (uint240 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(16, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint232(uint256 argOffset) internal pure returns (uint232 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(24, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint224(uint256 argOffset) internal pure returns (uint224 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(32, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint216(uint256 argOffset) internal pure returns (uint216 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(40, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint208(uint256 argOffset) internal pure returns (uint208 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(48, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint200(uint256 argOffset) internal pure returns (uint200 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(56, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint192(uint256 argOffset) internal pure returns (uint192 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(64, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint184(uint256 argOffset) internal pure returns (uint184 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(72, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint176(uint256 argOffset) internal pure returns (uint176 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(80, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint168(uint256 argOffset) internal pure returns (uint168 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(88, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint160(uint256 argOffset) internal pure returns (uint160 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(96, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint152(uint256 argOffset) internal pure returns (uint152 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(104, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint144(uint256 argOffset) internal pure returns (uint144 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(112, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint136(uint256 argOffset) internal pure returns (uint136 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(120, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint128(uint256 argOffset) internal pure returns (uint128 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(128, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint120(uint256 argOffset) internal pure returns (uint120 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(136, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint112(uint256 argOffset) internal pure returns (uint112 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(144, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint104(uint256 argOffset) internal pure returns (uint104 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(152, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint96(uint256 argOffset) internal pure returns (uint96 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(160, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint88(uint256 argOffset) internal pure returns (uint88 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(168, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint80(uint256 argOffset) internal pure returns (uint80 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(176, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint72(uint256 argOffset) internal pure returns (uint72 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(184, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
     function _getArgUint64(uint256 argOffset) internal pure returns (uint64 arg) {
         uint256 offset = _getImmutableArgsOffset();
         /// @solidity memory-safe-assembly
         assembly {
-            arg := shr(0xc0, calldataload(add(offset, argOffset)))
+            arg := shr(192, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint56(uint256 argOffset) internal pure returns (uint56 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(200, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint48(uint256 argOffset) internal pure returns (uint48 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(208, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint40(uint256 argOffset) internal pure returns (uint40 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(216, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint32(uint256 argOffset) internal pure returns (uint32 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(224, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint24(uint256 argOffset) internal pure returns (uint24 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(232, calldataload(add(offset, argOffset)))
+        }
+    }
+
+    /// @dev Reads an immutable arg with type uint8.
+    function _getArgUint16(uint256 argOffset) internal pure returns (uint16 arg) {
+        uint256 offset = _getImmutableArgsOffset();
+        /// @solidity memory-safe-assembly
+        assembly {
+            arg := shr(240, calldataload(add(offset, argOffset)))
         }
     }
 
@@ -78,7 +339,7 @@ abstract contract Clone {
         uint256 offset = _getImmutableArgsOffset();
         /// @solidity memory-safe-assembly
         assembly {
-            arg := shr(0xf8, calldataload(add(offset, argOffset)))
+            arg := shr(248, calldataload(add(offset, argOffset)))
         }
     }
 
@@ -86,7 +347,7 @@ abstract contract Clone {
     function _getImmutableArgsOffset() internal pure returns (uint256 offset) {
         /// @solidity memory-safe-assembly
         assembly {
-            offset := sub(calldatasize(), shr(0xf0, calldataload(sub(calldatasize(), 2))))
+            offset := sub(calldatasize(), shr(240, calldataload(sub(calldatasize(), 2))))
         }
     }
 }
