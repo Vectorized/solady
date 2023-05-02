b6ec676b5e958f5b002e7e652cab33f27c6deffb
boredretard.eth <clandestine.eth@proton.me>
2023-05-02 17:06:58 -0500

    🥢 Fix natspec + var names
    - added clearer natpsec using `Base64.sol` as a reference
    - renamed str (input) -> data and r (output) -> return

--- src/utils/LibRLE.sol
+++ src/utils/LibRLE.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8.4;
 
 /// @notice Library for the Run-length encoding.
-/// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibString.sol)
+/// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibRLE.sol)
 library LibRLE {
-    /// @dev Returns encoded into RLE of the given bytes
+    /// @dev Encodes `data` using run-length encoding.
     /// See: https://en.wikipedia.org/wiki/Run-length_encoding
-    function encode(bytes memory str) internal pure returns (bytes memory r) {
+    function encode(bytes memory data) internal pure returns (bytes memory result) {
         /// @solidity memory-safe-assembly
         assembly {
-            if mload(str) {
-                let strEnd := add(str, add(0x20, mload(str)))
+            if mload(data) {
+                let dataEnd := add(data, add(0x20, mload(data)))
                 let count := 1 // counting for the char
-                r := mload(0x40) // free-memory pointer
-                let p := add(r, 0x20)
+                result := mload(0x40) // free-memory pointer
+                let p := add(result, 0x20)
 
-                let prevChar := byte(0, mload(add(str, 0x20)))
+                let prevChar := byte(0, mload(add(data, 0x20)))
 
-                for { let i := add(str, 0x20) } 1 {} {
+                for { let i := add(data, 0x20) } 1 {} {
                     i := add(i, 1)
 
                     let currChar := byte(0, mload(i))
@@ -32,35 +32,39 @@ library LibRLE {
 
                     p := add(p, 2)
 
-                    if iszero(lt(i, strEnd)) {
-                        mstore8(sub(p, 2), sub(count, sub(i, strEnd)))
+                    if iszero(lt(i, dataEnd)) {
+                        mstore8(sub(p, 2), sub(count, sub(i, dataEnd)))
                         break
                     }
 
                     count := 1
                     prevChar := currChar
                 }
-                // store length of the `r`
-                mstore(r, sub(sub(p, 0x20), r))
+                // store length of the `result`
+                mstore(result, sub(sub(p, 0x20), result))
                 // updates free memory pointer
                 mstore(0x40, shl(5, shr(5, add(p, 31))))
             }
         }
     }
 
-    /// @dev Returns decoded bytes encoded using {encode}.
-    /// Note: If `str` is not output of {encode}, the output behavious is undenfined.
-    function decode(bytes memory str) internal pure returns (bytes memory r) {
+    /// @dev Decodes RLE encoded `data`.
+    /// Note: For performance reasons,
+    /// this function will NOT revert on invalid `data` inputs.
+    /// Outputs for invalid inputs will simply be undefined behaviour.
+    /// It is the user's responsibility to ensure that the `data`
+    /// is a valid RLE encoded string.
+    function decode(bytes memory data) internal pure returns (bytes memory result) {
         /// @solidity memory-safe-assembly
         assembly {
-            if mload(str) {
-                let strEnd := add(str, add(0x20, mload(str)))
+            if mload(data) {
+                let dataEnd := add(data, add(0x20, mload(data)))
 
-                r := mload(0x40)
+                result := mload(0x40)
 
-                let p := add(r, 0x20)
+                let p := add(result, 0x20)
 
-                for { let i := add(str, 0x20) } 1 {} {
+                for { let i := add(data, 0x20) } 1 {} {
                     let n := byte(0, mload(i))
                     let char := byte(1, mload(i))
 
@@ -73,10 +77,10 @@ library LibRLE {
 
                     i := add(i, 2)
 
-                    if iszero(lt(i, strEnd)) { break }
+                    if iszero(lt(i, dataEnd)) { break }
                 }
-                // store length of the `r`
-                mstore(r, sub(sub(p, 0x20), r))
+                // store length of the `result`
+                mstore(result, sub(sub(p, 0x20), result))
                 // updates free memory pointer
                 mstore(0x40, shl(5, shr(5, add(p, 31))))
             }
