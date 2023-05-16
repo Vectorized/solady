f12500816ec56858f2da731420cab03a633ccfb3
Vectorized <webby1111@hotmail.com>
2023-05-16 21:56:15 +0800

    ✨ FastLZ (#416)

--- src/utils/LibZip.sol
+++ src/utils/LibZip.sol
@@ -4,7 +4,154 @@ pragma solidity ^0.8.4;
 /// @notice Library for compressing and decompressing bytes.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibZip.sol)
 /// @author Calldata compression by clabby (https://github.com/clabby/op-kompressor)
+/// @author FastLZ by ariya (https://github.com/ariya/FastLZ)
 library LibZip {
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     FAST LZ OPERATIONS                     */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Returns the compressed `data`.
+    function flzCompress(bytes memory data) internal pure returns (bytes memory result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            function ms8(d_, v_) -> _r {
+                mstore8(d_, v_)
+                _r := add(d_, 1)
+            }
+            function u24(p_) -> _r {
+                let w := mload(p_)
+                _r := or(shl(16, byte(2, w)), or(shl(8, byte(1, w)), byte(0, w)))
+            }
+            function cmp(p_, q_, r_) -> _r {
+                _r := p_
+                for { let t := 0 } and(lt(q_, r_), iszero(t)) {} {
+                    t := byte(0, xor(mload(p_), mload(q_)))
+                    p_ := add(p_, 1)
+                    q_ := add(q_, 1)
+                }
+                _r := sub(p_, _r)
+            }
+            function literals(runs_, src_, dest_) -> _r {
+                for { _r := dest_ } iszero(lt(runs_, 0x20)) { runs_ := sub(runs_, 0x20) } {
+                    mstore(ms8(_r, 31), mload(src_))
+                    _r := add(_r, 0x21)
+                    src_ := add(src_, 0x20)
+                }
+                if iszero(runs_) { leave }
+                mstore(ms8(_r, sub(runs_, 1)), mload(src_))
+                _r := add(1, add(_r, runs_))
+            }
+            function match(l_, d_, o_) -> _r {
+                for { d_ := sub(d_, 1) } iszero(lt(l_, 263)) { l_ := sub(l_, 262) } {
+                    o_ := ms8(ms8(ms8(o_, add(224, shr(8, d_))), 253), and(0xff, d_))
+                }
+                if iszero(lt(l_, 7)) {
+                    _r := ms8(ms8(ms8(o_, add(224, shr(8, d_))), sub(l_, 7)), and(0xff, d_))
+                    leave
+                }
+                _r := ms8(ms8(o_, add(shl(5, l_), shr(8, d_))), and(0xff, d_))
+            }
+            function setHash(i_, v_) {
+                let p := add(mload(0x40), shl(2, i_))
+                mstore(p, xor(mload(p), shl(224, xor(shr(224, mload(p)), v_))))
+            }
+            function hash(v_) -> _r {
+                _r := and(shr(19, mul(2654435769, v_)), 0x1fff)
+            }
+            codecopy(mload(0x40), codesize(), 0x8000) // Zeroize the hashmap.
+            let ip := add(data, 0x20)
+            let op := add(mload(0x40), 0x8000)
+            let a := ip
+            let ipStart := add(data, 0x20)
+            let ipLimit := sub(add(ipStart, mload(data)), 13)
+            for { ip := add(2, ip) } lt(ip, ipLimit) {} {
+                let r := 0
+                let d := 0
+                for {} 1 {} {
+                    let s := u24(ip)
+                    let h := hash(s)
+                    r := add(ipStart, shr(224, mload(add(mload(0x40), shl(2, h)))))
+                    setHash(h, sub(ip, ipStart))
+                    d := sub(ip, r)
+                    if iszero(lt(ip, ipLimit)) { break }
+                    ip := add(ip, 1)
+                    if iszero(gt(d, 0x1fff)) { if eq(s, u24(r)) { break } }
+                }
+                if iszero(lt(ip, ipLimit)) { break }
+                ip := sub(ip, 1)
+                if gt(ip, a) { op := literals(sub(ip, a), a, op) }
+                let l := cmp(add(r, 3), add(ip, 3), add(ipLimit, 9))
+                op := match(l, d, op)
+                ip := add(ip, l)
+                setHash(hash(u24(ip)), sub(ip, ipStart))
+                ip := add(ip, 1)
+                setHash(hash(u24(ip)), sub(ip, ipStart))
+                ip := add(ip, 1)
+                a := ip
+            }
+            op := literals(sub(add(ipStart, mload(data)), a), a, op)
+            result := mload(0x40)
+            let t := add(mload(0x40), 0x8000)
+            let n := sub(op, t)
+            mstore(result, n) // Store the length.
+            // Copy the result to compact the memory, overwriting the hashmap.
+            for { let i := 0 } lt(i, n) { i := add(i, 0x20) } {
+                mstore(add(add(result, 0x20), i), mload(add(t, i)))
+            }
+            let last := add(add(result, 0x20), n)
+            mstore(last, 0) // Zeroize the slot after the string.
+            mstore(0x40, and(add(last, 0x3f), not(0x1f))) // Allocate the memory.
+        }
+    }
+
+    /// @dev Returns the decompressed `data`.
+    function flzDecompress(bytes memory data) internal pure returns (bytes memory result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            function append(output_, dest_, ofs_, len_) {
+                let r := add(output_, sub(dest_, add(ofs_, 0x20)))
+                let o := add(output_, dest_)
+                for { let j := 0 } iszero(eq(j, len_)) { j := add(j, 1) } {
+                    mstore8(add(o, j), mload(add(r, j)))
+                }
+            }
+            let dest := 0
+            let end := add(add(data, 0x20), mload(data))
+            data := add(data, 0x20)
+            result := mload(0x40)
+            let output := add(result, 0x20)
+            for {} lt(data, end) {} {
+                let srcWord := mload(data)
+                let srcValue := byte(0, srcWord)
+                let t := shr(5, srcValue)
+                if iszero(t) {
+                    data := add(data, 1)
+                    mstore(add(output, dest), mload(data))
+                    data := add(data, add(1, srcValue))
+                    dest := add(dest, add(1, srcValue))
+                    continue
+                }
+                if iszero(lt(t, 7)) {
+                    let ofs := add(shl(8, and(31, srcValue)), byte(2, srcWord))
+                    let len := add(9, byte(1, srcWord))
+                    data := add(data, 3)
+                    append(output, dest, ofs, len)
+                    dest := add(dest, len)
+                    continue
+                }
+                let ofs := add(shl(8, and(31, srcValue)), byte(1, srcWord))
+                let len := add(2, t)
+                data := add(data, 2)
+                append(output, dest, ofs, len)
+                dest := add(dest, len)
+            }
+            mstore(result, dest) // Store the length.
+            let o := add(add(result, 0x20), dest)
+            mstore(o, 0) // Zeroize the slot after the string.
+            mstore(0x40, and(add(o, 0x3f), not(0x1f))) // Allocate the memory.
+        }
+    }
+
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                    CALLDATA OPERATIONS                     */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
