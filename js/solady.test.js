803664c16ef4805a3b96a0d67aa28ca232e37ac7
Vectorized <webby1111@hotmail.com>
2023-05-18 15:47:36 +0000

    Add test

--- js/solady.test.js
+++ js/solady.test.js
@@ -20,11 +20,9 @@ function assertEq(a, b) {
 }
 
 test("Calldata compress / decompress.", function () {
-    var LibZip = solady.LibZip;
-
     function randomData() {
         var n = ~~(Math.random() * 2000);
-        var s = "0x";
+        var s = Math.random() < 0.5 ? "" : "0x";
         var g = Math.random() < 0.5 ? 0.45 : 0.99;
         var h = Math.random() < 0.5 ? 0.90 : 0.9999;
         for (var i = 0; i < n; ++i) {
@@ -38,13 +36,28 @@ test("Calldata compress / decompress.", function () {
                 s += b.length === 1 ? "0" + b : b;
             }
         }
-        return s;
+        return Math.random() < 0.5 ? s.toUpperCase() : s.toLowerCase();
     }
 
     for (var t = 0; t < 10000; ++t) {
         var data = randomData();
-        var compressed = LibZip.cdCompress(data);
-        var decompressed = LibZip.cdDecompress(compressed);
-        assertEq(decompressed, data);
+        var compressed = solady.LibZip.cdCompress(data);
+        var decompressed = solady.LibZip.cdDecompress(compressed);
+        assertEq(compressed.slice(0, 2), "0x");
+        assertEq(decompressed.slice(0, 2), "0x");
+        assertEq(decompressed.replace(/^0x/, ""), data.toLowerCase().replace(/^0x/, ""));
     }
+});
+
+test("ERC1967Factory ABI and address.", function () {
+    function hashFnv32a(s) {
+        var h = 0x811c9dc5;
+        for (var i = 0; i < s.length; i++) {
+            h ^= s.charCodeAt(i);
+            h += (h << 1) + (h << 4) + (h << 7) + (h << 8) + (h << 24);
+        }
+        return h >>> 0;
+    }
+    assertEq(hashFnv32a(JSON.stringify(solady.ERC1967Factory.abi)), 1277805820);
+    assertEq(solady.ERC1967Factory.address, "0x0000000000006396FF2a80c067f99B3d2Ab4Df24");
 });
