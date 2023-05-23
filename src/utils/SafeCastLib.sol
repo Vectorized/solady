3c37afa826978e2c6a1fcc3008482fc016200f8c
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2023-05-23 15:20:44 -0600

    fmt

--- src/utils/SafeCastLib.sol
+++ src/utils/SafeCastLib.sol
@@ -16,142 +16,142 @@ library SafeCastLib {
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     function toUint8(uint256 x) internal pure returns (uint8) {
-        if (!(x< 1 << 8)) _revertOverflow();
+        if (!(x < 1 << 8)) _revertOverflow();
         return uint8(x);
     }
 
     function toUint16(uint256 x) internal pure returns (uint16) {
-        if (!(x< 1 << 16)) _revertOverflow();
+        if (!(x < 1 << 16)) _revertOverflow();
         return uint16(x);
     }
 
     function toUint24(uint256 x) internal pure returns (uint24) {
-        if (!(x< 1 << 24)) _revertOverflow();
+        if (!(x < 1 << 24)) _revertOverflow();
         return uint24(x);
     }
 
     function toUint32(uint256 x) internal pure returns (uint32) {
-        if (!(x< 1 << 32)) _revertOverflow();
+        if (!(x < 1 << 32)) _revertOverflow();
         return uint32(x);
     }
 
     function toUint40(uint256 x) internal pure returns (uint40) {
-        if (!(x< 1 << 40)) _revertOverflow();
+        if (!(x < 1 << 40)) _revertOverflow();
         return uint40(x);
     }
 
     function toUint48(uint256 x) internal pure returns (uint48) {
-        if (!(x< 1 << 48)) _revertOverflow();
+        if (!(x < 1 << 48)) _revertOverflow();
         return uint48(x);
     }
 
     function toUint56(uint256 x) internal pure returns (uint56) {
-        if (!(x< 1 << 56)) _revertOverflow();
+        if (!(x < 1 << 56)) _revertOverflow();
         return uint56(x);
     }
 
     function toUint64(uint256 x) internal pure returns (uint64) {
-        if (!(x< 1 << 64)) _revertOverflow();
+        if (!(x < 1 << 64)) _revertOverflow();
         return uint64(x);
     }
 
     function toUint72(uint256 x) internal pure returns (uint72) {
-        if (!(x< 1 << 72)) _revertOverflow();
+        if (!(x < 1 << 72)) _revertOverflow();
         return uint72(x);
     }
 
     function toUint80(uint256 x) internal pure returns (uint80) {
-        if (!(x< 1 << 80)) _revertOverflow();
+        if (!(x < 1 << 80)) _revertOverflow();
         return uint80(x);
     }
 
     function toUint88(uint256 x) internal pure returns (uint88) {
-        if (!(x< 1 << 88)) _revertOverflow();
+        if (!(x < 1 << 88)) _revertOverflow();
         return uint88(x);
     }
 
     function toUint96(uint256 x) internal pure returns (uint96) {
-        if (!(x< 1 << 96)) _revertOverflow();
+        if (!(x < 1 << 96)) _revertOverflow();
         return uint96(x);
     }
 
     function toUint104(uint256 x) internal pure returns (uint104) {
-        if (!(x< 1 << 104)) _revertOverflow();
+        if (!(x < 1 << 104)) _revertOverflow();
         return uint104(x);
     }
 
     function toUint112(uint256 x) internal pure returns (uint112) {
-        if (!(x< 1 << 112)) _revertOverflow();
+        if (!(x < 1 << 112)) _revertOverflow();
         return uint112(x);
     }
 
     function toUint120(uint256 x) internal pure returns (uint120) {
-        if (!(x< 1 << 120)) _revertOverflow();
+        if (!(x < 1 << 120)) _revertOverflow();
         return uint120(x);
     }
 
     function toUint128(uint256 x) internal pure returns (uint128) {
-        if (!(x< 1 << 128)) _revertOverflow();
+        if (!(x < 1 << 128)) _revertOverflow();
         return uint128(x);
     }
 
     function toUint136(uint256 x) internal pure returns (uint136) {
-        if (!(x< 1 << 136)) _revertOverflow();
+        if (!(x < 1 << 136)) _revertOverflow();
         return uint136(x);
     }
 
     function toUint144(uint256 x) internal pure returns (uint144) {
-        if (!(x< 1 << 144)) _revertOverflow();
+        if (!(x < 1 << 144)) _revertOverflow();
         return uint144(x);
     }
 
     function toUint152(uint256 x) internal pure returns (uint152) {
-        if (!(x< 1 << 152)) _revertOverflow();
+        if (!(x < 1 << 152)) _revertOverflow();
         return uint152(x);
     }
 
     function toUint160(uint256 x) internal pure returns (uint160) {
-        if (!(x< 1 << 160)) _revertOverflow();
+        if (!(x < 1 << 160)) _revertOverflow();
         return uint160(x);
     }
 
     function toUint168(uint256 x) internal pure returns (uint168) {
-        if (!(x< 1 << 168)) _revertOverflow();
+        if (!(x < 1 << 168)) _revertOverflow();
         return uint168(x);
     }
 
     function toUint176(uint256 x) internal pure returns (uint176) {
-        if (!(x< 1 << 176)) _revertOverflow();
+        if (!(x < 1 << 176)) _revertOverflow();
         return uint176(x);
     }
 
     function toUint184(uint256 x) internal pure returns (uint184) {
-        if (!(x< 1 << 184)) _revertOverflow();
+        if (!(x < 1 << 184)) _revertOverflow();
         return uint184(x);
     }
 
     function toUint192(uint256 x) internal pure returns (uint192) {
-        if (!(x< 1 << 192)) _revertOverflow();
+        if (!(x < 1 << 192)) _revertOverflow();
         return uint192(x);
     }
 
     function toUint200(uint256 x) internal pure returns (uint200) {
-        if (!(x< 1 << 200)) _revertOverflow();
+        if (!(x < 1 << 200)) _revertOverflow();
         return uint200(x);
     }
 
     function toUint208(uint256 x) internal pure returns (uint208) {
-        if (!(x< 1 << 208)) _revertOverflow();
+        if (!(x < 1 << 208)) _revertOverflow();
         return uint208(x);
     }
 
     function toUint216(uint256 x) internal pure returns (uint216) {
-        if (!(x< 1 << 216)) _revertOverflow();
+        if (!(x < 1 << 216)) _revertOverflow();
         return uint216(x);
     }
 
     function toUint224(uint256 x) internal pure returns (uint224) {
-        if (!(x< 1 << 224)) _revertOverflow();
+        if (!(x < 1 << 224)) _revertOverflow();
         return uint224(x);
     }
 
@@ -161,7 +161,7 @@ library SafeCastLib {
     }
 
     function toUint240(uint256 x) internal pure returns (uint240) {
-        if (! (x< 1 << 240)) _revertOverflow();
+        if (!(x < 1 << 240)) _revertOverflow();
         return uint240(x);
     }
 
