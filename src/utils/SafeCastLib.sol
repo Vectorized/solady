b2523926e3f791015250d09e16131f92babbe601
Vectorized <webby1111@hotmail.com>
2023-05-27 22:53:19 +0000

    🥢 SafeCastLib new header

--- src/utils/SafeCastLib.sol
+++ src/utils/SafeCastLib.sol
@@ -360,6 +360,10 @@ library SafeCastLib {
         return y;
     }
 
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*         UNSIGNED TO SIGNED SAFE CASTING OPERATIONS         */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
     function toInt256(uint256 x) internal pure returns (int256) {
         if (x >= 1 << 255) _revertOverflow();
         return int256(x);
