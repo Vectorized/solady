0d10e692f948c1b32059369b2cd23d01fba3f1e1
Vectorized <webby1111@hotmail.com>
2023-06-07 04:41:38 +0800

    ♻️ Checksumed -> Checksummed (#461)
    Co-authored-by: 0xfoobar <0xfoobar1@gmail.com>

--- test/LibString.t.sol
+++ test/LibString.t.sol
@@ -184,45 +184,45 @@ contract LibStringTest is SoladyTest {
         );
     }
 
-    function testFromAddressToHexStringChecksumed() public {
+    function testFromAddressToHexStringChecksummed() public {
         // All caps.
         assertEq(
-            LibString.toHexStringChecksumed(0x52908400098527886E0F7030069857D2E4169EE7),
+            LibString.toHexStringChecksummed(0x52908400098527886E0F7030069857D2E4169EE7),
             "0x52908400098527886E0F7030069857D2E4169EE7"
         );
         assertEq(
-            LibString.toHexStringChecksumed(0x8617E340B3D01FA5F11F306F4090FD50E238070D),
+            LibString.toHexStringChecksummed(0x8617E340B3D01FA5F11F306F4090FD50E238070D),
             "0x8617E340B3D01FA5F11F306F4090FD50E238070D"
         );
         // All lower.
         assertEq(
-            LibString.toHexStringChecksumed(0xde709f2102306220921060314715629080e2fb77),
+            LibString.toHexStringChecksummed(0xde709f2102306220921060314715629080e2fb77),
             "0xde709f2102306220921060314715629080e2fb77"
         );
         assertEq(
-            LibString.toHexStringChecksumed(0x27b1fdb04752bbc536007a920d24acb045561c26),
+            LibString.toHexStringChecksummed(0x27b1fdb04752bbc536007a920d24acb045561c26),
             "0x27b1fdb04752bbc536007a920d24acb045561c26"
         );
         // Normal.
         assertEq(
-            LibString.toHexStringChecksumed(0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed),
+            LibString.toHexStringChecksummed(0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed),
             "0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed"
         );
         assertEq(
-            LibString.toHexStringChecksumed(0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359),
+            LibString.toHexStringChecksummed(0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359),
             "0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359"
         );
         assertEq(
-            LibString.toHexStringChecksumed(0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB),
+            LibString.toHexStringChecksummed(0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB),
             "0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB"
         );
         assertEq(
-            LibString.toHexStringChecksumed(0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb),
+            LibString.toHexStringChecksummed(0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb),
             "0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb"
         );
     }
 
-    function testFromAddressToHexStringChecksumedDifferential(uint256 randomness)
+    function testFromAddressToHexStringChecksummedDifferential(uint256 randomness)
         public
         brutalizeMemory
     {
@@ -250,9 +250,9 @@ contract LibStringTest is SoladyTest {
                 }
             }
         }
-        string memory checksumed = LibString.toHexStringChecksumed(r);
-        _checkMemory(checksumed);
-        assertEq(checksumed, expectedResult);
+        string memory checksummed = LibString.toHexStringChecksummed(r);
+        _checkMemory(checksummed);
+        assertEq(checksummed, expectedResult);
     }
 
     function testHexStringNoPrefixVariants(uint256 x, uint256 randomness) public brutalizeMemory {
