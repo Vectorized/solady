79133427b07be1e6a79b7296ed92d2ccb058f193
Vectorized <webby1111@hotmail.com>
2023-06-09 12:46:56 +0800

    ✨ Add FastLZ operations to JS (#468)

--- js/solady.js
+++ js/solady.js
@@ -18,7 +18,7 @@
     var solady = {};
 
     /*============================================================*/
-    /*                 LibZip Calldata Operations                 */
+    /*                     LibZip Operations                      */
     /*============================================================*/
 
     // See: (src/utils/LibZip.sol)
@@ -43,6 +43,83 @@
         return (b | 0x100).toString(16).slice(1);
     }
 
+    function parseByte(data, i) {
+        return parseInt(data.substr(i, 2), 16);
+    }
+
+    function hexToBytes(data) {
+        var a = [], i = 0;
+        for (; i < data.length; i += 2) a.push(parseByte(data, i));
+        return a;
+    }
+
+    function bytesToHex(a) {
+        var o = "0x", i = 0;
+        for (; i < a.length; o += byteToString(a[i++])) ;
+        return o;
+    }
+
+    /**
+     * Compresses hex encoded data with the FastLZ LZ77 algorithm.
+     * @param {string} data A hex encoded string representing the original data.
+     * @returns {string} The compressed result as a hex encoded string.
+     */
+    LibZip.flzCompress = function(data) {
+        var ib = hexToBytes(hexString(data)), b = ib.length - 4;
+        var ht = [], ob = [], a = 0, i = 2, o = 0, j, s, h, d, c, l, r, p, q, e, m = 0xffffff;
+
+        function u32(i) {
+            return ib[i] | (ib[++i] << 8) | (ib[++i] << 16) | (ib[++i] << 24);
+        }
+
+        function hash(x) {
+            return ((2654435769 * x) >> 19) & 8191;
+        }
+
+        function literals(r, s) {
+            while (r >= 32) for (ob[o++] = 31, j = 32; j--; r--) ob[o++] = ib[s++];
+            if (r) for (ob[o++] = r - 1; r--; ) ob[o++] = ib[s++];
+        }
+
+        while (i < b - 9) {
+            do {
+                r = ht[h = hash(s = u32(i) & m)];
+                c = (d = (ht[h] = i) - r) < 8192 ? u32(r) & m : m + 1;
+            } while (i < b - 9 && i++ && s != c);
+            if (i >= b - 9) break;
+            if (--i > a) literals(i - a, a);
+            for (l = 0, p = r + 3, q = i + 3, e = b - q; l < e; l++) e *= ib[p + l] === ib[q + l];
+            s = u32(i += l);
+            for (--d; l > 262; l -= 262) ob[o++] = 224 + (d >> 8), ob[o++] = 253, ob[o++] = d & 255;
+            if (l < 7) ob[o++] = (l << 5) + (d >> 8), ob[o++] = d & 255;
+            else ob[o++] = 224 + (d >> 8), ob[o++] = l - 7, ob[o++] = d & 255;
+            ht[hash(s & m)] = i++, ht[hash(s >> 8)] = i++, a = i;
+        }
+        literals(b + 4 - a, a);
+        return bytesToHex(ob);
+    }
+
+    /**
+     * Decompresses hex encoded data with the FastLZ LZ77 algorithm.
+     * @param {string} data A hex encoded string representing the compressed data.
+     * @returns {string} The decompressed result as a hex encoded string.
+     */
+    LibZip.flzDecompress = function(data) {
+        var ib = hexToBytes(hexString(data)), i = 0, o = 0, l, f, t, r, h, ob = [];
+        while (i < ib.length) {
+            if (!(t = ib[i] >> 5)) {
+                for (l = 1 + ib[i++]; l--;) ob[o++] = ib[i++];
+            } else {
+                f = 256 * (ib[i] & 31) + ib[i + 2 - (t = t < 7)];
+                l = t ? 2 + (ib[i] >> 5) : 9 + ib[i + 1];
+                i = i + 3 - t;
+                r = o - f - 1;
+                while (l--) ob[o++] = ob[r++];
+            }
+        }
+        return bytesToHex(ob);
+    }
+
     /**
      * Compresses hex encoded calldata.
      * @param {string} data A hex encoded string representing the original data.
@@ -62,8 +139,8 @@
         }
 
         for (; i < data.length; i += 2) {
-            c = parseInt(data.slice(i, i + 2), 16);
-            if (c === 0x00) {
+            c = parseByte(data, i);
+            if (!c) {
                 if (y) rle(1, y), y = 0;
                 if (++z === 0x80) rle(0, 0x80), z = 0;
                 continue;
@@ -91,15 +168,11 @@
         data = hexString(data);
         var o = "0x", i = 0, c, s;
 
-        function getByte(j) {
-            return ((j < 4 * 2) * 0xff) ^ parseInt(data.slice(j, j + 2), 16);
-        }
-
         while (i < data.length) {
-            c = getByte(i);
+            c = ((i < 4 * 2) * 0xff) ^ parseByte(data, i);
             i += 2;
-            if (c === 0x00) {
-                c = getByte(i);
+            if (!c) {
+                c = ((i < 4 * 2) * 0xff) ^ parseByte(data, i);
                 s = (c & 0x7f) + 1;
                 i += 2;
                 while (s--) o += byteToString((c >> 7) * 0xff);
--- js/solady.test.js
+++ js/solady.test.js
@@ -26,40 +26,40 @@ function expectRevert(fn) {
     assert(hasRevert, "Revert expected.\n" + fn);
 }
 
-test("Calldata compress / decompress.", function() {
-    function randomData() {
-        var n = ~~(Math.random() * 2000);
-        var s = Math.random() < 0.5 ? "" : "0x";
-        var g = Math.random() < 0.5 ? 0.45 : (Math.random() ? 0.99 : 0.999);
-        var h = g + 0.5 * (1.0 - g);
-        for (var i = 0; i < n; ++i) {
-            var r = Math.random();
-            if (r < g) {
-                s += "00";
-            } else if (r < h) {
-                s += "ff";
-            } else {
-                var b = ((Math.random() * 0x100) & 0xff).toString(16);
-                s += b.length === 1 ? "0" + b : b;
-            }
+function randomData() {
+    var n = ~~(Math.random() * 2000);
+    var s = Math.random() < 0.5 ? "" : "0x";
+    var g = Math.random() < 0.5 ? 0.45 : (Math.random() ? 0.99 : 0.999);
+    var h = g + 0.5 * (1.0 - g);
+    for (var i = 0; i < n; ++i) {
+        var r = Math.random();
+        if (r < g) {
+            s += "00";
+        } else if (r < h) {
+            s += "ff";
+        } else {
+            var b = ((Math.random() * 0x100) & 0xff).toString(16);
+            s += b.length === 1 ? "0" + b : b;
         }
-        return Math.random() < 0.5 ? s.toUpperCase() : s.toLowerCase();
     }
+    return Math.random() < 0.5 ? s.toUpperCase() : s.toLowerCase();
+}
 
-    function padRandomWhitespace(data) {
-        var before = "";
-        var after = "";
-        while (Math.random() < 0.5) before += Math.random() ? "\t" : " ";
-        while (Math.random() < 0.5) after += Math.random() ? "\t" : " ";
-        return before + data + after;
-    }
+function padRandomWhitespace(data) {
+    var before = "";
+    var after = "";
+    while (Math.random() < 0.5) before += Math.random() ? "\t" : " ";
+    while (Math.random() < 0.5) after += Math.random() ? "\t" : " ";
+    return before + data + after;
+}
 
+function testCompressDecompress(compress, decompress) {
     var totalDataLength = 0;
     var totalCompressedLength = 0;
-    for (var t = 0; t < 10000; ++t) {
+    for (var t = 0; t < 1000; ++t) {
         var data = randomData();
-        var compressed = solady.LibZip.cdCompress(padRandomWhitespace(data));
-        var decompressed = solady.LibZip.cdDecompress(padRandomWhitespace(compressed));
+        var compressed = compress(padRandomWhitespace(data));
+        var decompressed = decompress(padRandomWhitespace(compressed));
         totalDataLength += data.length;
         totalCompressedLength += compressed.length;
         assertEq(compressed.slice(0, 2), "0x");
@@ -68,10 +68,10 @@ test("Calldata compress / decompress.", function() {
     }
     assert(totalCompressedLength < totalDataLength, "Compress not working as intended.");
 
-    assertEq(solady.LibZip.cdCompress(""), "0x");
-    assertEq(solady.LibZip.cdCompress("0x"), "0x");
-    assertEq(solady.LibZip.cdDecompress(""), "0x");
-    assertEq(solady.LibZip.cdDecompress("0x"), "0x");
+    assertEq(compress(""), "0x");
+    assertEq(compress("0x"), "0x");
+    assertEq(decompress(""), "0x");
+    assertEq(decompress("0x"), "0x");
 
     function checkRevertOnInvalidInputs(fn) {
         expectRevert(function () { fn("hehe") });
@@ -80,19 +80,30 @@ test("Calldata compress / decompress.", function() {
         expectRevert(function () { fn(123) });
         expectRevert(function () { fn(false) });
         expectRevert(function () { fn(null) });
+        expectRevert(function () { fn(undefined) });
+        expectRevert(function () { fn([]) });
+        expectRevert(function () { fn({}) });
     }
 
-    checkRevertOnInvalidInputs(solady.LibZip.cdCompress);
-    checkRevertOnInvalidInputs(solady.LibZip.cdDecompress);
+    checkRevertOnInvalidInputs(compress);
+    checkRevertOnInvalidInputs(decompress);
+}
+
+test("LibZip: FastLZ compress / decompress.", function() {
+    testCompressDecompress(solady.LibZip.flzCompress, solady.LibZip.flzDecompress);
 });
 
-test("Calldata compress", function() {
+test("LibZip: Calldata compress / decompress.", function() {
+    testCompressDecompress(solady.LibZip.cdCompress, solady.LibZip.cdDecompress);
+});
+
+test("LibZip: Calldata compress", function() {
     var data = "0xac9650d80000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000160000000000000000000000000000000000000000000000000000000000000022000000000000000000000000000000000000000000000000000000000000002a000000000000000000000000000000000000000000000000000000000000000a40c49ccbe000000000000000000000000000000000000000000000000000000000005b70e00000000000000000000000000000000000000000000000000000dfc79825feb0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000645c48a7000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000084fc6f7865000000000000000000000000000000000000000000000000000000000005b70e000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000ffffffffffffffffffffffffffffffff00000000000000000000000000000000ffffffffffffffffffffffffffffffff00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004449404b7c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000001f1cdf1a632eaaab40d1c263edf49faf749010a1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000064df2ab5bb0000000000000000000000007f5c764cbc14f9669b88837ca1490cca17c3160700000000000000000000000000000000000000000000000000000000000000000000000000000000000000001f1cdf1a632eaaab40d1c263edf49faf749010a100000000000000000000000000000000000000000000000000000000";
     var expected = "0x5369af27001e20001e04001e80001d0160001d0220001d02a0001ea40c49ccbe001c05b70e00190dfc79825feb005b645c48a7003a84fc6f7865001c05b70e002f008f000f008f003a4449404b7c002b1f1cdf1a632eaaab40d1c263edf49faf749010a1003a64df2ab5bb000b7f5c764cbc14f9669b88837ca1490cca17c31607002b1f1cdf1a632eaaab40d1c263edf49faf749010a1001b";
     assertEq(solady.LibZip.cdCompress(data), expected);
 });
 
-test("ERC1967Factory ABI and address", function() {
+test("ERC1967Factory: ABI and address", function() {
     function hashFnv32a(s) {
         var h = 0x811c9dc5;
         for (var i = 0; i < s.length; i++) {
