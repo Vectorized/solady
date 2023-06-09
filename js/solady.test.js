002ed04dd0a28c361b1cee902dfd9ebc035b249c
Vectorized <webby1111@hotmail.com>
2023-06-09 04:24:11 +0000

    Update JS library with FastLZ

--- js/solady.test.js
+++ js/solady.test.js
@@ -26,34 +26,45 @@ function expectRevert(fn) {
     assert(hasRevert, "Revert expected.\n" + fn);
 }
 
-test("Calldata compress / decompress.", function() {
-    function randomData() {
-        var n = ~~(Math.random() * 2000);
-        var s = Math.random() < 0.5 ? "" : "0x";
-        var g = Math.random() < 0.5 ? 0.45 : (Math.random() ? 0.99 : 0.999);
-        var h = g + 0.5 * (1.0 - g);
-        for (var i = 0; i < n; ++i) {
-            var r = Math.random();
-            if (r < g) {
-                s += "00";
-            } else if (r < h) {
-                s += "ff";
-            } else {
-                var b = ((Math.random() * 0x100) & 0xff).toString(16);
-                s += b.length === 1 ? "0" + b : b;
-            }
+function randomData() {
+    var n = ~~(Math.random() * 2000);
+    var s = Math.random() < 0.5 ? "" : "0x";
+    var g = Math.random() < 0.5 ? 0.45 : (Math.random() ? 0.99 : 0.999);
+    var h = g + 0.5 * (1.0 - g);
+    for (var i = 0; i < n; ++i) {
+        var r = Math.random();
+        if (r < g) {
+            s += "00";
+        } else if (r < h) {
+            s += "ff";
+        } else {
+            var b = ((Math.random() * 0x100) & 0xff).toString(16);
+            s += b.length === 1 ? "0" + b : b;
         }
-        return Math.random() < 0.5 ? s.toUpperCase() : s.toLowerCase();
     }
+    return Math.random() < 0.5 ? s.toUpperCase() : s.toLowerCase();
+}
 
-    function padRandomWhitespace(data) {
-        var before = "";
-        var after = "";
-        while (Math.random() < 0.5) before += Math.random() ? "\t" : " ";
-        while (Math.random() < 0.5) after += Math.random() ? "\t" : " ";
-        return before + data + after;
+function padRandomWhitespace(data) {
+    var before = "";
+    var after = "";
+    while (Math.random() < 0.5) before += Math.random() ? "\t" : " ";
+    while (Math.random() < 0.5) after += Math.random() ? "\t" : " ";
+    return before + data + after;
+}
+
+test("LibZip: FastLZ compress / decompress.", function() {
+    for (var t = 0; t < 1000; ++t) {
+        var data = randomData();
+        var compressed = solady.LibZip.flzCompress(padRandomWhitespace(data));
+        var decompressed = solady.LibZip.flzDecompress(padRandomWhitespace(compressed));
+        assertEq(compressed.slice(0, 2), "0x");
+        assertEq(decompressed.slice(0, 2), "0x");
+        assertEq(decompressed.replace(/^0x/, ""), data.toLowerCase().replace(/^0x/, ""));
     }
+});
 
+test("LibZip: Calldata compress / decompress.", function() {
     var totalDataLength = 0;
     var totalCompressedLength = 0;
     for (var t = 0; t < 10000; ++t) {
@@ -86,13 +97,13 @@ test("Calldata compress / decompress.", function() {
     checkRevertOnInvalidInputs(solady.LibZip.cdDecompress);
 });
 
-test("Calldata compress", function() {
+test("LibZip: Calldata compress", function() {
     var data = "0xac9650d80000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000160000000000000000000000000000000000000000000000000000000000000022000000000000000000000000000000000000000000000000000000000000002a000000000000000000000000000000000000000000000000000000000000000a40c49ccbe000000000000000000000000000000000000000000000000000000000005b70e00000000000000000000000000000000000000000000000000000dfc79825feb0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000645c48a7000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000084fc6f7865000000000000000000000000000000000000000000000000000000000005b70e000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000ffffffffffffffffffffffffffffffff00000000000000000000000000000000ffffffffffffffffffffffffffffffff00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004449404b7c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000001f1cdf1a632eaaab40d1c263edf49faf749010a1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000064df2ab5bb0000000000000000000000007f5c764cbc14f9669b88837ca1490cca17c3160700000000000000000000000000000000000000000000000000000000000000000000000000000000000000001f1cdf1a632eaaab40d1c263edf49faf749010a100000000000000000000000000000000000000000000000000000000";
     var expected = "0x5369af27001e20001e04001e80001d0160001d0220001d02a0001ea40c49ccbe001c05b70e00190dfc79825feb005b645c48a7003a84fc6f7865001c05b70e002f008f000f008f003a4449404b7c002b1f1cdf1a632eaaab40d1c263edf49faf749010a1003a64df2ab5bb000b7f5c764cbc14f9669b88837ca1490cca17c31607002b1f1cdf1a632eaaab40d1c263edf49faf749010a1001b";
     assertEq(solady.LibZip.cdCompress(data), expected);
 });
 
-test("ERC1967Factory ABI and address", function() {
+test("ERC1967Factory: ABI and address", function() {
     function hashFnv32a(s) {
         var h = 0x811c9dc5;
         for (var i = 0; i < s.length; i++) {
