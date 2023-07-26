2c42e0611791a2066bbae68fb27506928b6e86c5
ross <92001561+z0r0z@users.noreply.github.com>
2023-07-26 22:57:48 +0530

    📥 Add token receivers to MockERC1271Wallet.sol
    More testing possibilities with contract wallets.

--- test/utils/mocks/MockERC1271Wallet.sol
+++ test/utils/mocks/MockERC1271Wallet.sol
@@ -3,7 +3,44 @@ pragma solidity ^0.8.4;
 
 import "../../../src/utils/ECDSA.sol";
 
-contract MockERC1271Wallet {
+/// @notice A generic interface for a contract which properly accepts ERC721 tokens.
+/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC721.sol)
+abstract contract ERC721TokenReceiver {
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return ERC721TokenReceiver.onERC721Received.selector;
+    }
+}
+
+/// @notice A generic interface for a contract which properly accepts ERC1155 tokens.
+/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC1155.sol)
+abstract contract ERC1155TokenReceiver {
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return ERC1155TokenReceiver.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return ERC1155TokenReceiver.onERC1155BatchReceived.selector;
+    }
+}
+
+contract MockERC1271Wallet is ERC721TokenReceiver, ERC1155TokenReceiver {
     address signer;
 
     constructor(address signer_) {
