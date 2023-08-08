0c7d2f4dfe5b2e00fa50fd5eee1877e8524ac345
Vectorized <webby1111@hotmail.com>
2023-08-08 09:47:33 +0000

    Add limit to truncate readString result

--- src/utils/MetadataReaderLib.sol
+++ src/utils/MetadataReaderLib.sol
@@ -9,7 +9,7 @@ library MetadataReaderLib {
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     // Best-effort string reading operations.
-    // Should NOT revert if sufficient gas is provided.
+    // Should NOT revert as long as sufficient gas is provided.
     //
     // Performs the following in order:
     // 1. Returns the empty string for the following cases:
@@ -20,23 +20,28 @@ library MetadataReaderLib {
     // 3. With any remaining gas, scans the returndata from start to end for the
     //    null byte '\0', to interpret the returndata as a null-terminated string.
 
-    /// @dev Equivalent to `readString(abi.encodeWithSignature("name()"))`.
-    function readName(address target) internal view returns (string memory) {
-        return _string(target, _ptr(0x06fdde03));
+    /// @dev Equivalent to `readString(abi.encodeWithSignature("name()"), limit)`.
+    function readName(address target, uint256 limit) internal view returns (string memory) {
+        return _string(target, _ptr(0x06fdde03), limit);
     }
 
-    /// @dev Equivalent to `readString(abi.encodeWithSignature("symbol()"))`.
-    function readSymbol(address target) internal view returns (string memory) {
-        return _string(target, _ptr(0x95d89b41));
+    /// @dev Equivalent to `readString(abi.encodeWithSignature("symbol()"), limit)`.
+    function readSymbol(address target, uint256 limit) internal view returns (string memory) {
+        return _string(target, _ptr(0x95d89b41), limit);
     }
 
     /// @dev Performs a best-effort string query on `target` with `data` as the calldata.
-    function readString(address target, bytes memory data) internal view returns (string memory) {
-        return _string(target, _ptr(data));
+    /// The string will be truncated to `limit` bytes.
+    function readString(address target, bytes memory data, uint256 limit)
+        internal
+        view
+        returns (string memory)
+    {
+        return _string(target, _ptr(data), limit);
     }
 
     // Best-effort unsigned integer reading operations.
-    // Should NOT revert if sufficient gas is provided.
+    // Should NOT revert as long as sufficient gas is provided.
     //
     // Performs the following in order:
     // 1. Attempts to `abi.decode` the result into a uint256
@@ -62,9 +67,16 @@ library MetadataReaderLib {
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     /// @dev Attempts to read and return a string at `target`.
-    function _string(address target, bytes32 ptr) private view returns (string memory result) {
+    function _string(address target, bytes32 ptr, uint256 limit)
+        private
+        view
+        returns (string memory result)
+    {
         /// @solidity memory-safe-assembly
         assembly {
+            function min(x_, y_) -> _z {
+                _z := xor(x_, mul(xor(x_, y_), lt(y_, x_)))
+            }
             for {} staticcall(gas(), target, add(ptr, 0x20), mload(ptr), 0x00, 0x20) {} {
                 let m := mload(0x40) // Grab the free memory pointer.
                 let p := add(0x20, m) // Pointer to the string's contents in memory.
@@ -75,8 +87,10 @@ library MetadataReaderLib {
                     if iszero(gt(o, sub(returndatasize(), 0x20))) {
                         returndatacopy(m, o, 0x20) // Copy the string's length.
                         let n := mload(m) // Load the string's length.
-                        // If the string's end is within bounds.
+                        // If the full string's end is within bounds.
                         if iszero(gt(n, sub(returndatasize(), add(o, 0x20)))) {
+                            n := min(n, limit) // Truncate if needed.
+                            mstore(m, n) // Overwrite the length.
                             returndatacopy(p, add(o, 0x20), n) // Copy the string's contents.
                             mstore(add(p, n), 0) // Zeroize the slot after the string.
                             mstore(0x40, add(0x20, add(p, n))) // Allocate memory for the string.
@@ -87,8 +101,9 @@ library MetadataReaderLib {
                 }
                 // Try interpreting as a null-terminated string.
                 let i := p
-                returndatacopy(p, 0, returndatasize()) // Copy the string's contents.
-                mstore8(add(p, returndatasize()), 0) // Place a '\0' at the end.
+                let n := min(returndatasize(), limit)
+                returndatacopy(p, 0, n) // Copy the string's contents.
+                mstore8(add(p, n), 0) // Place a '\0' at the end.
                 for {} byte(0, mload(i)) { i := add(i, 1) } {} // Scan for '\0'.
                 mstore(m, sub(i, p)) // Store the string's length.
                 mstore(i, 0) // Zeroize the slot after the string.
