c3010f2ff077b4fc9e919406c63ee7b76ab588dd
zerosnacks <zerosnacks@protonmail.com>
2023-08-09 09:18:07 +0200

    tweaks

--- test/FixedPointMathLib.t.sol
+++ test/FixedPointMathLib.t.sol
@@ -209,11 +209,9 @@ contract FixedPointMathLibTest is SoladyTest {
         assertEq(FixedPointMathLib.rpow(0, 0, 0), 0);
         assertEq(FixedPointMathLib.rpow(1, 0, 0), 0);
         assertEq(FixedPointMathLib.rpow(0, 1, 0), 0);
-        assertEq(FixedPointMathLib.rpow(0, 1, 1), 0);
-        assertEq(FixedPointMathLib.rpow(1, 1, 1), 1);
-        assertEq(FixedPointMathLib.rpow(1, 0, 1), 1);
         assertEq(FixedPointMathLib.rpow(0, 0, 1), 1);
-        assertEq(FixedPointMathLib.rpow(1, 0, 2), 2);
+        assertEq(FixedPointMathLib.rpow(1, 1, 0), 1);
+        assertEq(FixedPointMathLib.rpow(1, 1, 1), 1);
         assertEq(FixedPointMathLib.rpow(2e27, 0, 1e27), 1e27);
         assertEq(FixedPointMathLib.rpow(2e27, 2, 1e27), 4e27);
         assertEq(FixedPointMathLib.rpow(2e18, 2, 1e18), 4e18);
