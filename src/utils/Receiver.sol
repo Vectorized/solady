84b98a4fa7bce9755c76241df4291dc50ef479e8
ross <92001561+z0r0z@users.noreply.github.com>
2023-09-21 23:34:52 +0530

    ⚡ optimize / formatting
    ⚡ optimize / formatting

--- src/utils/Receiver.sol
+++ src/utils/Receiver.sol
@@ -3,26 +3,23 @@ pragma solidity ^0.8.4;
 
 /// @notice Optimized token callback handler via `fallback()`.
 contract Receiver {
-    fallback() external payable {
+    fallback() external virtual {
+        /// @solidity memory-safe-assembly
         assembly {
-            // Shift right by 224 bits to get the function signature.
-            let shiftedSig := shr(224, calldataload(0))
-
-            // `keccak256("onERC721Received(address,address,uint256,bytes)")`.
-            if eq(shiftedSig, 0x150b7a02) {
-                mstore(0x20, shiftedSig)
+            let s := shr(224, calldataload(0))
+            // `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`.
+            if eq(s, 0x150b7a02) {
+                mstore(0x20, s)
                 return(0x3c, 0x20)
             }
-
-            // `keccak256("onERC1155Received(address,address,uint256,uint256,bytes)")`.
-            if eq(shiftedSig, 0xf23a6e61) {
-                mstore(0x00, shl(224, shiftedSig))
+            // `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes))")`.
+            if eq(s, 0xf23a6e61) {
+                mstore(0x00, shl(224, s))
                 return(0x00, 0x20)
             }
-
-            // `keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"`.
-            if eq(shiftedSig, 0xbc197c81) {
-                mstore(0x00, shl(224, shiftedSig))
+            // `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes))"`.
+            if eq(s, 0xbc197c81) {
+                mstore(0x00, shl(224, s))
                 return(0x00, 0x20)
             }
         }
