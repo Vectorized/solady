3b573b36e28ee1c2e5eeb9bb327ac8fd5bafcb39
ross <92001561+z0r0z@users.noreply.github.com>
2023-09-21 12:10:21 +0000

    ⚡📬 callback fallback

---
+++ src/utils/Receiver.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @notice Optimized token callback handler via `fallback()`.
+contract Receiver {
+    fallback() external payable {
+        assembly {
+            // Shift right by 224 bits to get the function signature.
+            let shiftedSig := shr(224, calldataload(0))
+
+            // `keccak256("onERC721Received(address,address,uint256,bytes)")`.
+            if eq(shiftedSig, 0x150b7a02) {
+                // Equivalent to ERC721_RECEIVED
+                mstore(0x00, shl(224, shiftedSig))
+                return(0x00, 0x20)
+            }
+
+            // `keccak256("onERC1155Received(address,address,uint256,uint256,bytes)")`.
+            if eq(shiftedSig, 0xf23a6e61) {
+                // Equivalent to ERC1155_RECEIVED
+                mstore(0x00, shl(224, shiftedSig))
+                return(0x00, 0x20)
+            }
+
+            // `keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"`.
+            if eq(shiftedSig, 0xbc197c81) {
+                // Equivalent to ERC1155_BATCH_RECEIVED
+                mstore(0x00, shl(224, shiftedSig))
+                return(0x00, 0x20)
+            }
+        }
+    }
+}
