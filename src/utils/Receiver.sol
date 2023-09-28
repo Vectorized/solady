0f673d32c0ef1ed904d9b0c2838d56487c19be01
ross <92001561+z0r0z@users.noreply.github.com>
2023-09-28 17:20:39 +0000

    🥌 Simpler and future-proofed is also cheaper ofc

--- src/utils/Receiver.sol
+++ src/utils/Receiver.sol
@@ -1,37 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-/// @notice Optimized token callback handler using a unified fallback function.
+/// @notice Universal transfer callback receiver using fallback returns of selector calldata.
 /// @author Solady (https://github.com/Vectorized/solady/blob/main/src/utils/Receiver.sol)
 ///
 /// @dev Note:
-/// - Handles ERC721 and ERC1155 token callbacks.
-/// - Optimizes for gas by using a single fallback function to handle multiple selectors,
-///   avoiding the overhead of a selector table.
+/// - Handles all ERC721 and ERC1155 token safety callbacks.
+/// - Future-proofed for any selector-based callback check.
+/// - Collapses function table gas overhead and code size.
 abstract contract Receiver {
-    /// @dev Accepts incoming Ether.
+    /// @dev Accepts ether (ETH).
+    /// Override to reject ETH
+    /// or add custom logic.
     receive() external payable virtual {}
 
-    /// @dev Fallback function for handling ERC721 and ERC1155 `safeTransferFrom()` callbacks.
+    /// @dev Unified fallback function.
+    /// Override to reject or specify.
     fallback() external virtual {
         /// @solidity memory-safe-assembly
         assembly {
-            let s := shr(224, calldataload(0))
-            // `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`.
-            if eq(s, 0x150b7a02) {
-                mstore(0x20, s)
-                return(0x3c, 0x20)
-            }
-            // `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes))")`.
-            if eq(s, 0xf23a6e61) {
-                mstore(0x20, s)
-                return(0x3c, 0x20)
-            }
-            // `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes))"`.
-            if eq(s, 0xbc197c81) {
-                mstore(0x20, s)
-                return(0x3c, 0x20)
-            }
+            // Shift and load the calldata `msg.sig`.
+            mstore(0x20, shr(224, calldataload(0)))
+            return(0x3C, 0x20) // Return `msg.sig`.
         }
     }
 }
