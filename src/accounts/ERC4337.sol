2bc9c0972ef9217119d8dea9c2b95a8004e848c9
ross <92001561+z0r0z@users.noreply.github.com>
2023-10-12 19:48:16 +0000

    1️⃣2️⃣7️⃣1️⃣

--- src/accounts/ERC4337.sol
+++ src/accounts/ERC4337.sol
@@ -140,6 +140,20 @@ contract ERC4337 is Ownable, UUPSUpgradeable, Receiver {
         _;
     }
 
+    /// @dev Validates the signature with ERC1271 return.
+    function isValidSignature(bytes32 hash, bytes calldata signature)
+        public
+        view
+        returns (bytes4)
+    {
+        if (
+            SignatureCheckerLib.isValidERC1271SignatureNowCalldata(
+                owner(), ECDSA.toEthSignedMessageHash(hash), signature
+            )
+        ) return 0x1626ba7e;
+        return 0xffffffff;
+    }
+
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                    EXECUTION OPERATIONS                    */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
