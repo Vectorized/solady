727b6d109048ddd159d213698a3785eac385122c
ross <92001561+z0r0z@users.noreply.github.com>
2023-10-18 13:00:59 +0000

    ✨ Simple ERC6551 Account

---
+++ src/accounts/ERC6551.sol
@@ -0,0 +1,221 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {Receiver} from "./Receiver.sol";
+import {LibZip} from "../utils/LibZip.sol";
+import {UUPSUpgradeable} from "../utils/UUPSUpgradeable.sol";
+import {SignatureCheckerLib} from "../utils/SignatureCheckerLib.sol";
+
+/// @notice Simple ERC6551 account implementation.
+/// @author Solady (https://github.com/vectorized/solady/blob/main/src/accounts/ERC6551.sol)
+///
+/// Recommended usage:
+/// 1. Deploy the ERC6551 as an implementation contract, and verify it on Etherscan.
+/// 2. Create a factory that uses `LibClone.deployERC1967` or
+///    `LibClone.deployDeterministicERC1967` to clone the implementation.
+///    See: `ERC6551Factory.sol`.
+contract ERC6551 is UUPSUpgradeable, Receiver {
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       CUSTOM ERRORS                        */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev The caller is not authorized to call the function.
+    error Unauthorized();
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                          STRUCTS                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Call struct for the `executeBatch` function.
+    struct Call {
+        address target;
+        uint256 value;
+        bytes data;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                          STORAGE                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    uint256 internal _chainId;
+    address internal _tokenContract;
+    uint256 internal _tokenId;
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                        INITIALIZER                         */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Initializes the account with the token. Can only be called once.
+    function initialize(uint256 chainId, address tokenContract, uint256 tokenId)
+        public
+        payable
+        virtual
+    {
+        assert(tokenContract == address(0));
+        _chainId = chainId;
+        _tokenContract = tokenContract;
+        _tokenId = tokenId;
+        assert(tokenContract != address(0));
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                        ERC6551                             */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Returns the identifier of the non-fungible token which owns the account.
+    /// The return value of this function MUST be constant - it MUST NOT change
+    /// over time.
+    function token()
+        public
+        view
+        virtual
+        returns (uint256 chainId, address tokenContract, uint256 tokenId)
+    {
+        chainId = _chainId;
+        tokenContract = _tokenContract;
+        tokenId = _tokenId;
+    }
+
+    /// @dev Returns a value that SHOULD be modified each time the account changes state.
+    /// @return The current account state.
+    function state() public view virtual returns (uint256) {}
+
+    /// @dev Returns a magic value indicating whether a given signer is authorized to act on behalf of the account.
+    /// MUST return the bytes4 magic value `0x523e3260 `if the given signer is valid.
+    /// By default, the holder of the non-fungible token the account is bound to MUST be considered a valid
+    /// signer.
+    ///
+    /// Accounts MAY implement additional authorization logic which invalidates the holder as a
+    /// signer or grants signing permissions to other non-holder accounts
+    function isValidSigner(address signer, bytes calldata)
+        public
+        view
+        virtual
+        returns (bytes4 result)
+    {
+        bool success = signer == owner();
+        /// @solidity memory-safe-assembly
+        assembly {
+            // `success ? bytes4(keccak256("isValidSigner(address,bytes)")) : 0xffffffff`.
+            result := shl(224, or(0x523e3260, sub(0, iszero(success))))
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                        AUTH                                */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function owner() public view virtual returns (address result) {
+        (, address tokenContract, uint256 tokenId) = token();
+        result = IERC721(tokenContract).ownerOf(tokenId);
+    }
+
+    modifier onlyOwner() virtual {
+        if (msg.sender != owner()) revert Unauthorized();
+        _;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                        ERC1271                             */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Validates the signature with ERC1271 return,
+    /// so that this account can also be used as a signer.
+    function isValidSignature(bytes32 hash, bytes calldata signature)
+        public
+        view
+        virtual
+        returns (bytes4 result)
+    {
+        bool success = SignatureCheckerLib.isValidSignatureNowCalldata(
+            owner(), SignatureCheckerLib.toEthSignedMessageHash(hash), signature
+        );
+        /// @solidity memory-safe-assembly
+        assembly {
+            // `success ? bytes4(keccak256("isValidSignature(bytes32,bytes)")) : 0xffffffff`.
+            result := shl(224, or(0x1626ba7e, sub(0, iszero(success))))
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                    EXECUTION OPERATIONS                    */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Execute a call from this account.
+    function execute(address target, uint256 value, bytes calldata data)
+        public
+        payable
+        virtual
+        onlyOwner
+        returns (bytes memory result)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := mload(0x40)
+            calldatacopy(result, data.offset, data.length)
+            if iszero(call(gas(), target, value, result, data.length, codesize(), 0x00)) {
+                // Bubble up the revert if the call reverts.
+                returndatacopy(result, 0x00, returndatasize())
+                revert(result, returndatasize())
+            }
+            mstore(result, returndatasize()) // Store the length.
+            let o := add(result, 0x20)
+            returndatacopy(o, 0x00, returndatasize()) // Copy the returndata.
+            mstore(0x40, add(o, returndatasize())) // Allocate the memory.
+        }
+    }
+
+    /// @dev Execute a sequence of calls from this account.
+    function executeBatch(Call[] calldata calls)
+        public
+        payable
+        virtual
+        onlyOwner
+        returns (bytes[] memory results)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            results := mload(0x40)
+            mstore(results, calls.length)
+            let r := add(0x20, results)
+            let m := add(r, shl(5, calls.length))
+            calldatacopy(r, calls.offset, shl(5, calls.length))
+            for { let end := m } iszero(eq(r, end)) { r := add(r, 0x20) } {
+                let e := add(calls.offset, mload(r))
+                let o := add(e, calldataload(add(e, 0x40)))
+                calldatacopy(m, add(o, 0x20), calldataload(o))
+                // forgefmt: disable-next-item
+                if iszero(call(gas(), calldataload(e), calldataload(add(e, 0x20)),
+                    m, calldataload(o), codesize(), 0x00)) {
+                    // Bubble up the revert if the call reverts.
+                    returndatacopy(m, 0x00, returndatasize())
+                    revert(m, returndatasize())
+                }
+                mstore(r, m) // Append `m` into `results`.
+                mstore(m, returndatasize()) // Store the length,
+                let p := add(m, 0x20)
+                returndatacopy(p, 0x00, returndatasize()) // and copy the returndata.
+                m := add(p, returndatasize()) // Advance `m`.
+            }
+            mstore(0x40, m) // Allocate the memory.
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         OVERRIDES                          */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev To ensure that only the owner or the account itself can upgrade the implementation.
+    function _authorizeUpgrade(address) internal virtual override(UUPSUpgradeable) onlyOwner {}
+
+    /// @dev Handle token callbacks. If no token callback is triggered,
+    /// use `LibZip.cdFallback` for generalized calldata decompression.
+    /// If you don't need either, re-override this function.
+    fallback() external payable virtual override(Receiver) receiverFallback {
+        LibZip.cdFallback();
+    }
+}
+
+interface IERC721 {
+    function ownerOf(uint256) external view returns (address);
+}
