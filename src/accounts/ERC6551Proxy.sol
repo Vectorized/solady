619c99bac40caec5062a19c3bf389d435086d810
Vectorized <webby1111@hotmail.com>
2023-10-21 03:59:49 +0000

    Edit comments

--- src/accounts/ERC6551Proxy.sol
+++ src/accounts/ERC6551Proxy.sol
@@ -5,8 +5,10 @@ pragma solidity ^0.8.4;
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/accounts/ERC6551Proxy.sol)
 /// @author ERC6551 team (https://github.com/erc6551/reference/blob/main/src/examples/upgradeable/ERC6551AccountProxy.sol)
 ///
-/// Note: This proxy is specially developed for use with ERC6551 upgradeable accounts,
-/// such that it can show the "Read as Proxy" and "Write as Proxy" tabs on Etherscan.
+/// Note: This proxy is specially developed for use with upgradeable ERC6551 accounts,
+/// so that it can correctly reveal the "Read as Proxy" and "Write as Proxy" tabs on Etherscan.
+/// After using the registry to deploy a ERC6551 clone pointing to this proxy,
+/// users must send 0 ETH to the clone before clicking on "Is this a proxy?" on Etherscan.
 contract ERC6551Proxy {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                         IMMUTABLES                         */
