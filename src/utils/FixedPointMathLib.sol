819000a6ec56adfcc2fc5667ba94f7cf16c58b58
Vectorized <webby1111@hotmail.com>
2023-10-26 05:38:26 +0000

    Tidy

--- src/utils/FixedPointMathLib.sol
+++ src/utils/FixedPointMathLib.sol
@@ -570,14 +570,9 @@ library FixedPointMathLib {
             r := or(r, shl(5, lt(0xffffffff, shr(r, x))))
             r := or(r, shl(4, lt(0xffff, shr(r, x))))
             r := or(r, shl(3, lt(0xff, shr(r, x))))
-            r :=
-                or(
-                    r,
-                    byte(
-                        and(0x1f, shr(shr(r, x), 0xc30c30c30c30c30cfcfcfcf924928add)),
-                        0x706030604030605030403020602000506030405030202010605ca0105000505
-                    )
-                )
+            // forgefmt: disable-next-item
+            r := or(r, byte(and(0x1f, shr(shr(r, x), 0xc30c30c30c30c30cfcfcfcf924928add)),
+                0x706030604030605030403020602000506030405030202010605ca0105000505))
         }
     }
 
