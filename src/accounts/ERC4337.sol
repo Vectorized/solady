94eb1ccaf41a64fd5b2750c3284cf2def2fa832d
Vectorized <webby1111@hotmail.com>
2023-10-31 20:29:28 +0000

    Refactor out common logic into ERC1271

--- src/accounts/ERC4337.sol
+++ src/accounts/ERC4337.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.4;
 
 import {Receiver} from "./Receiver.sol";
+import {ERC1271} from "./ERC1271.sol";
 import {EIP712} from "../utils/EIP712.sol";
 import {LibZip} from "../utils/LibZip.sol";
 import {Ownable} from "../auth/Ownable.sol";
@@ -17,7 +18,7 @@ import {SignatureCheckerLib} from "../utils/SignatureCheckerLib.sol";
 /// 2. Create a factory that uses `LibClone.deployERC1967` or
 ///    `LibClone.deployDeterministicERC1967` to clone the implementation.
 ///    See: `ERC4337Factory.sol`.
-abstract contract ERC4337 is Ownable, UUPSUpgradeable, Receiver, EIP712 {
+abstract contract ERC4337 is Ownable, UUPSUpgradeable, Receiver, ERC1271 {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                          STRUCTS                           */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
@@ -89,95 +90,6 @@ abstract contract ERC4337 is Ownable, UUPSUpgradeable, Receiver, EIP712 {
     {
         validationData = _validateSignature(userOp, userOpHash);
         _validateNonce(userOp.nonce);
-    }
-
-    /// @dev Validates the signature with ERC1271 return,
-    /// so that this account can also be used as a signer.
-    function isValidSignature(bytes32 hash, bytes calldata signature)
-        public
-        view
-        virtual
-        returns (bytes4 result)
-    {
-        bool success = _isValidSignatureWithNestedEIP712(hash, signature);
-        /// @solidity memory-safe-assembly
-        assembly {
-            // `success ? bytes4(keccak256("isValidSignature(bytes32,bytes)")) : 0xffffffff`.
-            result := shl(224, or(0x1626ba7e, sub(0, iszero(success))))
-        }
-    }
-
-    /// @dev Returns if the `signature` is valid.
-    ///
-    /// This method uses the nested EIP-712 approach to prevent signature replays
-    /// when a single EOA owns multiple smart contract accounts, while still enabling
-    /// wallet UIs (e.g. Metamask) to show the EIP-712 values.
-    ///
-    /// In pseudocode, the final hash for the nested EIP-712 workflow will be:
-    /// ```
-    ///     keccak256(\x19\x01 || DOMAIN_SEP_A ||
-    ///         hashStruct(Parent({
-    ///             childHash: keccak256(\x19\x01 || DOMAIN_SEP_B || hashStruct(originalStruct)),
-    ///             child: hashStruct(originalStruct)
-    ///         }))
-    ///     )
-    /// ```
-    /// where `||` denotes the concatenation operator for bytes.
-    /// The signature will be `r || s || v || PARENT_TYPEHASH || child || DOMAIN_SEP_B`.
-    ///
-    /// For the `personal_sign` workflow, the final hash will be:
-    /// ```
-    ///     keccak256(\x19\x01 || DOMAIN_SEP_A ||
-    ///         hashStruct(Parent({
-    ///             childHash: personalSign(someBytes)
-    ///         }))
-    ///     )
-    /// ```
-    /// where `||` denotes the concatenation operator for bytes.
-    /// The signature will be `r || s || v || PARENT_TYPEHASH || bytes32(0) || bytes32(anything)`.
-    ///
-    /// See: https://github.com/junomonster/nested-eip-712 for demo and frontend typescript code.
-    function _isValidSignatureWithNestedEIP712(bytes32 hash, bytes calldata signature)
-        internal
-        view
-        virtual
-        returns (bool result)
-    {
-        if (signature.length < 0x60) return result;
-        uint256 childHashMismatch;
-        /// @solidity memory-safe-assembly
-        assembly {
-            // Truncate the `signature.length` by 3 words (96 bytes).
-            // A nested EIP-712 ECDSA signature will contain 65 + 96 bytes.
-            signature.length := sub(signature.length, 0x60)
-            let o := add(signature.offset, signature.length)
-            let child := calldataload(add(o, 0x20))
-            switch child
-            // `personal_sign` workflow.
-            case 0 {
-                mstore(0x00, calldataload(o)) // Store the `PARENT_TYPEHASH`.
-                mstore(0x20, hash) // Store the `childHash`.
-                hash := keccak256(0x00, 0x40) // Compute the parent's structHash.
-            }
-            // Nested EIP-712 workflow.
-            default {
-                let m := mload(0x40) // Cache the free memory pointer.
-                mstore(0x00, 0x1901) // Store the "\x19\x01" prefix.
-                mstore(0x20, calldataload(add(o, 0x40))) // Store the `DOMAIN_SEP_B`
-                mstore(0x40, child) // Store the `child`.
-                childHashMismatch := xor(keccak256(0x1e, 0x42), hash)
-                mstore(0x00, calldataload(o)) // Store the `PARENT_TYPEHASH`.
-                mstore(0x20, hash) // Store the `childHash`.
-                // The `child` is already at 0x40.
-                hash := keccak256(0x00, 0x60) // Compute the parent's structHash.
-                mstore(0x40, m) // Restore the free memory pointer.
-            }
-        }
-        if (childHashMismatch == 0) {
-            result = SignatureCheckerLib.isValidSignatureNowCalldata(
-                owner(), _hashTypedData(hash), signature
-            );
-        }
     }
 
     /// @dev Validate `userOp.signature` for the `userOpHash`.
@@ -457,6 +369,11 @@ abstract contract ERC4337 is Ownable, UUPSUpgradeable, Receiver, EIP712 {
     /// @dev To prevent double-initialization (reuses the owner storage slot for efficiency).
     function _guardInitializeOwner() internal pure virtual override(Ownable) returns (bool) {
         return true;
+    }
+
+    /// @dev Uses the `onwer` as the ERC1271 signer.
+    function _erc1271Signer() internal view virtual override(ERC1271) returns (address) {
+        return owner();
     }
 
     /// @dev To ensure that only the owner or the account itself can upgrade the implementation.
--- src/accounts/ERC6551.sol
+++ src/accounts/ERC6551.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.4;
 
 import {Receiver} from "./Receiver.sol";
-import {EIP712} from "../utils/EIP712.sol";
+import {ERC1271} from "./ERC1271.sol";
 import {LibZip} from "../utils/LibZip.sol";
 import {UUPSUpgradeable} from "../utils/UUPSUpgradeable.sol";
-import {SignatureCheckerLib} from "../utils/SignatureCheckerLib.sol";
 
 /// @notice Simple ERC6551 account implementation.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/accounts/ERC6551.sol)
@@ -31,7 +30,7 @@ import {SignatureCheckerLib} from "../utils/SignatureCheckerLib.sol";
 ///   due to storage access limitations during ERC4337 UserOp validation.
 /// - Please refer to the official [ERC6551](https://github.com/erc6551/reference) reference
 ///   for latest updates on the ERC6551 standard, as well as canonical registry information.
-abstract contract ERC6551 is UUPSUpgradeable, Receiver, EIP712 {
+abstract contract ERC6551 is UUPSUpgradeable, Receiver, ERC1271 {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                          STRUCTS                           */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
@@ -111,95 +110,6 @@ abstract contract ERC6551 is UUPSUpgradeable, Receiver, EIP712 {
                     )
             }
             mstore(0x40, m) // Restore the free memory pointer.
-        }
-    }
-
-    /// @dev Validates the signature with ERC1271 return,
-    /// so that this account can also be used as a signer.
-    function isValidSignature(bytes32 hash, bytes calldata signature)
-        public
-        view
-        virtual
-        returns (bytes4 result)
-    {
-        bool success = _isValidSignatureWithNestedEIP712(hash, signature);
-        /// @solidity memory-safe-assembly
-        assembly {
-            // `success ? bytes4(keccak256("isValidSignature(bytes32,bytes)")) : 0xffffffff`.
-            result := shl(224, or(0x1626ba7e, sub(0, iszero(success))))
-        }
-    }
-
-    /// @dev Returns if the `signature` is valid.
-    ///
-    /// This method uses the nested EIP-712 approach to prevent signature replays
-    /// when a single EOA owns multiple smart contract accounts, while still enabling
-    /// wallet UIs (e.g. Metamask) to show the EIP-712 values.
-    ///
-    /// In pseudocode, the final hash for the nested EIP-712 workflow will be:
-    /// ```
-    ///     keccak256(\x19\x01 || DOMAIN_SEP_A ||
-    ///         hashStruct(Parent({
-    ///             childHash: keccak256(\x19\x01 || DOMAIN_SEP_B || hashStruct(originalStruct)),
-    ///             child: hashStruct(originalStruct)
-    ///         }))
-    ///     )
-    /// ```
-    /// where `||` denotes the concatenation operator for bytes.
-    /// The signature will be `r || s || v || PARENT_TYPEHASH || child || DOMAIN_SEP_B`.
-    ///
-    /// For the `personal_sign` workflow, the final hash will be:
-    /// ```
-    ///     keccak256(\x19\x01 || DOMAIN_SEP_A ||
-    ///         hashStruct(Parent({
-    ///             childHash: personalSign(someBytes)
-    ///         }))
-    ///     )
-    /// ```
-    /// where `||` denotes the concatenation operator for bytes.
-    /// The signature will be `r || s || v || PARENT_TYPEHASH || bytes32(0) || bytes32(anything)`.
-    ///
-    /// See: https://github.com/junomonster/nested-eip-712 for demo and frontend typescript code.
-    function _isValidSignatureWithNestedEIP712(bytes32 hash, bytes calldata signature)
-        internal
-        view
-        virtual
-        returns (bool result)
-    {
-        if (signature.length < 0x60) return result;
-        uint256 childHashMismatch;
-        /// @solidity memory-safe-assembly
-        assembly {
-            // Truncate the `signature.length` by 3 words (96 bytes).
-            // A nested EIP-712 ECDSA signature will contain 65 + 96 bytes.
-            signature.length := sub(signature.length, 0x60)
-            let o := add(signature.offset, signature.length)
-            let child := calldataload(add(o, 0x20))
-            switch child
-            // `personal_sign` workflow.
-            case 0 {
-                mstore(0x00, calldataload(o)) // Store the `PARENT_TYPEHASH`.
-                mstore(0x20, hash) // Store the `childHash`.
-                hash := keccak256(0x00, 0x40) // Compute the parent's structHash.
-            }
-            // Nested EIP-712 workflow.
-            default {
-                let m := mload(0x40) // Cache the free memory pointer.
-                mstore(0x00, 0x1901) // Store the "\x19\x01" prefix.
-                mstore(0x20, calldataload(add(o, 0x40))) // Store the `DOMAIN_SEP_B`
-                mstore(0x40, child) // Store the `child`.
-                childHashMismatch := xor(keccak256(0x1e, 0x42), hash)
-                mstore(0x00, calldataload(o)) // Store the `PARENT_TYPEHASH`.
-                mstore(0x20, hash) // Store the `childHash`.
-                // The `child` is already at 0x40.
-                hash := keccak256(0x00, 0x60) // Compute the parent's structHash.
-                mstore(0x40, m) // Restore the free memory pointer.
-            }
-        }
-        if (childHashMismatch == 0) {
-            result = SignatureCheckerLib.isValidSignatureNowCalldata(
-                owner(), _hashTypedData(hash), signature
-            );
         }
     }
 
@@ -339,6 +249,11 @@ abstract contract ERC6551 is UUPSUpgradeable, Receiver, EIP712 {
         onlyValidSigner
         incrementState
     {}
+
+    /// @dev Uses the `onwer` as the ERC1271 signer.
+    function _erc1271Signer() internal view virtual override(ERC1271) returns (address) {
+        return owner();
+    }
 
     /// @dev For handling token callbacks.
     /// Safe-transferred ERC721 tokens will trigger a ownership cycle check.
