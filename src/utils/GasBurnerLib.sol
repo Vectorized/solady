3b45542d186158a9eda620a99dc451bfb0cee219
Vectorized <webby1111@hotmail.com>
2023-11-21 00:57:39 +0000

    Add GasBurnerLib

---
+++ src/utils/GasBurnerLib.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @notice Library for burning gas without reverting.
+/// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/GasBurnerLib.sol)
+library GasBurnerLib {
+    /// @dev Burns about `x` amount of gas.
+    function burn(uint256 x) internal pure {
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x00, or(1, x))
+            let n := mul(gt(x, 120), div(x, 88))
+            // We use keccak256 instead of blake2f precompile for better widespread compatibility.
+            for { let i := 0 } iszero(eq(i, n)) { i := add(i, 1) } {
+                mstore(0x00, keccak256(0x10, 0x10))
+            }
+            if iszero(mload(0x00)) { invalid() }
+        }
+    }
+}
---
+++ test/GasBurnerLib.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./utils/SoladyTest.sol";
+import {GasBurnerLib} from "../src/utils/GasBurnerLib.sol";
+
+contract GasBurnerLibTest is SoladyTest {
+    function testBurnGas() public view {
+        unchecked {
+            GasBurnerLib.burn(0);
+            GasBurnerLib.burn(1);
+            uint256 gasBefore = gasleft();
+            GasBurnerLib.burn(3000);
+            uint256 gasAfter = gasleft();
+            console.log(gasBefore - gasAfter);
+        }
+    }
+}
