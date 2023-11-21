1d9401d3ce093ebb4a1065ec62b1830c4353e61c
Vectorized <webby1111@hotmail.com>
2023-11-21 01:15:06 +0000

    Add comments

--- src/utils/GasBurnerLib.sol
+++ src/utils/GasBurnerLib.sol
@@ -5,6 +5,10 @@ pragma solidity ^0.8.4;
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/GasBurnerLib.sol)
 library GasBurnerLib {
     /// @dev Burns about `x` amount of gas.
+    /// Intended for Contract Secured Revenue (CSR).
+    /// For best results, pass in a admin-controlled dynamic value instead of a hardcoded one.
+    /// This is so that you can adjust your contract as needed depending on market conditions,
+    /// and to give your users a leeway just in case.
     function burn(uint256 x) internal pure {
         /// @solidity memory-safe-assembly
         assembly {
@@ -12,7 +16,7 @@ library GasBurnerLib {
             let n := mul(gt(x, 120), div(x, 88))
             // We use keccak256 instead of blake2f precompile for better widespread compatibility.
             for { let i := 0 } iszero(eq(i, n)) { i := add(i, 1) } {
-                mstore(0x00, keccak256(0x10, 0x10))
+                mstore(0x00, keccak256(0x10, 0x10)) // Yes.
             }
             if iszero(mload(0x00)) { invalid() }
         }
