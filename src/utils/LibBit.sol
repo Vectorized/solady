e80695436c936e57df3f8912e4b3a801b68e3846
Vectorized <webby1111@hotmail.com>
2023-12-15 04:47:09 +0000

    T

--- src/utils/LibBit.sol
+++ src/utils/LibBit.sol
@@ -109,15 +109,16 @@ library LibBit {
     function reverseBytes(uint256 x) internal pure returns (uint256 r) {
         /// @solidity memory-safe-assembly
         assembly {
+            r := or(shl(128, x), shr(128, x))
             // Computing masks on-the-fly reduces bytecode size by about 200 bytes.
-            let m := not(0)
-            r := x
-            for { let s := 128 } 1 {} {
-                m := xor(m, shl(s, m))
-                r := or(and(shr(s, r), m), and(shl(s, r), not(m)))
-                s := shr(1, s)
-                if eq(s, 4) { break }
-            }
+            let m := or(shr(192, not(0)), shl(128, shr(192, not(iszero(x)))))
+            r := or(and(shr(64, r), m), shl(64, and(m, r)))
+            m := xor(m, shl(32, m))
+            r := or(and(shr(32, r), m), shl(32, and(m, r)))
+            m := xor(m, shl(16, m))
+            r := or(and(shr(16, r), m), shl(16, and(m, r)))
+            m := xor(m, shl(8, m))
+            r := or(and(shr(8, r), m), shl(8, and(m, r)))
         }
     }
 
