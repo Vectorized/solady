2c2dcbc84637169fce5ded84555e4ba940477083
Vectorized <webby1111@hotmail.com>
2023-12-28 14:26:01 +0800

    ♻️ Improve standardNormalWad (#770)
    Co-authored-by: atapara <akpatel0618@gmail.com>
    Co-authored-by: wangshouh <wongshouhao@outlook.com>

--- src/utils/LibPRNG.sol
+++ src/utils/LibPRNG.sol
@@ -158,22 +158,21 @@ library LibPRNG {
             // Technically, this is the Irwin-Hall distribution with 20 samples.
             // The chance of drawing a sample outside 10 σ from the standard normal distribution
             // is ≈ 0.000000000000000000000015, which is insignificant for most practical purposes.
-            // This function uses about 319 gas.
+            // Passes the Kolmogorov-Smirnov test for 200k samples. Uses about 322 gas.
             result := keccak256(prng, 0x20)
             mstore(prng, result)
-            let n := 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001 // Prime.
-            let a := 0x2d3e5279f07911d5062dc0dd5c89992d // Prime and a primitive root of `n`.
-            let m := 0x0fffffffffffffff0fffffffffffffff0fffffffffffffff0fffffffffffffff
+            let n := 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff43 // Prime.
+            let a := 0x100000000000000000000000000000051 // Prime and a primitive root of `n`.
+            let m := 0x1fffffffffffffff1fffffffffffffff1fffffffffffffff1fffffffffffffff
             let s := 0x1000000000000000100000000000000010000000000000001
             let r1 := mulmod(result, a, n)
             let r2 := mulmod(r1, a, n)
             let r3 := mulmod(r2, a, n)
             // forgefmt: disable-next-item
-            result := sar(96, sub(mul(53229877791723203740515581680,
+            result := sub(sar(96, mul(26614938895861601847173011183,
                 add(add(shr(192, mul(s, add(and(m, result), and(m, r1)))),
                 shr(192, mul(s, add(and(m, r2), and(m, r3))))),
-                shr(192, mul(s, and(m, mulmod(r3, a, n)))))),
-                613698707936721051257405563935529819467266145679))
+                shr(192, mul(s, and(m, mulmod(r3, a, n))))))), 7745966692414833770)
         }
     }
 }
