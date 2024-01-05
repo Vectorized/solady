063582551c6e884164a51963af7a781e51b7c548
atarpara <akpatel0618@gmail.com>
2024-01-05 17:20:09 +0530

    ♻ fix complier version

--- test/ReentrancyGuard.t.sol
+++ test/ReentrancyGuard.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.4;
 
 import "./utils/SoladyTest.sol";
 import {ReentrancyGuard} from "../src/utils/ReentrancyGuard.sol";
