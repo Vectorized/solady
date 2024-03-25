3a4e38702e1a87f53c21f6afaf70eb8738535560
Vectorized <webby1111@hotmail.com>
2024-03-25 14:42:29 +0800

    ♻️ Tidy ERC1271 (#866)

--- src/accounts/ERC1271.sol
+++ src/accounts/ERC1271.sol
@@ -22,6 +22,22 @@ abstract contract ERC1271 is EIP712 {
     /// prevent signature replays when a single EOA owns multiple smart contract accounts,
     /// while still enabling wallet UIs (e.g. Metamask) to show the EIP-712 values.
     ///
+    /// The `hash` parameter to this method is the `childHash`.
+    /// __________________________________________________________________________________________
+    ///
+    /// Glossary:
+    ///
+    /// - `DOMAIN_SEP_B`: The domain separator of the `childHash`.
+    ///   Provided by the front end. Intended to be the domain separator of the contract
+    ///   that will call `isValidSignature` on this account.
+    ///
+    /// - `DOMAIN_SEP_A`: The domain separator of this account.
+    ///   See: `EIP712._domainSeparator()`.
+    ///
+    /// - `Parent`: The parent struct type.
+    ///   To be defined by the front end, such that `child` can be visible via EIP-712.
+    /// __________________________________________________________________________________________
+    ///
     /// For the nested EIP-712 workflow, the final hash will be:
     /// ```
     ///     keccak256(\x19\x01 || DOMAIN_SEP_A ||
@@ -32,31 +48,37 @@ abstract contract ERC1271 is EIP712 {
     ///     )
     /// ```
     /// where `||` denotes the concatenation operator for bytes.
-    /// The signature will be `r || s || v || PARENT_TYPEHASH || DOMAIN_SEP_B || child`.
+    /// The order of Parent's fields is important: `childHash` comes before `child`.
+    ///
+    /// The signature will be `r || s || v || PARENT_TYPEHASH || DOMAIN_SEP_B || child`,
+    /// where `child` is the bytes32 struct hash of the original struct.
     ///
     /// The `DOMAIN_SEP_B` and `child` will be used to verify if `childHash` is indeed correct.
+    /// __________________________________________________________________________________________
     ///
-    /// For the `personal_sign` workflow, the final hash will be:
+    /// For the `personalSign` workflow, the final hash will be:
     /// ```
     ///     keccak256(\x19\x01 || DOMAIN_SEP_A ||
     ///         hashStruct(Parent({
-    ///             childHash: personalSign(someBytes)
+    ///             childHash: keccak256(\x19Ethereum Signed Message:\n ||
+    ///                 base10(bytes(someString).length) || someString)
     ///         }))
     ///     )
     /// ```
     /// where `||` denotes the concatenation operator for bytes.
     /// The signature will be `r || s || v || PARENT_TYPEHASH`.
+    /// __________________________________________________________________________________________
     ///
     /// For demo and typescript code, see:
     /// - https://github.com/junomonster/nested-eip-712
     /// - https://github.com/frangio/eip712-wrapper-for-eip1271
     ///
+    /// Their nomenclature may differ from ours, although the high-level idea is similar.
+    ///
     /// Of course, if you are a wallet app maker and can update your app's UI at will,
     /// you can choose a more minimalistic signature scheme like
     /// `keccak256(abi.encode(address(this), hash))` instead of all these acrobatics.
     /// All these are just for widespead out-of-the-box compatibility with other wallet apps.
-    ///
-    /// The `hash` parameter is the `childHash`.
     function isValidSignature(bytes32 hash, bytes calldata signature)
         public
         view
@@ -70,23 +92,23 @@ abstract contract ERC1271 is EIP712 {
             calldatacopy(0x00, o, 0x60) // Copy the `DOMAIN_SEP_B` and child's structHash.
             mstore(0x00, 0x1901) // Store the "\x19\x01" prefix, overwriting 0x00.
             for {} 1 {} {
-                // Use the nested EIP-712 workflow if the reconstructed childHash matches,
+                // Use the nested EIP-712 workflow if the reconstructed `childHash` matches,
                 // and the signature is at least 96 bytes long.
                 if iszero(or(xor(keccak256(0x1e, 0x42), hash), lt(signature.length, 0x60))) {
                     // Truncate the `signature.length` by 3 words (96 bytes).
                     signature.length := sub(signature.length, 0x60)
                     mstore(0x00, calldataload(o)) // Store the `PARENT_TYPEHASH`.
                     mstore(0x20, hash) // Store the `childHash`.
-                    // The child's structHash is already at 0x40.
-                    hash := keccak256(0x00, 0x60) // Compute the parent's structHash.
+                    // The `child` struct hash is already at 0x40.
+                    hash := keccak256(0x00, 0x60) // Compute the `parent` struct hash.
                     break
                 }
-                // Else, use the `personal_sign` workflow.
-                // Truncate the `signature.length` by 1 word (32 bytes), until zero.
+                // Else, use the `personalSign` workflow.
+                // If `signature.length` > 1 word (32 bytes), reduce by 1 word, else set to 0.
                 signature.length := mul(gt(signature.length, 0x20), sub(signature.length, 0x20))
                 // The `PARENT_TYPEHASH` is already at 0x40.
                 mstore(0x60, hash) // Store the `childHash`.
-                hash := keccak256(0x40, 0x40) // Compute the parent's structHash.
+                hash := keccak256(0x40, 0x40) // Compute the `parent` struct hash.
                 mstore(0x60, 0) // Restore the zero pointer.
                 break
             }
