fa7c1bfaba07a1c6f19c49246452be6735fba0f6
Vectorized <webby1111@hotmail.com>
2024-04-29 05:02:18 +0000

    Add checks for banned characters

--- src/accounts/ERC1271.sol
+++ src/accounts/ERC1271.sol
@@ -175,8 +175,14 @@ abstract contract ERC1271 is EIP712 {
                 mstore(m, "TypedDataSign(bytes32 hash,")
                 let p := add(m, 0x1b) // Advance 27 bytes.
                 calldatacopy(p, add(o, 0x40), c) // Copy the contents type.
-                // Store the end sentinel ")(", and advance `p` until we encounter a '(' byte.
-                for { mstore(add(p, c), 0x2928) } xor(byte(0, mload(p)), 40) {} { p := add(p, 1) }
+                // Whether the contents name is invalid.
+                let d := gt(26, sub(byte(0, mload(p)), 97)) // Starts with lowercase.
+                // Store the end sentinel '(', and advance `p` until we encounter a '(' byte.
+                for { mstore(add(p, c), 40) } 1 { p := add(p, 1) } {
+                    let b := byte(0, mload(p))
+                    if eq(b, 40) { break }
+                    d := or(d, and(1, shr(b, 0x120100000001))) // Has a byte in ", )\x00".
+                }
                 mstore(p, " contents,bytes1 fields,string n")
                 mstore(add(p, 0x20), "ame,string version,uint256 chain")
                 mstore(add(p, 0x40), "Id,address verifyingContract,byt")
@@ -190,8 +196,7 @@ abstract contract ERC1271 is EIP712 {
                 // The "\x19\x01" prefix is already at 0x00.
                 // `DOMAIN_SEP_B` is already at 0x20.
                 mstore(0x40, keccak256(t, 0x140)) // `hashStruct(typedDataSign)`.
-                // Compute the final hash, corrupted if contents name begins with lowercase alphabet.
-                hash := keccak256(0x1e, add(0x42, gt(26, sub(byte(0, mload(add(0x1b, m))), 97))))
+                hash := keccak256(0x1e, add(0x42, d)) // Compute the final hash.
                 result := 1 // Use `result` to temporarily denote if we will use `DOMAIN_SEP_B`.
                 signature.length := sub(signature.length, l) // Truncate the signature.
                 break
