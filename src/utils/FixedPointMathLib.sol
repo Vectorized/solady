b0e5399743c4aed02d1fac5786ed4728f4aed1ce
Vectorized <webby1111@hotmail.com>
2024-05-16 09:30:13 +0000

    Tidy

--- src/utils/FixedPointMathLib.sol
+++ src/utils/FixedPointMathLib.sol
@@ -436,21 +436,19 @@ library FixedPointMathLib {
     function fullMulDiv(uint256 x, uint256 y, uint256 d) internal pure returns (uint256 result) {
         /// @solidity memory-safe-assembly
         assembly {
-            for {} 1 {} {
-                // 512-bit multiply `[p1 p0] = x * y`.
-                // Compute the product mod `2**256` and mod `2**256 - 1`
-                // then use the Chinese Remainder Theorem to reconstruct
-                // the 512 bit result. The result is stored in two 256
-                // variables such that `product = p1 * 2**256 + p0`.
+            // 512-bit multiply `[p1 p0] = x * y`.
+            // Compute the product mod `2**256` and mod `2**256 - 1`
+            // then use the Chinese Remainder Theorem to reconstruct
+            // the 512 bit result. The result is stored in two 256
+            // variables such that `product = p1 * 2**256 + p0`.
 
-                // Least significant 256 bits of the product.
-                result := mul(x, y) // Temporarily use `result` as `p0` to save gas.
+            // Temporarily use `result` as `p0` to save gas.
+            result := mul(x, y) // Lower 256 bits of `x * y`.
+            for {} 1 {} {
                 // If overflows.
                 if iszero(mul(or(iszero(x), eq(div(result, x), y)), d)) {
                     let mm := mulmod(x, y, not(0))
-                    // Most significant 256 bits of the product.
-                    let p1 := sub(mm, add(result, lt(mm, result)))
-
+                    let p1 := sub(mm, add(result, lt(mm, result))) // Upper 256 bits of `x * y`.
                     // Make sure the result is less than `2**256`. Also prevents `d == 0`.
                     if iszero(gt(d, p1)) {
                         mstore(0x00, 0xae47f702) // `FullMulDivFailed()`.
@@ -460,13 +458,9 @@ library FixedPointMathLib {
                     /*------------------- 512 by 256 division --------------------*/
 
                     // Make division exact by subtracting the remainder from `[p1 p0]`.
-                    // Compute remainder using mulmod.
-                    let r := mulmod(x, y, d)
-                    // `t` is the least significant bit of `d`.
-                    // Always greater or equal to 1.
-                    let t := and(d, sub(0, d))
-                    // Divide `d` by `t`, which is a power of two.
-                    d := div(d, t)
+                    let r := mulmod(x, y, d) // Compute remainder using mulmod.
+                    let t := and(d, sub(0, d)) // The least significant bit of `d`. `t >= 1`.
+                    d := div(d, t) // Divide `d` by `t`, which is a power of two.
                     // Invert `d mod 2**256`
                     // Now that `d` is an odd number, it has an inverse
                     // modulo `2**256` such that `d * inv = 1 mod 2**256`.
@@ -490,8 +484,7 @@ library FixedPointMathLib {
                                 mul(sub(p1, gt(r, result)), add(div(sub(0, t), t), 1)),
                                 div(sub(result, r), t)
                             ),
-                            // inverse mod 2**256
-                            mul(inv, sub(2, mul(d, inv)))
+                            mul(inv, sub(2, mul(d, inv))) // inverse mod 2**256
                         )
                     break
                 }
