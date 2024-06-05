871502561137ad790937d5e2bcc1d3cfe83f3142
Vectorized <webby1111@hotmail.com>
2024-06-05 17:10:14 +0000

    T

--- src/utils/FixedPointMathLib.sol
+++ src/utils/FixedPointMathLib.sol
@@ -719,13 +719,12 @@ library FixedPointMathLib {
     /// @dev Returns the square root of `x`, denominated in `WAD`, rounded down.
     function sqrtWad(uint256 x) internal pure returns (uint256 z) {
         unchecked {
-            uint256 wad = 10 ** 18;
-            if (x <= type(uint256).max / wad) return sqrt(x * wad);
+            if (x <= type(uint256).max / 10 ** 18) return sqrt(x * 10 ** 18);
             z = (1 + sqrt(x)) * 10 ** 9;
-            z = (fullMulDivUnchecked(x, wad, z) + z) >> 1;
+            z = (fullMulDivUnchecked(x, 10 ** 18, z) + z) >> 1;
             // Heuristic to early return if `z` is not ceil.
-            if (rawMulMod(z, z, x) >= wad) return z;
-            uint256 t = fullMulDivUnchecked(x, wad, z);
+            if (rawMulMod(z, z, x) >= 10 ** 18) return z;
+            uint256 t = fullMulDivUnchecked(x, 10 ** 18, z);
             /// @solidity memory-safe-assembly
             assembly {
                 z := sub(z, lt(t, z))
@@ -736,13 +735,12 @@ library FixedPointMathLib {
     /// @dev Returns the cube root of `x`, denominated in `WAD`, rounded down.
     function cbrtWad(uint256 x) internal pure returns (uint256 z) {
         unchecked {
-            uint256 wadSq = 10 ** 36;
-            if (x <= type(uint256).max / wadSq) return cbrt(x * wadSq);
+            if (x <= type(uint256).max / 10 ** 36) return cbrt(x * 10 ** 36);
             z = (1 + cbrt(x)) * 10 ** 12;
-            z = (fullMulDivUnchecked(x, wadSq, z * z) + z + z) / 3;
+            z = (fullMulDivUnchecked(x, 10 ** 36, z * z) + z + z) / 3;
             // Heuristic to early return if `z` is not ceil.
-            if (rawMulMod(z * z, z, x) >= wadSq) return z;
-            uint256 t = fullMulDivUnchecked(x, wadSq, z * z);
+            if (rawMulMod(z * z, z, x) >= 10 ** 36) return z;
+            uint256 t = fullMulDivUnchecked(x, 10 ** 36, z * z);
             /// @solidity memory-safe-assembly
             assembly {
                 z := sub(z, lt(t, z))
