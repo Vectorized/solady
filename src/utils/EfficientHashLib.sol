e0a9834c8a854891048af59131ee4cf636c15706
Vectorized <webby1111@hotmail.com>
2024-06-23 12:37:13 +0000

    T

--- src/utils/EfficientHashLib.sol
+++ src/utils/EfficientHashLib.sol
@@ -343,6 +343,7 @@ library EfficientHashLib {
     }
 
     /// @dev Frees memory that has been created by `malloc`.
+    /// No-op if `buffer.length` is zero, or if new memory has been allocated after `malloc`.
     function free(bytes32[] memory buffer) internal pure {
         /// @solidity memory-safe-assembly
         assembly {
