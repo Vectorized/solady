f1e46a82bfa38fccf52e49a1101f354b52d6d4f3
Vectorized <webby1111@hotmail.com>
2024-06-23 07:49:50 +0000

    T

--- src/utils/EfficientHashLib.sol
+++ src/utils/EfficientHashLib.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 /// @notice Library for efficiently performing keccak256 hashes.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/EfficientHashLib.sol)
 library EfficientHashLib {
+    /// @dev Returns `keccak256(abi.encode(value0))`.
     function hash(bytes32 value0) internal pure returns (bytes32 result) {
         /// @solidity memory-safe-assembly
         assembly {
@@ -12,6 +13,7 @@ library EfficientHashLib {
         }
     }
 
+    /// @dev Returns `keccak256(abi.encode(value0, value1))`.
     function hash(bytes32 value0, bytes32 value1) internal pure returns (bytes32 result) {
         /// @solidity memory-safe-assembly
         assembly {
@@ -21,6 +23,7 @@ library EfficientHashLib {
         }
     }
 
+    /// @dev Returns `keccak256(abi.encode(value0, value1, value2))`.
     function hash(bytes32 value0, bytes32 value1, bytes32 value2)
         internal
         pure
@@ -29,14 +32,14 @@ library EfficientHashLib {
         /// @solidity memory-safe-assembly
         assembly {
             let m := mload(0x40)
-            mstore(0x00, value0)
-            mstore(0x20, value1)
-            mstore(0x40, value2)
-            result := keccak256(0x00, 0x60)
-            mstore(0x40, m)
+            mstore(m, value0)
+            mstore(add(m, 0x20), value1)
+            mstore(add(m, 0x40), value2)
+            result := keccak256(m, 0x60)
         }
     }
 
+    /// @dev Returns `keccak256(abi.encode(value0, value1, value2, value3))`.
     function hash(bytes32 value0, bytes32 value1, bytes32 value2, bytes32 value3)
         internal
         pure
@@ -45,16 +48,15 @@ library EfficientHashLib {
         /// @solidity memory-safe-assembly
         assembly {
             let m := mload(0x40)
-            mstore(0x00, value0)
-            mstore(0x20, value1)
-            mstore(0x40, value2)
-            mstore(0x60, value3)
-            result := keccak256(0x00, 0x80)
-            mstore(0x40, m)
-            mstore(0x60, 0)
+            mstore(m, value0)
+            mstore(add(m, 0x20), value1)
+            mstore(add(m, 0x40), value2)
+            mstore(add(m, 0x60), value3)
+            result := keccak256(m, 0x80)
         }
     }
 
+    /// @dev Returns `keccak256(abi.encode(value0, .., value4))`.
     function hash(bytes32 value0, bytes32 value1, bytes32 value2, bytes32 value3, bytes32 value4)
         internal
         pure
@@ -72,6 +74,7 @@ library EfficientHashLib {
         }
     }
 
+    /// @dev Returns `keccak256(abi.encode(value0, .., value5))`.
     function hash(
         bytes32 value0,
         bytes32 value1,
@@ -93,6 +96,7 @@ library EfficientHashLib {
         }
     }
 
+    /// @dev Returns `keccak256(abi.encode(value0, .., value6))`.
     function hash(
         bytes32 value0,
         bytes32 value1,
@@ -116,6 +120,33 @@ library EfficientHashLib {
         }
     }
 
+    /// @dev Returns `keccak256(abi.encode(value0, .., value7))`.
+    function hash(
+        bytes32 value0,
+        bytes32 value1,
+        bytes32 value2,
+        bytes32 value3,
+        bytes32 value4,
+        bytes32 value5,
+        bytes32 value6,
+        bytes32 value7
+    ) internal pure returns (bytes32 result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let m := mload(0x40)
+            mstore(m, value0)
+            mstore(add(m, 0x20), value1)
+            mstore(add(m, 0x40), value2)
+            mstore(add(m, 0x60), value3)
+            mstore(add(m, 0x80), value4)
+            mstore(add(m, 0xa0), value5)
+            mstore(add(m, 0xc0), value6)
+            mstore(add(m, 0xe0), value7)
+            result := keccak256(m, 0x100)
+        }
+    }
+
+    /// @dev Returns `keccak256(abi.encode(buffer[0], .., value[n-1]))`.
     function hash(bytes32[] memory buffer, uint256 n) internal pure returns (bytes32 result) {
         /// @solidity memory-safe-assembly
         assembly {
@@ -123,28 +154,31 @@ library EfficientHashLib {
         }
     }
 
+    /// @dev Sets `buffer[i]` to value.
     function set(bytes32[] memory buffer, uint256 i, bytes32 value) internal pure {
         /// @solidity memory-safe-assembly
         assembly {
-            mstore(add(add(buffer, 0x20), shl(5, i)), value)
+            mstore(add(buffer, shl(5, add(1, i))), value)
         }
     }
 
+    /// @dev Returns `new bytes32[](n)`, but without zeroing out the memory.
     function malloc(uint256 n) internal pure returns (bytes32[] memory buffer) {
         /// @solidity memory-safe-assembly
         assembly {
             buffer := mload(0x40)
             mstore(buffer, n)
-            mstore(0x40, add(add(buffer, 0x20), shl(5, n)))
+            mstore(0x40, add(buffer, shl(5, add(1, n))))
         }
     }
 
+    /// @dev Frees memory that has been created by `malloc`.
     function free(bytes32[] memory buffer) internal pure {
         /// @solidity memory-safe-assembly
         assembly {
-            if eq(add(add(buffer, 0x20), shl(5, mload(buffer))), mload(0x40)) {
-                mstore(0x40, buffer)
-            }
+            let m := mload(0x40)
+            let t := eq(add(buffer, shl(5, add(1, mload(buffer)))), m)
+            mstore(0x40, xor(m, mul(xor(m, buffer), t)))
         }
     }
 }
--- test/EfficientHashLib.t.sol
+++ test/EfficientHashLib.t.sol
@@ -5,11 +5,17 @@ import "./utils/SoladyTest.sol";
 import {EfficientHashLib} from "../src/utils/EfficientHashLib.sol";
 
 contract EfficientHashLibTest is SoladyTest {
+    function testEfficientHash() public {
+        testEfficientHash(0);
+    }
+
     function testEfficientHash(uint256 x) public {
-        bytes32[] memory a = EfficientHashLib.malloc(8);
+        string memory t = "01234567890123456789012345678901";
+        _checkMemory();
+        bytes32[] memory a = EfficientHashLib.malloc(10);
         _checkMemory();
         unchecked {
-            for (uint256 i; i < 8; ++i) {
+            for (uint256 i; i < 10; ++i) {
                 EfficientHashLib.set(a, i, bytes32(x ^ (i << 128)));
             }
         }
@@ -22,10 +28,18 @@ contract EfficientHashLibTest is SoladyTest {
         assertEq(EfficientHashLib.hash(a[0], a[1], a[2], a[3], a[4]), _hash(encoded, 5));
         assertEq(EfficientHashLib.hash(a[0], a[1], a[2], a[3], a[4], a[5]), _hash(encoded, 6));
         assertEq(EfficientHashLib.hash(a[0], a[1], a[2], a[3], a[4], a[5], a[6]), _hash(encoded, 7));
-        assertEq(EfficientHashLib.hash(a, 7), _hash(encoded, 7));
+        assertEq(
+            EfficientHashLib.hash(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]), _hash(encoded, 8)
+        );
+        assertEq(EfficientHashLib.hash(a, 8), _hash(encoded, 8));
         _checkMemory();
         EfficientHashLib.free(a);
         _checkMemory();
+        a = EfficientHashLib.malloc(1);
+        assertEq(t, "01234567890123456789012345678901");
+        EfficientHashLib.free(a);
+        _checkMemory();
+        assertEq(t, "01234567890123456789012345678901");
     }
 
     function _hash(bytes memory encoded, uint256 n) internal pure returns (bytes32 result) {
