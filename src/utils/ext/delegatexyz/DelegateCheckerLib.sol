be827b98f27e59355f07c575672e4d9d7c257a15
Vectorized <webby1111@hotmail.com>
2024-07-03 11:00:04 +0000

    Reduce SLOC

--- src/utils/ext/delegatexyz/DelegateCheckerLib.sol
+++ src/utils/ext/delegatexyz/DelegateCheckerLib.sol
@@ -95,18 +95,12 @@ library DelegateCheckerLib {
             mstore(add(0x2c, m), shl(96, to))
             // `checkDelegateForContract(address,address,address,bytes32)`.
             mstore(add(0x0c, m), 0x8988eea9000000000000000000000000)
-            isValid :=
-                and(
-                    eq(mload(m), 1),
-                    staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0x84, m, 0x20)
-                )
+            isValid := staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0x84, m, 0x20)
+            isValid := and(eq(mload(m), 1), isValid)
             if iszero(isValid) {
                 mstore(m, 0x90c9a2d0) // `checkDelegateForContract(address,address,address)`.
-                isValid :=
-                    and(
-                        eq(mload(m), 1),
-                        staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x64, m, 0x20)
-                    )
+                isValid := staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x64, m, 0x20)
+                isValid := and(eq(mload(m), 1), isValid)
             }
         }
     }
@@ -131,18 +125,12 @@ library DelegateCheckerLib {
             mstore(add(0x2c, m), shl(96, to))
             // `checkDelegateForContract(address,address,address,bytes32)`.
             mstore(add(0x0c, m), 0x8988eea9000000000000000000000000)
-            isValid :=
-                and(
-                    eq(mload(m), 1),
-                    staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0x84, m, 0x20)
-                )
+            isValid := staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0x84, m, 0x20)
+            isValid := and(eq(mload(m), 1), isValid)
             if iszero(or(rights, isValid)) {
                 mstore(m, 0x90c9a2d0) // `checkDelegateForContract(address,address,address)`.
-                isValid :=
-                    and(
-                        eq(mload(m), 1),
-                        staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x64, m, 0x20)
-                    )
+                isValid := staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x64, m, 0x20)
+                isValid := and(eq(mload(m), 1), isValid)
             }
         }
     }
@@ -168,18 +156,12 @@ library DelegateCheckerLib {
             mstore(add(0x2c, m), shl(96, to))
             // `checkDelegateForERC721(address,address,address,uint256,bytes32)`.
             mstore(add(0x0c, m), 0xb9f36874000000000000000000000000)
-            isValid :=
-                and(
-                    eq(mload(m), 1),
-                    staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0xa4, m, 0x20)
-                )
+            isValid := staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0xa4, m, 0x20)
+            isValid := and(eq(mload(m), 1), isValid)
             if iszero(isValid) {
                 mstore(m, 0xaba69cf8) // `checkDelegateForToken(address,address,address,uint256)`.
-                isValid :=
-                    and(
-                        eq(mload(m), 1),
-                        staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x84, m, 0x20)
-                    )
+                isValid := staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x84, m, 0x20)
+                isValid := and(eq(mload(m), 1), isValid)
             }
         }
     }
@@ -207,18 +189,12 @@ library DelegateCheckerLib {
             mstore(add(0x2c, m), shl(96, to))
             // `checkDelegateForERC721(address,address,address,uint256,bytes32)`.
             mstore(add(0x0c, m), 0xb9f36874000000000000000000000000)
-            isValid :=
-                and(
-                    eq(mload(m), 1),
-                    staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0xa4, m, 0x20)
-                )
+            isValid := staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0xa4, m, 0x20)
+            isValid := and(eq(mload(m), 1), isValid)
             if iszero(or(rights, isValid)) {
                 mstore(m, 0xaba69cf8) // `checkDelegateForToken(address,address,address,uint256)`.
-                isValid :=
-                    and(
-                        eq(mload(m), 1),
-                        staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x84, m, 0x20)
-                    )
+                isValid := staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x84, m, 0x20)
+                isValid := and(eq(mload(m), 1), isValid)
             }
         }
     }
@@ -247,8 +223,8 @@ library DelegateCheckerLib {
             mstore(add(0x2c, m), shl(96, to))
             // `checkDelegateForERC20(address,address,address,bytes32)`.
             mstore(add(0x0c, m), 0xba63c817000000000000000000000000)
-            amount :=
-                mul(mload(o), staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0x84, o, 0x20))
+            amount := staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0x84, o, 0x20)
+            amount := mul(mload(o), amount)
             if not(amount) {
                 mstore(m, 0x90c9a2d0) // `checkDelegateForContract(address,address,address)`.
                 let t := staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x64, m, 0x20)
@@ -281,10 +257,8 @@ library DelegateCheckerLib {
             mstore(add(0x2c, m), shl(96, to))
             // `checkDelegateForERC20(address,address,address,bytes32)`.
             mstore(add(0x0c, m), 0xba63c817000000000000000000000000)
-            amount :=
-                mul(
-                    mload(0x00), staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0x84, 0x00, 0x20)
-                )
+            amount := staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0x84, 0x00, 0x20)
+            amount := mul(mload(0x00), amount)
             if iszero(or(rights, iszero(not(amount)))) {
                 mstore(m, 0x90c9a2d0) // `checkDelegateForContract(address,address,address)`.
                 let t := staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x64, m, 0x20)
@@ -318,8 +292,8 @@ library DelegateCheckerLib {
             mstore(add(0x2c, m), shl(96, to))
             // `checkDelegateForERC1155(address,address,address,uint256,bytes32)`.
             mstore(add(0x0c, m), 0xb8705875000000000000000000000000)
-            amount :=
-                mul(mload(o), staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0xa4, o, 0x20))
+            amount := staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0xa4, o, 0x20)
+            amount := mul(mload(o), amount)
             if not(amount) {
                 mstore(m, 0x90c9a2d0) // `checkDelegateForContract(address,address,address)`.
                 let t := staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x64, m, 0x20)
@@ -355,10 +329,8 @@ library DelegateCheckerLib {
             mstore(add(0x2c, m), shl(96, to))
             // `checkDelegateForERC1155(address,address,address,uint256,bytes32)`.
             mstore(add(0x0c, m), 0xb8705875000000000000000000000000)
-            amount :=
-                mul(
-                    mload(0x00), staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0xa4, 0x00, 0x20)
-                )
+            amount := staticcall(gas(), DELEGATE_REGISTRY_V2, add(m, 0x1c), 0xa4, 0x00, 0x20)
+            amount := mul(mload(0x00), amount)
             if iszero(or(rights, iszero(not(amount)))) {
                 mstore(m, 0x90c9a2d0) // `checkDelegateForContract(address,address,address)`.
                 let t := staticcall(gas(), DELEGATE_REGISTRY_V1, add(m, 0x1c), 0x64, m, 0x20)
