2d8edfac49975ab9d78e984ed49a12c15e3234d7
Vectorized <webby1111@hotmail.com>
2024-07-09 23:52:23 +0000

    Tidy ERC1271

--- src/accounts/ERC1271.sol
+++ src/accounts/ERC1271.sol
@@ -19,6 +19,33 @@ abstract contract ERC1271 is EIP712 {
     /*                     ERC1271 OPERATIONS                     */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
+    /// @dev Validates the signature with ERC1271 return,
+    /// so that this account can also be used as a signer.
+    function isValidSignature(bytes32 hash, bytes calldata signature)
+        public
+        view
+        virtual
+        returns (bytes4 result)
+    {
+        bool success = _erc1271IsValidSignature(hash, _erc1271UnwrapSignature(signature));
+        /// @solidity memory-safe-assembly
+        assembly {
+            // `success ? bytes4(keccak256("isValidSignature(bytes32,bytes)")) : 0xffffffff`.
+            // We use `0xffffffff` for invalid, in convention with the reference implementation.
+            result := shl(224, or(0x1626ba7e, sub(0, iszero(success))))
+        }
+    }
+
+    /// @dev For automatic detection that the smart account supports the nested EIP-712 workflow.
+    /// By default, it returns `bytes32(bytes4(keccak256("supportsNestedTypedDataSign()")))`,
+    /// denoting support for the default behavior, as implemented in
+    /// `_erc1271IsValidSignatureViaNestedEIP712`, which is called in `isValidSignature`.
+    /// Future extensions should return a different non-zero `result` to denote different behavior.
+    /// This method intentionally returns bytes32 to allow freedom for future extensions.
+    function supportsNestedTypedDataSign() public view virtual returns (bytes32 result) {
+        result = bytes4(0xd620c85a);
+    }
+
     /// @dev Returns the ERC1271 signer.
     /// Override to return the signer `isValidSignature` checks against.
     function _erc1271Signer() internal view virtual returns (address);
@@ -33,23 +60,6 @@ abstract contract ERC1271 is EIP712 {
         return msg.sender == 0x000000000000D9ECebf3C23529de49815Dac1c4c;
     }
 
-    /// @dev Validates the signature with ERC1271 return,
-    /// so that this account can also be used as a signer.
-    function isValidSignature(bytes32 hash, bytes calldata signature)
-        public
-        view
-        virtual
-        returns (bytes4 result)
-    {
-        bool success = _erc1271IsValidSignature(hash, signature);
-        /// @solidity memory-safe-assembly
-        assembly {
-            // `success ? bytes4(keccak256("isValidSignature(bytes32,bytes)")) : 0xffffffff`.
-            // We use `0xffffffff` for invalid, in convention with the reference implementation.
-            result := shl(224, or(0x1626ba7e, sub(0, iszero(success))))
-        }
-    }
-
     /// @dev Returns whether the `hash` and `signature` are valid.
     /// Override if you need non-ECDSA logic.
     function _erc1271IsValidSignatureNowCalldata(bytes32 hash, bytes calldata signature)
@@ -61,6 +71,29 @@ abstract contract ERC1271 is EIP712 {
         return SignatureCheckerLib.isValidSignatureNowCalldata(_erc1271Signer(), hash, signature);
     }
 
+    /// @dev Unwraps and returns the signature.
+    function _erc1271UnwrapSignature(bytes calldata signature)
+        internal
+        view
+        virtual
+        returns (bytes calldata result)
+    {
+        result = signature;
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Unwraps the ERC6492 wrapper if it exists.
+            // See: https://eips.ethereum.org/EIPS/eip-6492
+            if eq(
+                calldataload(add(result.offset, sub(result.length, 0x20))),
+                mul(0x6492, div(not(mload(0x60)), 0xffff)) // `0x6492...6492`.
+            ) {
+                let o := add(result.offset, calldataload(add(result.offset, 0x40)))
+                result.length := calldataload(o)
+                result.offset := add(o, 0x20)
+            }
+        }
+    }
+
     /// @dev Returns whether the `signature` is valid for the `hash.
     function _erc1271IsValidSignature(bytes32 hash, bytes calldata signature)
         internal
@@ -68,19 +101,6 @@ abstract contract ERC1271 is EIP712 {
         virtual
         returns (bool)
     {
-        /// @solidity memory-safe-assembly
-        assembly {
-            // Unwraps the ERC6492 wrapper if it exists.
-            // See: https://eips.ethereum.org/EIPS/eip-6492
-            if eq(
-                calldataload(add(signature.offset, sub(signature.length, 0x20))),
-                mul(0x6492, div(not(mload(0x60)), 0xffff)) // `0x6492...6492`.
-            ) {
-                let o := add(signature.offset, calldataload(add(signature.offset, 0x40)))
-                signature.length := calldataload(o)
-                signature.offset := add(o, 0x20)
-            }
-        }
         return _erc1271IsValidSignatureViaSafeCaller(hash, signature)
             || _erc1271IsValidSignatureViaNestedEIP712(hash, signature)
             || _erc1271IsValidSignatureViaRPC(hash, signature);
@@ -95,16 +115,6 @@ abstract contract ERC1271 is EIP712 {
         returns (bool result)
     {
         if (_erc1271CallerIsSafe()) result = _erc1271IsValidSignatureNowCalldata(hash, signature);
-    }
-
-    /// @dev For automatic detection that the smart account supports the nested EIP-712 workflow.
-    /// By default, it returns `bytes32(bytes4(keccak256("supportsNestedTypedDataSign()")))`,
-    /// denoting support for the default behavior, as implemented in
-    /// `_erc1271IsValidSignatureViaNestedEIP712`, which is called in `isValidSignature`.
-    /// Future extensions should return a different non-zero `result` to denote different behavior.
-    /// This method intentionally returns bytes32 to allow freedom for future extensions.
-    function supportsNestedTypedDataSign() public view virtual returns (bytes32 result) {
-        result = bytes4(0xd620c85a);
     }
 
     /// @dev ERC1271 signature validation (Nested EIP-712 workflow).
@@ -188,53 +198,54 @@ abstract contract ERC1271 is EIP712 {
         /// @solidity memory-safe-assembly
         assembly {
             let m := mload(0x40) // Cache the free memory pointer.
-            // Length of the contents type.
-            let c := and(0xffff, calldataload(add(signature.offset, sub(signature.length, 0x20))))
+            // `c` is `contentsType.length`, which is stored in the last 2 bytes of the signature.
+            let c := shr(240, calldataload(add(signature.offset, sub(signature.length, 2))))
             for {} 1 {} {
                 let l := add(0x42, c) // Total length of appended data (32 + 32 + c + 2).
-                let o := add(signature.offset, sub(signature.length, l))
-                calldatacopy(0x20, o, 0x40) // Copy the `APP_DOMAIN_SEPARATOR` and contents struct hash.
+                let o := add(signature.offset, sub(signature.length, l)) // Offset of appended data.
                 mstore(0x00, 0x1901) // Store the "\x19\x01" prefix.
-                // Use the `PersonalSign` workflow if the reconstructed contents hash doesn't match,
-                // or if the appended data is invalid (length too long, or empty contents type).
+                calldatacopy(0x20, o, 0x40) // Copy the `APP_DOMAIN_SEPARATOR` and `contents` struct hash.
+                // Use the `PersonalSign` workflow if the reconstructed hash doesn't match,
+                // or if the appended data is invalid, i.e.
+                // `appendedData.length > signature.length || contentsType.length == 0`.
                 if or(xor(keccak256(0x1e, 0x42), hash), or(lt(signature.length, l), iszero(c))) {
-                    mstore(0x00, _PERSONAL_SIGN_TYPEHASH)
+                    t := 0 // Set `t` to 0, denoting that we need to `hash = _hashTypedData(hash)`.
+                    mstore(t, _PERSONAL_SIGN_TYPEHASH)
                     mstore(0x20, hash) // Store the `prefixed`.
-                    hash := keccak256(0x00, 0x40) // Compute the `PersonalSign` struct hash.
+                    hash := keccak256(t, 0x40) // Compute the `PersonalSign` struct hash.
                     break
                 }
                 // Else, use the `TypedDataSign` workflow.
-                mstore(m, "TypedDataSign(") // To construct `TYPED_DATA_SIGN_TYPEHASH` on-the-fly.
-                let p := add(m, 0x0e) // Advance 14 bytes.
-                calldatacopy(p, add(o, 0x40), c) // Copy the contents type.
-                let d := byte(0, mload(p)) // For denoting if the contents name is invalid.
-                d := or(gt(26, sub(d, 97)), eq(40, d)) // Starts with lowercase or '('.
+                // `TypedDataSign({ContentsName} contents,bytes1 fields,...){ContentsType}`.
+                mstore(m, "TypedDataSign(") // Store the start of `TypedDataSign`'s type encoding.
+                let p := add(m, 0x0e) // Advance 14 bytes to skip "TypedDataSign(".
+                calldatacopy(p, add(o, 0x40), c) // Copy `contentsType` to extract `contentsName`.
+                // `d & 1 == 1` means that `contentsName` is invalid.
+                let d := shr(byte(0, mload(p)), 0x7fffffe000000000000010000000000) // Starts with `[a-z(]`.
                 // Store the end sentinel '(', and advance `p` until we encounter a '(' byte.
-                for { mstore(add(p, c), 40) } 1 { p := add(p, 1) } {
-                    let b := byte(0, mload(p))
-                    if eq(40, b) { break }
-                    d := or(d, shr(b, 0x120100000001)) // Has a byte in ", )\x00".
+                for { mstore(add(p, c), 40) } iszero(eq(byte(0, mload(p)), 40)) { p := add(p, 1) } {
+                    d := or(shr(byte(0, mload(p)), 0x120100000001), d) // Has a byte in ", )\x00".
                 }
-                mstore(p, " contents,bytes1 fields,string n")
+                mstore(p, " contents,bytes1 fields,string n") // Store the rest of the encoding.
                 mstore(add(p, 0x20), "ame,string version,uint256 chain")
                 mstore(add(p, 0x40), "Id,address verifyingContract,byt")
                 mstore(add(p, 0x60), "es32 salt,uint256[] extensions)")
-                calldatacopy(add(p, 0x7f), add(o, 0x40), c) // Copy the contents type.
+                p := add(p, 0x7f)
+                calldatacopy(p, add(o, 0x40), c) // Copy `contentsType`.
                 // Fill in the missing fields of the `TypedDataSign`.
-                calldatacopy(t, o, 0x40) // Copy `contents` to `add(t, 0x20)`.
-                mstore(t, keccak256(m, sub(add(add(p, 0x7f), c), m))) // `TYPED_DATA_SIGN_TYPEHASH`.
+                calldatacopy(t, o, 0x40) // Copy the `contents` struct hash to `add(t, 0x20)`.
+                mstore(t, keccak256(m, sub(add(p, c), m))) // Store `TYPED_DATA_SIGN_TYPEHASH`.
                 // The "\x19\x01" prefix is already at 0x00.
                 // `APP_DOMAIN_SEPARATOR` is already at 0x20.
                 mstore(0x40, keccak256(t, 0x120)) // `hashStruct(typedDataSign)`.
-                // Compute the final hash, corrupted if the contents name is invalid.
+                // Compute the final hash, corrupted if `contentsName` is invalid.
                 hash := keccak256(0x1e, add(0x42, and(1, d)))
-                result := 1 // Use `result` to temporarily denote if we will use `APP_DOMAIN_SEPARATOR`.
                 signature.length := sub(signature.length, l) // Truncate the signature.
                 break
             }
             mstore(0x40, m) // Restore the free memory pointer.
         }
-        if (!result) hash = _hashTypedData(hash);
+        if (t == bytes32(0)) hash = _hashTypedData(hash); // `PersonalSign` workflow.
         result = _erc1271IsValidSignatureNowCalldata(hash, signature);
     }
 
@@ -253,7 +264,7 @@ abstract contract ERC1271 is EIP712 {
         assembly {
             m := mload(0x40) // Grab the free memory pointer.
             mstore(0x40, add(m, 0x120)) // Allocate the memory.
-            // Skip 2 words: `TYPED_DATA_SIGN_TYPEHASH, contents`.
+            // Skip 2 words for the `TYPED_DATA_SIGN_TYPEHASH` and `contents` struct hash.
             mstore(add(m, 0x40), shl(248, byte(0, fields)))
             mstore(add(m, 0x60), keccak256(add(name, 0x20), mload(name)))
             mstore(add(m, 0x80), keccak256(add(version, 0x20), mload(version)))
