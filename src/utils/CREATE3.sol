aeafd33a5ff7f00a213bd3abda765fd343a11901
Vectorized <webby1111@hotmail.com>
2024-07-13 13:38:01 +0000

    Make SSTORE2 deployments use CREATE3

--- src/utils/CREATE3.sol
+++ src/utils/CREATE3.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-/// @notice Deploy to deterministic addresses without an initcode factor.
+/// @notice Deterministic deployments agnostic to the initialization code.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/CREATE3.sol)
 /// @author Modified from Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/CREATE3.sol)
 /// @author Modified from 0xSequence (https://github.com/0xSequence/create3/blob/master/contracts/Create3.sol)
@@ -12,9 +12,6 @@ library CREATE3 {
 
     /// @dev Unable to deploy the contract.
     error DeploymentFailed();
-
-    /// @dev Unable to initialize the contract.
-    error InitializationFailed();
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                      BYTECODE CONSTANTS                    */
@@ -44,12 +41,12 @@ library CREATE3 {
      * -------------------------------------------------------------------+
      */
 
-    /// @dev The proxy bytecode.
-    uint256 private constant _PROXY_BYTECODE = 0x67363d3d37363d34f03d5260086018f3;
+    /// @dev The proxy initialization code.
+    uint256 private constant _PROXY_INITCODE = 0x67363d3d37363d34f03d5260086018f3;
 
-    /// @dev Hash of the `_PROXY_BYTECODE`.
+    /// @dev Hash of the `_PROXY_INITCODE`.
     /// Equivalent to `keccak256(abi.encodePacked(hex"67363d3d37363d34f03d5260086018f3"))`.
-    bytes32 private constant _PROXY_BYTECODE_HASH =
+    bytes32 internal constant PROXY_INITCODE_HASH =
         0x21c35dbe1b344a2488cf3321d6ce542f8e9f305544ff09e4993a62319a497c1f;
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -76,52 +73,25 @@ library CREATE3 {
     {
         /// @solidity memory-safe-assembly
         assembly {
-            // Store the `_PROXY_BYTECODE` into scratch space.
-            mstore(0x00, _PROXY_BYTECODE)
-            // Deploy a new contract with our pre-made bytecode via CREATE2.
+            mstore(0x00, _PROXY_INITCODE) // Store the `_PROXY_INITCODE`.
             let proxy := create2(0, 0x10, 0x10, salt)
-
-            // If the result of `create2` is the zero address, revert.
             if iszero(proxy) {
-                // Store the function selector of `DeploymentFailed()`.
-                mstore(0x00, 0x30116425)
-                // Revert with (offset, size).
+                mstore(0x00, 0x30116425) // `DeploymentFailed()`.
                 revert(0x1c, 0x04)
             }
-
-            // Store the proxy's address.
-            mstore(0x14, proxy)
+            mstore(0x14, proxy) // Store the proxy's address.
             // 0xd6 = 0xc0 (short RLP prefix) + 0x16 (length of: 0x94 ++ proxy ++ 0x01).
             // 0x94 = 0x80 + 0x14 (0x14 = the length of an address, 20 bytes, in hex).
             mstore(0x00, 0xd694)
-            // Nonce of the proxy contract (1).
-            mstore8(0x34, 0x01)
-
+            mstore8(0x34, 0x01) // Nonce of the proxy contract (1).
             deployed := keccak256(0x1e, 0x17)
-
-            // If the `call` fails, revert.
             if iszero(
-                call(
-                    gas(), // Gas remaining.
-                    proxy, // Proxy's address.
-                    value, // Ether value.
-                    add(initCode, 0x20), // Start of `initCode`.
-                    mload(initCode), // Length of `initCode`.
-                    0x00, // Offset of output.
-                    0x00 // Length of output.
+                mul( // The arguments of `mul` are evaluated last to first.
+                    extcodesize(deployed),
+                    call(gas(), proxy, value, add(initCode, 0x20), mload(initCode), 0x00, 0x00)
                 )
             ) {
-                // Store the function selector of `InitializationFailed()`.
-                mstore(0x00, 0x19b991a8)
-                // Revert with (offset, size).
-                revert(0x1c, 0x04)
-            }
-
-            // If the code size of `deployed` is zero, revert.
-            if iszero(extcodesize(deployed)) {
-                // Store the function selector of `InitializationFailed()`.
-                mstore(0x00, 0x19b991a8)
-                // Revert with (offset, size).
+                mstore(0x00, 0x30116425) // `DeploymentFailed()`.
                 revert(0x1c, 0x04)
             }
         }
@@ -140,27 +110,18 @@ library CREATE3 {
     {
         /// @solidity memory-safe-assembly
         assembly {
-            // Cache the free memory pointer.
-            let m := mload(0x40)
-            // Store `deployer`.
-            mstore(0x00, deployer)
-            // Store the prefix.
-            mstore8(0x0b, 0xff)
-            // Store the salt.
-            mstore(0x20, salt)
-            // Store the bytecode hash.
-            mstore(0x40, _PROXY_BYTECODE_HASH)
+            let m := mload(0x40) // Cache the free memory pointer.
+            mstore(0x00, deployer) // Store `deployer`.
+            mstore8(0x0b, 0xff) // Store the prefix.
+            mstore(0x20, salt) // Store the salt.
+            mstore(0x40, PROXY_INITCODE_HASH) // Store the bytecode hash.
 
-            // Store the proxy's address.
-            mstore(0x14, keccak256(0x0b, 0x55))
-            // Restore the free memory pointer.
-            mstore(0x40, m)
+            mstore(0x14, keccak256(0x0b, 0x55)) // Store the proxy's address.
+            mstore(0x40, m) // Restore the free memory pointer.
             // 0xd6 = 0xc0 (short RLP prefix) + 0x16 (length of: 0x94 ++ proxy ++ 0x01).
             // 0x94 = 0x80 + 0x14 (0x14 = the length of an address, 20 bytes, in hex).
             mstore(0x00, 0xd694)
-            // Nonce of the proxy contract (1).
-            mstore8(0x34, 0x01)
-
+            mstore8(0x34, 0x01) // Nonce of the proxy contract (1).
             deployed := keccak256(0x1e, 0x17)
         }
     }
