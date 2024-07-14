f0e6ac241e86a3a766f0f4fa32cfdd1b2c23f9a4
Vectorized <webby1111@hotmail.com>
2024-07-14 18:38:45 +0000

    T

--- test/SSTORE2.t.sol
+++ test/SSTORE2.t.sol
@@ -225,4 +225,29 @@ contract SSTORE2Test is SoladyTest {
             mstore(result, n) // Store the length of `result`.
         }
     }
+
+    function testReadBoundsCheckTrick(uint256 dataLength, uint256 start) public {
+        dataLength = _bound(dataLength, 0, 0xffffffff);
+        bool expected = !(dataLength + 1 > start) || start == type(uint256).max;
+        bool computed;
+        /// @solidity memory-safe-assembly
+        assembly {
+            let pointerCodesize := add(dataLength, 1)
+            computed := iszero(gt(pointerCodesize, start))
+        }
+        assertEq(computed, expected);
+    }
+
+    function testReadBoundsCheckTrick(uint256 dataLength, uint256 start, uint256 end) public {
+        dataLength = _bound(dataLength, 0, 0xffffffff);
+        bool expected = !(dataLength + 1 > end && start <= end) || start == type(uint256).max
+            || end == type(uint256).max;
+        bool computed;
+        /// @solidity memory-safe-assembly
+        assembly {
+            let pointerCodesize := add(dataLength, 1)
+            computed := iszero(gt(gt(pointerCodesize, end), gt(start, end)))
+        }
+        assertEq(computed, expected);
+    }
 }
