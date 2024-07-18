2b1043a9c04d8d373fdb2eb3fff4eade616786ec
Vectorized <webby1111@hotmail.com>
2024-07-18 11:16:59 +0000

    T

--- src/utils/GasBurnerLib.sol
+++ src/utils/GasBurnerLib.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.8.4;
 
 /// @notice Library for burning gas without reverting.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/GasBurnerLib.sol)
+///
+/// @dev Intended for Contract Secured Revenue (CSR).
+///
+/// Recommendation: pass in an admin-controlled dynamic value instead of a hardcoded one.
+/// This is so that you can adjust your contract as needed depending on market conditions,
+/// and to give you and your users a leeway in case the L2 chain change the rules.
 library GasBurnerLib {
     /// @dev Burns approximately `x` amount of gas.
-    /// Intended for Contract Secured Revenue (CSR).
-    ///
-    /// Recommendation: pass in an admin-controlled dynamic value instead of a hardcoded one.
-    /// This is so that you can adjust your contract as needed depending on market conditions,
-    /// and to give you and your users a leeway in case the L2 chain change the rules.
-    function burn(uint256 x) internal pure {
+    function burnPure(uint256 x) internal pure {
         /// @solidity memory-safe-assembly
         assembly {
             mstore(0x10, or(1, x))
@@ -20,6 +21,51 @@ library GasBurnerLib {
                 mstore(0x10, keccak256(0x10, 0x10)) // Yes.
             }
             if iszero(mload(0x10)) { invalid() }
+        }
+    }
+
+    /// @dev Burns approximately `x` amount of gas.
+    function burnView(uint256 x) internal view {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let n := mul(gt(x, 3500), div(x, 3200))
+            let m := mload(0x40)
+            mstore(0x00, xor(origin(), timestamp()))
+            mstore(0x00, keccak256(0x00, 0x20))
+            mstore(0x20, 27) // `v`.
+            mstore(0x40, 45) // `r`.
+            mstore(0x60, 10) // `s`.
+            for { let i := 0 } iszero(eq(i, n)) { i := add(i, 1) } {
+                pop(staticcall(gas(), 1, 0x00, 0x81, 0x00, 0x20))
+            }
+            if iszero(mload(0x10)) { invalid() }
+            mstore(0x60, 0) // Restore the zero slot.
+            mstore(0x40, m) // Restore the free memory pointer.
+        }
+    }
+
+    /// @dev Burns approximately `x` amount of gas.
+    function burn(uint256 x) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let m := mload(0x40)
+            let n := mul(gt(x, 18000), div(x, 17700))
+            mstore(m, xor(origin(), timestamp()))
+            codecopy(add(m, 0x20), and(keccak256(m, 0x20), 0xff), 2080)
+            for { let i := 0 } 1 { i := add(i, 1) } {
+                let h := keccak256(m, 0x21)
+                mstore(m, h)
+                codecopy(add(m, and(h, 0x7ff)), and(0xff, h), 0xff)
+                mstore(add(m, 2048), not(h))
+                if eq(i, n) {
+                    n := shr(3, mod(x, 17700))
+                    n := mul(gt(n, 0x30), sub(n, 0x30))
+                    mstore(add(m, n), h)
+                    log0(m, add(n, 0x20))
+                    break
+                }
+                log0(m, 2080)
+            }
         }
     }
 }
--- test/GasBurnerLib.t.sol
+++ test/GasBurnerLib.t.sol
@@ -7,20 +7,65 @@ import {GasBurnerLib} from "../src/utils/GasBurnerLib.sol";
 contract GasBurnerLibTest is SoladyTest {
     event LogGasBurn(uint256 required, uint256 actual);
 
-    function testBurnGas() public {
-        _testBurnGas(0);
-        _testBurnGas(1);
-        _testBurnGas(110);
-        _testBurnGas(119);
-        _testBurnGas(120);
-        _testBurnGas(121);
-        _testBurnGas(300);
+    function testBurnPure() public {
+        _testBurnPure(0);
+        _testBurnPure(1);
+        _testBurnPure(110);
+        _testBurnPure(119);
+        _testBurnPure(120);
+        _testBurnPure(121);
+        _testBurnPure(300);
         for (uint256 x = 300; x < 9000; x += 32) {
-            _testBurnGas(x);
+            _testBurnPure(x);
         }
     }
 
-    function _testBurnGas(uint256 x) internal {
+    function testBurnPureTiming() public pure {
+        GasBurnerLib.burnPure(300000);
+    }
+
+    function testBurnView() public {
+        _testBurnView(1 * 3000);
+        _testBurnView(2 * 3000);
+        _testBurnView(3 * 3000);
+        _testBurnView(4 * 3000);
+        _testBurnView(5 * 3000);
+        _testBurnView(9 * 3000);
+    }
+
+    function testBurn() public {
+        _testBurn(20000);
+        _testBurn(30000);
+        _testBurn(50000);
+    }
+
+    function testBurnViewTiming() public view {
+        GasBurnerLib.burnView(300000);
+    }
+
+    function testBurnTiming() public {
+        GasBurnerLib.burn(300000);
+    }
+
+    function _testBurnPure(uint256 x) internal {
+        unchecked {
+            uint256 gasBefore = gasleft();
+            GasBurnerLib.burnPure(x);
+            uint256 gasAfter = gasleft();
+            emit LogGasBurn(x, gasBefore - gasAfter);
+        }
+    }
+
+    function _testBurnView(uint256 x) internal {
+        unchecked {
+            uint256 gasBefore = gasleft();
+            GasBurnerLib.burnView(x);
+            uint256 gasAfter = gasleft();
+            emit LogGasBurn(x, gasBefore - gasAfter);
+        }
+    }
+
+    function _testBurn(uint256 x) internal {
         unchecked {
             uint256 gasBefore = gasleft();
             GasBurnerLib.burn(x);
