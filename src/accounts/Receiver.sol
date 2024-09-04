659810a7ac7c3765b967263c70132fc3ae039da9
Vectorized <webby1111@hotmail.com>
2024-09-04 12:35:37 +0000

    Make receiverFallback more customizable

--- src/accounts/Receiver.sol
+++ src/accounts/Receiver.sol
@@ -17,17 +17,32 @@ abstract contract Receiver {
 
     /// @dev Modifier for the fallback function to handle token callbacks.
     modifier receiverFallback() virtual {
-        /// @solidity memory-safe-assembly
-        assembly {
-            let s := shr(224, calldataload(0))
-            // 0x150b7a02: `onERC721Received(address,address,uint256,bytes)`.
-            // 0xf23a6e61: `onERC1155Received(address,address,uint256,uint256,bytes)`.
-            // 0xbc197c81: `onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)`.
-            if or(eq(s, 0x150b7a02), or(eq(s, 0xf23a6e61), eq(s, 0xbc197c81))) {
-                mstore(0x20, s) // Store `msg.sig`.
-                return(0x3c, 0x20) // Return `msg.sig`.
+        _beforeReceiverFallbackBody();
+        if (_useReceiverFallbackBody()) {
+            /// @solidity memory-safe-assembly
+            assembly {
+                let s := shr(224, calldataload(0))
+                // 0x150b7a02: `onERC721Received(address,address,uint256,bytes)`.
+                // 0xf23a6e61: `onERC1155Received(address,address,uint256,uint256,bytes)`.
+                // 0xbc197c81: `onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)`.
+                if or(eq(s, 0x150b7a02), or(eq(s, 0xf23a6e61), eq(s, 0xbc197c81))) {
+                    mstore(0x20, s) // Store `msg.sig`.
+                    return(0x3c, 0x20) // Return `msg.sig`.
+                }
             }
         }
+        _afterReceiverFallbackBody();
         _;
     }
+
+    /// @dev Whether we want to use the body of the `receiverFallback` modifier.
+    function _useReceiverFallbackBody() internal view virtual returns (bool) {
+        return true;
+    }
+
+    /// @dev Called before the body of the `receiverFallback` modifier.
+    function _beforeReceiverFallbackBody() internal virtual {}
+
+    /// @dev Called after the body of the `receiverFallback` modifier.
+    function _afterReceiverFallbackBody() internal virtual {}
 }
