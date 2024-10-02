e9dfaf6970b1985938d87afc4bb7e7a5103fe2e8
Vectorized <webby1111@hotmail.com>
2024-10-02 09:51:37 +0000

    Make mothership internal

--- test/utils/mocks/MockPod.sol
+++ test/utils/mocks/MockPod.sol
@@ -7,6 +7,10 @@ import {Brutalizer} from "../Brutalizer.sol";
 /// @dev WARNING! This mock is strictly intended for testing purposes only.
 /// Do NOT copy anything here into production code unless you really know what you are doing.
 contract MockPod is Pod, Brutalizer {
+    function mothership() public view returns (address) {
+        return _mothership();
+    }
+
     function initializeMothership(address initialMothership) public {
         _initializeMothership(_brutalized(initialMothership));
     }
