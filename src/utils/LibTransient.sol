34038f4355abdf942a139918f0d4ec35a5a69406
Vectorized <webby1111@hotmail.com>
2024-11-05 02:02:19 +0800

    ✨ LibTransient (#1145)

---
+++ src/utils/LibTransient.sol
@@ -0,0 +1,426 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @notice Library for RLP encoding and CREATE address computation.
+/// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibTransient.sol)
+/// @author Modified from Transient Goodies by Philogy (https://github.com/Philogy/transient-goodies/blob/main/src/TransientBytesLib.sol)
+library LibTransient {
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                          STRUCTS                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Pointer struct to a `uint256` in transient storage.
+    struct TUint256 {
+        uint256 _spacer;
+    }
+
+    /// @dev Pointer struct to a `int256` in transient storage.
+    struct TInt256 {
+        uint256 _spacer;
+    }
+
+    /// @dev Pointer struct to a `bytes32` in transient storage.
+    struct TBytes32 {
+        uint256 _spacer;
+    }
+
+    /// @dev Pointer struct to a `address` in transient storage.
+    struct TAddress {
+        uint256 _spacer;
+    }
+
+    /// @dev Pointer struct to a `bool` in transient storage.
+    struct TBool {
+        uint256 _spacer;
+    }
+
+    /// @dev Pointer struct to a `bytes` in transient storage.
+    struct TBytes {
+        uint256 _spacer;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     UINT256 OPERATIONS                     */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Returns a pointer to a `uint256` in transient storage.
+    function tUint256(bytes32 tSlot) internal pure returns (TUint256 storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns a pointer to a `uint256` in transient storage.
+    function tUint256(uint256 tSlot) internal pure returns (TUint256 storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns the value at transient `ptr`.
+    function get(TUint256 storage ptr) internal view returns (uint256 result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := tload(ptr.slot)
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr`.
+    function set(TUint256 storage ptr, uint256 value) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, value)
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr` to zero.
+    function clear(TUint256 storage ptr) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, 0)
+        }
+    }
+
+    /// @dev Increments the value at transient `ptr` by 1.
+    function inc(TUint256 storage ptr) internal returns (uint256 newValue) {
+        set(ptr, newValue = get(ptr) + 1);
+    }
+
+    /// @dev Increments the value at transient `ptr` by `delta`.
+    function inc(TUint256 storage ptr, uint256 delta) internal returns (uint256 newValue) {
+        set(ptr, newValue = get(ptr) + delta);
+    }
+
+    /// @dev Decrements the value at transient `ptr` by 1.
+    function dec(TUint256 storage ptr) internal returns (uint256 newValue) {
+        set(ptr, newValue = get(ptr) - 1);
+    }
+
+    /// @dev Decrements the value at transient `ptr` by `delta`.
+    function dec(TUint256 storage ptr, uint256 delta) internal returns (uint256 newValue) {
+        set(ptr, newValue = get(ptr) - delta);
+    }
+
+    /// @dev Increments the value at transient `ptr` by `delta`.
+    function incSigned(TUint256 storage ptr, int256 delta) internal returns (uint256 newValue) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let currentValue := tload(ptr.slot)
+            newValue := add(currentValue, delta)
+            if iszero(eq(lt(newValue, currentValue), slt(delta, 0))) {
+                mstore(0x00, 0x4e487b71) // `Panic(uint256)`.
+                mstore(0x20, 0x11) // Underflow or overflow panic.
+                revert(0x1c, 0x24)
+            }
+            tstore(ptr.slot, newValue)
+        }
+    }
+
+    /// @dev Decrements the value at transient `ptr` by `delta`.
+    function decSigned(TUint256 storage ptr, int256 delta) internal returns (uint256 newValue) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let currentValue := tload(ptr.slot)
+            newValue := sub(currentValue, delta)
+            if iszero(eq(lt(newValue, currentValue), sgt(delta, 0))) {
+                mstore(0x00, 0x4e487b71) // `Panic(uint256)`.
+                mstore(0x20, 0x11) // Underflow or overflow panic.
+                revert(0x1c, 0x24)
+            }
+            tstore(ptr.slot, newValue)
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     INT256 OPERATIONS                      */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Returns a pointer to a `int256` in transient storage.
+    function tInt256(bytes32 tSlot) internal pure returns (TInt256 storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns a pointer to a `int256` in transient storage.
+    function tInt256(uint256 tSlot) internal pure returns (TInt256 storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns the value at transient `ptr`.
+    function get(TInt256 storage ptr) internal view returns (int256 result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := tload(ptr.slot)
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr`.
+    function set(TInt256 storage ptr, int256 value) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, value)
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr` to zero.
+    function clear(TInt256 storage ptr) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, 0)
+        }
+    }
+
+    /// @dev Increments the value at transient `ptr` by 1.
+    function inc(TInt256 storage ptr) internal returns (int256 newValue) {
+        set(ptr, newValue = get(ptr) + 1);
+    }
+
+    /// @dev Increments the value at transient `ptr` by `delta`.
+    function inc(TInt256 storage ptr, int256 delta) internal returns (int256 newValue) {
+        set(ptr, newValue = get(ptr) + delta);
+    }
+
+    /// @dev Decrements the value at transient `ptr` by 1.
+    function dec(TInt256 storage ptr) internal returns (int256 newValue) {
+        set(ptr, newValue = get(ptr) - 1);
+    }
+
+    /// @dev Decrements the value at transient `ptr` by `delta`.
+    function dec(TInt256 storage ptr, int256 delta) internal returns (int256 newValue) {
+        set(ptr, newValue = get(ptr) - delta);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     BYTES32 OPERATIONS                     */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Returns a pointer to a `bytes32` in transient storage.
+    function tBytes32(bytes32 tSlot) internal pure returns (TBytes32 storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns a pointer to a `bytes32` in transient storage.
+    function tBytes32(uint256 tSlot) internal pure returns (TBytes32 storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns the value at transient `ptr`.
+    function get(TBytes32 storage ptr) internal view returns (bytes32 result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := tload(ptr.slot)
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr`.
+    function set(TBytes32 storage ptr, bytes32 value) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, value)
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr` to zero.
+    function clear(TBytes32 storage ptr) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, 0)
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     ADDRESS OPERATIONS                     */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Returns a pointer to a `address` in transient storage.
+    function tAddress(bytes32 tSlot) internal pure returns (TAddress storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns a pointer to a `address` in transient storage.
+    function tAddress(uint256 tSlot) internal pure returns (TAddress storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns the value at transient `ptr`.
+    function get(TAddress storage ptr) internal view returns (address result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := tload(ptr.slot)
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr`.
+    function set(TAddress storage ptr, address value) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, shr(96, shl(96, value)))
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr` to zero.
+    function clear(TAddress storage ptr) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, 0)
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                      BOOL OPERATIONS                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Returns a pointer to a `bool` in transient storage.
+    function tBool(bytes32 tSlot) internal pure returns (TBool storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns a pointer to a `bool` in transient storage.
+    function tBool(uint256 tSlot) internal pure returns (TBool storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns the value at transient `ptr`.
+    function get(TBool storage ptr) internal view returns (bool result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := tload(ptr.slot)
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr`.
+    function set(TBool storage ptr, bool value) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, iszero(iszero(value)))
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr` to zero.
+    function clear(TBool storage ptr) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, 0)
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                      BYTES OPERATIONS                      */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Returns a pointer to a `bytes` in transient storage.
+    function tBytes(bytes32 tSlot) internal pure returns (TBytes storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns a pointer to a `bytes` in transient storage.
+    function tBytes(uint256 tSlot) internal pure returns (TBytes storage ptr) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            ptr.slot := tSlot
+        }
+    }
+
+    /// @dev Returns the length of the bytes stored at transient `ptr`.
+    function length(TBytes storage ptr) internal view returns (uint256 result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := shr(224, tload(ptr.slot))
+        }
+    }
+
+    /// @dev Returns the bytes stored at transient `ptr`.
+    function get(TBytes storage ptr) internal view returns (bytes memory result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result := mload(0x40)
+            mstore(result, 0x00)
+            mstore(add(result, 0x1c), tload(ptr.slot)) // Length and first `0x1c` bytes.
+            let n := mload(result)
+            let e := add(add(result, 0x20), n)
+            if iszero(lt(n, 0x1d)) {
+                mstore(0x00, ptr.slot)
+                let o := add(result, 0x3c)
+                for { let d := sub(keccak256(0x00, 0x20), o) } 1 {} {
+                    mstore(o, tload(add(o, d)))
+                    o := add(o, 0x20)
+                    if iszero(lt(o, e)) { break }
+                }
+            }
+            mstore(e, 0) // Zeroize the slot after the string.
+            mstore(0x40, add(0x20, e)) // Allocate memory.
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr`.
+    function set(TBytes storage ptr, bytes memory value) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, mload(add(value, 0x1c)))
+            if iszero(lt(mload(value), 0x1d)) {
+                mstore(0x00, ptr.slot)
+                let e := add(add(value, 0x20), mul(gt(0x100000000, mload(value)), mload(value)))
+                let o := add(value, 0x3c)
+                for { let d := sub(keccak256(0x00, 0x20), o) } 1 {} {
+                    tstore(add(o, d), mload(o))
+                    o := add(o, 0x20)
+                    if iszero(lt(o, e)) { break }
+                }
+            }
+        }
+    }
+
+    /// @dev Sets the value at transient `ptr`.
+    function setCalldata(TBytes storage ptr, bytes calldata value) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, calldataload(sub(value.offset, 0x04)))
+            if iszero(lt(value.length, 0x1d)) {
+                mstore(0x00, ptr.slot)
+                let e := add(value.offset, mul(gt(0x100000000, value.length), value.length))
+                let o := add(value.offset, 0x1c)
+                for { let d := sub(keccak256(0x00, 0x20), o) } 1 {} {
+                    tstore(add(o, d), calldataload(o))
+                    o := add(o, 0x20)
+                    if iszero(lt(o, e)) { break }
+                }
+            }
+        }
+    }
+
+    /// @dev Clears the value at transient `ptr`.
+    function clear(TBytes storage ptr) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            tstore(ptr.slot, 0)
+        }
+    }
+}
---
+++ test/LibTransient.t.sol
@@ -0,0 +1,198 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./utils/SoladyTest.sol";
+import {LibTransient} from "../src/utils/LibTransient.sol";
+
+contract LibTransientTest is SoladyTest {
+    using LibTransient for *;
+
+    function testSetAndGetBytesTransient() public {
+        _testSetAndGetBytesTransient("123");
+        _testSetAndGetBytesTransient("12345678901234567890123456789012345678901234567890");
+        _testSetAndGetBytesTransient("123");
+    }
+
+    function _testSetAndGetBytesTransient(bytes memory data) internal {
+        LibTransient.TBytes storage p = LibTransient.tBytes(uint256(0));
+        p.set(data);
+        assertEq(p.length(), data.length);
+        assertEq(p.get(), data);
+    }
+
+    function testSetAndGetBytesTransientCalldata(
+        uint256 tSlot,
+        bytes calldata data0,
+        bytes calldata data1
+    ) public {
+        unchecked {
+            LibTransient.TBytes storage p0 = LibTransient.tBytes(tSlot);
+            LibTransient.TBytes storage p1 = LibTransient.tBytes(tSlot + 1);
+            if (_randomChance(2)) {
+                p0.setCalldata(data0);
+                p1.setCalldata(data1);
+            } else {
+                p0.set(data0);
+                p1.set(data1);
+            }
+            assertEq(p0.get(), data0);
+            assertEq(p1.get(), data1);
+            if (_randomChance(2)) {
+                p0.setCalldata(data1);
+                p1.setCalldata(data0);
+            } else {
+                p0.set(data1);
+                p1.set(data0);
+            }
+            assertEq(p0.get(), data1);
+            assertEq(p1.get(), data0);
+            p0.clear();
+            assertEq(p0.length(), 0);
+            assertEq(p0.get(), "");
+            assertEq(p1.get(), data0);
+            p1.clear();
+            assertEq(p1.length(), 0);
+            assertEq(p1.get(), "");
+            assertEq(p0.length(), 0);
+            assertEq(p0.get(), "");
+        }
+    }
+
+    function testSetAndGetBytesTransient(uint256 tSlot, bytes memory data) public {
+        LibTransient.TBytes storage p = LibTransient.tBytes(tSlot);
+        if (_randomChance(8)) data = _randomBytes();
+        p.set(data);
+        assertEq(p.length(), data.length);
+        if (_randomChance(8)) {
+            _misalignFreeMemoryPointer();
+            _brutalizeMemory();
+        }
+        bytes memory retrieved = p.get();
+        _checkMemory(retrieved);
+        assertEq(retrieved, data);
+        p.clear();
+        assertEq(p.length(), 0);
+        assertEq(p.get(), "");
+    }
+
+    function testSetAndGetBytesTransientCalldata(uint256 tSlot, bytes calldata data) public {
+        LibTransient.TBytes storage p = LibTransient.tBytes(tSlot);
+        p.set(data);
+        assertEq(p.length(), data.length);
+        assertEq(p.get(), data);
+        p.clear();
+        assertEq(p.length(), 0);
+        assertEq(p.get(), "");
+    }
+
+    function testSetAndGetUint256Transient(uint256 tSlot, uint256 value) public {
+        LibTransient.TUint256 storage p = LibTransient.tUint256(tSlot);
+        p.set(value);
+        assertEq(p.get(), value);
+        p.clear();
+        assertEq(p.get(), 0);
+    }
+
+    function testSetAndGetAddressTransient(uint256 tSlot, address value) public {
+        LibTransient.TAddress storage p = LibTransient.tAddress(tSlot);
+        p.set(_brutalized(value));
+        assertEq(p.get(), value);
+        p.clear();
+        assertEq(p.get(), address(0));
+    }
+
+    function testSetAndGetBytes32Transient(uint256 tSlot, bytes32 value) public {
+        LibTransient.TBytes32 storage p = LibTransient.tBytes32(tSlot);
+        p.set(value);
+        assertEq(p.get(), value);
+        p.clear();
+        assertEq(p.get(), bytes32(0));
+    }
+
+    function testSetAndGetBoolTransient(uint256 tSlot, bool value) public {
+        LibTransient.TBool storage p = LibTransient.tBool(tSlot);
+        p.set(_brutalized(value));
+        assertEq(p.get(), value);
+        p.clear();
+        assertEq(p.get(), false);
+    }
+
+    function testUint256IncDecTransient() public {
+        uint256 tSlot;
+        LibTransient.TUint256 storage p = LibTransient.tUint256(tSlot);
+        p.set(10);
+        assertEq(this.tUintInc(tSlot), 11);
+        assertEq(p.get(), 11);
+        assertEq(this.tUintInc(tSlot, 20), 31);
+        assertEq(p.get(), 31);
+        p.set(2 ** 256 - 2);
+        assertEq(this.tUintInc(tSlot), 2 ** 256 - 1);
+        assertEq(p.get(), 2 ** 256 - 1);
+        vm.expectRevert();
+        this.tUintInc(tSlot);
+        vm.expectRevert();
+        this.tUintInc(tSlot, 10);
+        assertEq(this.tUintDec(tSlot), 2 ** 256 - 2);
+        assertEq(p.get(), 2 ** 256 - 2);
+        p.set(10);
+        assertEq(this.tUintDec(tSlot, 5), 5);
+        assertEq(p.get(), 5);
+        assertEq(this.tUintDec(tSlot, 5), 0);
+        assertEq(p.get(), 0);
+        vm.expectRevert();
+        this.tUintDec(tSlot);
+        vm.expectRevert();
+        this.tUintDec(tSlot, 5);
+        p.set(10);
+        assertEq(this.tUintIncSigned(tSlot, 1), 11);
+        assertEq(p.get(), 11);
+        assertEq(this.tUintIncSigned(tSlot, -1), 10);
+        assertEq(p.get(), 10);
+        assertEq(this.tUintDecSigned(tSlot, 1), 9);
+        assertEq(p.get(), 9);
+        assertEq(this.tUintDecSigned(tSlot, -1), 10);
+        assertEq(p.get(), 10);
+    }
+
+    function tUintIncSigned(uint256 tSlot, int256 delta) public returns (uint256) {
+        return LibTransient.tUint256(tSlot).incSigned(delta);
+    }
+
+    function tUintDecSigned(uint256 tSlot, int256 delta) public returns (uint256) {
+        return LibTransient.tUint256(tSlot).decSigned(delta);
+    }
+
+    function tUintInc(uint256 tSlot, uint256 delta) public returns (uint256) {
+        return LibTransient.tUint256(tSlot).inc(delta);
+    }
+
+    function tUintDec(uint256 tSlot, uint256 delta) public returns (uint256) {
+        return LibTransient.tUint256(tSlot).dec(delta);
+    }
+
+    function tUintInc(uint256 tSlot) public returns (uint256) {
+        return LibTransient.tUint256(tSlot).inc();
+    }
+
+    function tUintDec(uint256 tSlot) public returns (uint256) {
+        return LibTransient.tUint256(tSlot).dec();
+    }
+
+    function testInt256IncDecTransient() public {}
+
+    function tIntInc(uint256 tSlot, int256 delta) public returns (int256) {
+        return LibTransient.tInt256(tSlot).inc(delta);
+    }
+
+    function tIntDec(uint256 tSlot, int256 delta) public returns (int256) {
+        return LibTransient.tInt256(tSlot).dec(delta);
+    }
+
+    function tIntInc(uint256 tSlot) public returns (int256) {
+        return LibTransient.tInt256(tSlot).inc();
+    }
+
+    function tIntDec(uint256 tSlot) public returns (int256) {
+        return LibTransient.tInt256(tSlot).dec();
+    }
+}
