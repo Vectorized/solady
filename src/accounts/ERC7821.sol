420911c1c3893e00bed7b324f7d9cd3b1042ae72
Vectorized <webby1111@hotmail.com>
2024-11-22 21:53:39 +0000

    T

--- src/accounts/ERC7821.sol
+++ src/accounts/ERC7821.sol
@@ -10,9 +10,9 @@ contract ERC7821 {
 
     /// @dev Call struct for the `execute` function.
     struct Call {
-        address target; // Treated as `address(this)` if `address(0)`.
-        uint256 value; // Amount of native currency (i.e. Ether).
-        bytes data; // The calldata to send with the call.
+        address target; // Replaced as `address(this)` if `address(0)`.
+        uint256 value; // Amount of native currency (i.e. Ether) to send.
+        bytes data; // Calldata to send with the call.
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -63,7 +63,8 @@ contract ERC7821 {
             assembly {
                 // Direct extract the arguments of `call[i]` without bounds checks.
                 let c := add(calls.offset, calldataload(add(calls.offset, shl(5, i))))
-                target := calldataload(c)
+                // Replaces `target` with `address(this)` if `address(0)` is provided.
+                target := or(mul(address(), iszero(calldataload(c))), calldataload(c))
                 value := calldataload(add(c, 0x20))
                 let o := add(c, calldataload(add(c, 0x40)))
                 data.offset := add(o, 0x20)
@@ -89,10 +90,7 @@ contract ERC7821 {
         assembly {
             result := mload(0x40) // Grab the free memory pointer.
             calldatacopy(result, data.offset, data.length)
-            // Defaults `target` to `address(this)` if `address(0)` is provided.
-            // forgefmt: disable-next-item
-            if iszero(call(gas(), or(target, mul(iszero(target), address())),
-                value, result, data.length, codesize(), 0x00)) {
+            if iszero(call(gas(), target, value, result, data.length, codesize(), 0x00)) {
                 // Bubble up the revert if the call reverts.
                 returndatacopy(result, 0x00, returndatasize())
                 revert(result, returndatasize())
