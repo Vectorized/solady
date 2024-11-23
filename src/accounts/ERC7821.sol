e8759bd554b217103b291784a6dcf8143138e231
Vectorized <webby1111@hotmail.com>
2024-11-23 19:59:41 +0800

    ♻️ Update ERC7821 (#1174)
    * Optimize ERC7821
    
    * Optimize 7821
    
    * Add comments
    
    * T

--- src/accounts/ERC7821.sol
+++ src/accounts/ERC7821.sol
@@ -3,24 +3,17 @@ pragma solidity ^0.8.4;
 
 /// @notice Minimal batch executor mixin.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/accounts/ERC7821.sol)
-abstract contract ERC7821 {
+contract ERC7821 {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                          STRUCTS                           */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     /// @dev Call struct for the `execute` function.
     struct Call {
-        address target;
-        uint256 value;
-        bytes data;
+        address target; // Replaced as `address(this)` if `address(0)`.
+        uint256 value; // Amount of native currency (i.e. Ether) to send.
+        bytes data; // Calldata to send with the call.
     }
-
-    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
-    /*                   FUNCTIONS TO OVERRIDE                    */
-    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
-
-    /// @dev Ensures that `execute` can only be called by the correct caller or `opData`.
-    function _authorizeExecute(Call[] calldata calls, bytes calldata opData) internal virtual;
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                    EXECUTION OPERATIONS                    */
@@ -32,15 +25,21 @@ abstract contract ERC7821 {
         public
         payable
         virtual
-        returns (bytes[] memory results)
+        returns (bytes[] memory)
     {
-        _authorizeExecute(calls, opData);
-        return _execute(calls);
+        // Very basic auth to only allow this contract to be called by itself.
+        // Override `execute` to perform more complex auth with `opData`.
+        if (opData.length == uint256(0)) {
+            require(msg.sender == address(this));
+            // Remember to return `_execute(calls)` when you override `execute`.
+            return _execute(calls);
+        }
+        revert(); // In your override, replace this with logic to operate on `opData`.
     }
 
     /// @dev This function is provided for frontends to detect support.
     function minimalBatchExecutorVersion() public pure virtual returns (uint256) {
-        return 1; // This number may change.
+        return 1; // This number may be updated in the future.
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -62,8 +61,10 @@ abstract contract ERC7821 {
             bytes calldata data;
             /// @solidity memory-safe-assembly
             assembly {
+                // Direct extract the arguments of `call[i]` without bounds checks.
                 let c := add(calls.offset, calldataload(add(calls.offset, shl(5, i))))
-                target := calldataload(c)
+                // Replaces `target` with `address(this)` if `address(0)` is provided.
+                target := or(mul(address(), iszero(calldataload(c))), calldataload(c))
                 value := calldataload(add(c, 0x20))
                 let o := add(c, calldataload(add(c, 0x40)))
                 data.offset := add(o, 0x20)
--- test/utils/mocks/MockERC7821.sol
+++ test/utils/mocks/MockERC7821.sol
@@ -7,11 +7,15 @@ import {Brutalizer} from "../Brutalizer.sol";
 /// @dev WARNING! This mock is strictly intended for testing purposes only.
 /// Do NOT copy anything here into production code unless you really know what you are doing.
 contract MockERC7821 is ERC7821, Brutalizer {
-    function _authorizeExecute(Call[] calldata calls, bytes calldata opData)
-        internal
+    function execute(Call[] calldata calls, bytes calldata)
+        public
+        payable
         virtual
         override
-    {}
+        returns (bytes[] memory)
+    {
+        return _execute(calls);
+    }
 
     function executeDirect(Call[] calldata calls)
         public
