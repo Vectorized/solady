ceb1019a774a04c2ee1a283d3db5a3dfcb7ab8c2
Vectorized <webby1111@hotmail.com>
2024-11-26 02:18:48 +0800

    ✨ LibERC7579 (#1178)

---
+++ src/accounts/LibERC7579.sol
@@ -0,0 +1,285 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @notice Library for interacting with ERC6551 accounts.
+/// @author Solady (https://github.com/vectorized/solady/blob/main/src/accounts/LibERC7579.sol)
+/// @author Modified from OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/main/contracts/account/utils/draft-ERC7579Utils.sol)
+library LibERC7579 {
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       CUSTOM ERRORS                        */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Cannot decode `executionData`.
+    error DecodingError();
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         CONSTANTS                          */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev A single execution.
+    bytes1 internal constant CALLTYPE_SINGLE = 0x00;
+
+    /// @dev A batch of executions.
+    bytes1 internal constant CALLTYPE_BATCH = 0x01;
+
+    /// @dev A `delegatecall` execution.
+    bytes1 internal constant CALLTYPE_DELEGATECALL = 0xff;
+
+    /// @dev Default execution type that reverts on failure.
+    bytes1 internal constant EXECTYPE_DEFAULT = 0x00;
+
+    /// @dev Execution type that does not revert on failure.
+    bytes1 internal constant EXECTYPE_TRY = 0x01;
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                      MODE OPERATIONS                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Encodes the fields into a mode.
+    function encodeMode(bytes1 callType, bytes1 execType, bytes4 selector, bytes22 payload)
+        internal
+        pure
+        returns (bytes32 result)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x00, callType)
+            mstore(0x01, execType)
+            mstore(0x02, selector)
+            mstore(0x06, 0)
+            mstore(0x0a, payload)
+            result := mload(0x00)
+        }
+    }
+
+    /// @dev Returns the call type of the mode.
+    function getCallType(bytes32 mode) internal pure returns (bytes1) {
+        return bytes1(mode);
+    }
+
+    /// @dev Returns the call type of the mode.
+    function getExecType(bytes32 mode) internal pure returns (bytes1) {
+        return mode[1];
+    }
+
+    /// @dev Returns the selector of the mode.
+    function getSelector(bytes32 mode) internal pure returns (bytes4) {
+        return bytes4(bytes32(uint256(mode) << 16));
+    }
+
+    /// @dev Returns the payload stored in the mode.
+    function getPayload(bytes32 mode) internal pure returns (bytes22) {
+        return bytes22(bytes32(uint256(mode) << 80));
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*             EXECUTION DATA DECODING OPERATIONS             */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Decodes a single call execution.
+    /// Reverts if `executionData` is not correctly encoded.
+    function decodeSingle(bytes calldata executionData)
+        internal
+        pure
+        returns (address target, uint256 value, bytes calldata data)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            if iszero(gt(executionData.length, 0x33)) {
+                mstore(0x00, 0xba597e7e) // `DecodingError()`.
+                revert(0x1c, 0x04)
+            }
+            target := shr(96, calldataload(executionData.offset))
+            value := calldataload(add(executionData.offset, 0x14))
+            data.offset := add(executionData.offset, 0x34)
+            data.length := sub(executionData.length, 0x34)
+        }
+    }
+
+    /// @dev Decodes a single call execution without bounds checks.
+    function decodeSingleUnchecked(bytes calldata executionData)
+        internal
+        pure
+        returns (address target, uint256 value, bytes calldata data)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            target := shr(96, calldataload(executionData.offset))
+            value := calldataload(add(executionData.offset, 0x14))
+            data.offset := add(executionData.offset, 0x34)
+            data.length := sub(executionData.length, 0x34)
+        }
+    }
+
+    /// @dev Decodes a single delegate execution.
+    /// Reverts if `executionData` is not correctly encoded.
+    function decodeDelegate(bytes calldata executionData)
+        internal
+        pure
+        returns (address target, bytes calldata data)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            if iszero(gt(executionData.length, 0x13)) {
+                mstore(0x00, 0xba597e7e) // `DecodingError()`.
+                revert(0x1c, 0x04)
+            }
+            target := shr(96, calldataload(executionData.offset))
+            data.offset := add(executionData.offset, 0x14)
+            data.length := sub(executionData.length, 0x14)
+        }
+    }
+
+    /// @dev Decodes a single delegate execution without bounds checks.
+    function decodeDelegateUnchecked(bytes calldata executionData)
+        internal
+        pure
+        returns (address target, bytes calldata data)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            target := shr(96, calldataload(executionData.offset))
+            data.offset := add(executionData.offset, 0x14)
+            data.length := sub(executionData.length, 0x14)
+        }
+    }
+
+    /// @dev Decodes a batch.
+    /// Reverts if `executionData` is not correctly encoded.
+    function decodeBatch(bytes calldata executionData)
+        internal
+        pure
+        returns (bytes32[] calldata pointers)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let u := calldataload(executionData.offset)
+            if or(shr(64, u), gt(0x20, executionData.length)) {
+                mstore(0x00, 0xba597e7e) // `DecodingError()`.
+                revert(0x1c, 0x04)
+            }
+            pointers.offset := add(add(executionData.offset, u), 0x20)
+            pointers.length := calldataload(add(executionData.offset, u))
+            if pointers.length {
+                let e := sub(add(executionData.offset, executionData.length), 0x20)
+                // Perform bounds checks on the decoded `pointers`.
+                // Does an out-of-gas revert.
+                for { let i := pointers.length } 1 {} {
+                    i := sub(i, 1)
+                    let p := calldataload(add(pointers.offset, shl(5, i)))
+                    let c := add(pointers.offset, p)
+                    let q := calldataload(add(c, 0x40))
+                    let o := add(c, q)
+                    // forgefmt: disable-next-item
+                    if or(shr(64, or(calldataload(o), or(p, q))),
+                        or(gt(add(c, 0x40), e), gt(add(o, calldataload(o)), e))) {
+                        mstore(0x00, 0xba597e7e) // `DecodingError()`.
+                        revert(0x1c, 0x04)
+                    }
+                    if iszero(i) { break }
+                }
+            }
+        }
+    }
+
+    /// @dev Decodes a batch without bounds checks.
+    /// This function can be used in `execute`, if the validation phase has already
+    /// decoded the `executionData` with checks via `decodeBatch`.
+    function decodeBatchUnchecked(bytes calldata executionData)
+        internal
+        pure
+        returns (bytes32[] calldata pointers)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let o := add(executionData.offset, calldataload(executionData.offset))
+            pointers.offset := add(o, 0x20)
+            pointers.length := calldataload(o)
+        }
+    }
+
+    /// @dev Decodes a batch and optional `opData`.
+    /// Reverts if `executionData` is not correctly encoded.
+    function decodeBatchAndOpData(bytes calldata executionData)
+        internal
+        pure
+        returns (bytes32[] calldata pointers, bytes calldata opData)
+    {
+        opData = emptyCalldataBytes();
+        pointers = decodeBatch(executionData);
+        if (hasOpData(executionData)) {
+            /// @solidity memory-safe-assembly
+            assembly {
+                let e := sub(add(executionData.offset, executionData.length), 0x20)
+                let p := calldataload(add(0x20, executionData.offset))
+                let q := add(executionData.offset, p)
+                opData.offset := add(q, 0x20)
+                opData.length := calldataload(q)
+                if or(shr(64, or(opData.length, p)), gt(add(q, opData.length), e)) {
+                    mstore(0x00, 0xba597e7e) // `DecodingError()`.
+                    revert(0x1c, 0x04)
+                }
+            }
+        }
+    }
+
+    /// @dev Decodes a batch without bounds checks.
+    /// This function can be used in `execute`, if the validation phase has already
+    /// decoded the `executionData` with checks via `decodeBatchAndOpData`.
+    function decodeBatchAndOpDataUnchecked(bytes calldata executionData)
+        internal
+        pure
+        returns (bytes32[] calldata pointers, bytes calldata opData)
+    {
+        opData = emptyCalldataBytes();
+        pointers = decodeBatchUnchecked(executionData);
+        if (hasOpData(executionData)) {
+            /// @solidity memory-safe-assembly
+            assembly {
+                let q := add(executionData.offset, calldataload(add(0x20, executionData.offset)))
+                opData.offset := add(q, 0x20)
+                opData.length := calldataload(q)
+            }
+        }
+    }
+
+    /// @dev Returns whether the `executionData` has optional `opData`.
+    function hasOpData(bytes calldata executionData) internal pure returns (bool result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result :=
+                iszero(or(lt(executionData.length, 0x40), lt(calldataload(executionData.offset), 0x40)))
+        }
+    }
+
+    /// @dev Returns the `i`th execution at `pointers`, without bounds checks.
+    /// The bounds check is excluded as this function is intended to be called in a bounded loop.
+    function getExecution(bytes32[] calldata pointers, uint256 i)
+        internal
+        pure
+        returns (address target, uint256 value, bytes calldata data)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let c := add(pointers.offset, calldataload(add(pointers.offset, shl(5, i))))
+            target := calldataload(c)
+            value := calldataload(add(c, 0x20))
+            let o := add(c, calldataload(add(c, 0x40)))
+            data.offset := add(o, 0x20)
+            data.length := calldataload(o)
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                          HELPERS                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Helper function to return empty calldata bytes.
+    function emptyCalldataBytes() internal pure returns (bytes calldata result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            result.offset := 0
+            result.length := 0
+        }
+    }
+}
---
+++ test/LibERC7579.t.sol
@@ -0,0 +1,212 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./utils/SoladyTest.sol";
+import {LibERC7579} from "../src/accounts/LibERC7579.sol";
+
+contract LibERC7579Test is SoladyTest {
+    function testEncodeAndDecodeMode(bytes32) public {
+        bytes1 callType = bytes1(bytes32(_randomUniform()));
+        bytes1 execType = bytes1(bytes32(_randomUniform()));
+        bytes4 selector = bytes4(bytes32(_randomUniform()));
+        bytes22 payload = bytes22(bytes32(_randomUniform()));
+        bytes32 mode = LibERC7579.encodeMode(callType, execType, selector, payload);
+        assertEq(LibERC7579.getCallType(mode), callType);
+        assertEq(LibERC7579.getExecType(mode), execType);
+        assertEq(LibERC7579.getSelector(mode), selector);
+        assertEq(LibERC7579.getPayload(mode), payload);
+    }
+
+    struct Call {
+        address target;
+        uint256 value;
+        bytes data;
+    }
+
+    struct _TestTemps {
+        address target;
+        uint256 value;
+        bytes data;
+        Call[] calls;
+        bytes opData;
+    }
+
+    function testDecodeSingle(address target, uint256 value, bytes memory data) public {
+        bytes memory executionData = abi.encodePacked(target, value, data);
+        _TestTemps memory t;
+        (t.target, t.value, t.data) = this.decodeSingle(executionData);
+        assertEq(t.target, target);
+        assertEq(t.value, value);
+        assertEq(t.data, data);
+    }
+
+    function decodeSingle(bytes calldata executionData)
+        public
+        pure
+        returns (address, uint256, bytes memory)
+    {
+        return LibERC7579.decodeSingle(executionData);
+    }
+
+    function testDecodeDelegate(address target, bytes memory data) public {
+        bytes memory executionData = abi.encodePacked(target, data);
+        _TestTemps memory t;
+        (t.target, t.data) = this.decodeDelegate(executionData);
+        assertEq(t.target, target);
+        assertEq(t.data, data);
+    }
+
+    function decodeDelegate(bytes calldata executionData)
+        public
+        pure
+        returns (address, bytes memory)
+    {
+        return LibERC7579.decodeDelegate(executionData);
+    }
+
+    function testDecodeBatchAndOpData(bytes32) public {
+        Call[] memory calls = new Call[](_randomUniform() & 3);
+        bytes memory opData = _truncateBytes(_randomBytes(), 0x1ff);
+        for (uint256 i; i != calls.length; ++i) {
+            Call memory c = calls[i];
+            c.target = address(uint160(_randomUniform()));
+            c.value = _random();
+            c.data = _truncateBytes(_randomBytes(), 0x1ff);
+        }
+        _TestTemps memory t;
+        bool useOpData = _randomChance(2);
+        if (useOpData) {
+            (t.calls, t.opData) = this.decodeBatchAndOpData(abi.encode(calls, opData));
+        } else {
+            bytes memory executionData;
+            if (_randomChance(2)) {
+                executionData = abi.encode(calls);
+            } else {
+                executionData = abi.encode(calls, opData);
+            }
+            if (_randomChance(2)) {
+                t.calls = this.decodeBatch(executionData);
+            } else {
+                (t.calls, t.opData) = this.decodeBatchAndOpData(executionData);
+            }
+        }
+
+        assertEq(t.calls.length, calls.length);
+        for (uint256 i; i != calls.length; ++i) {
+            assertEq(t.calls[i].target, calls[i].target);
+            assertEq(t.calls[i].value, calls[i].value);
+            assertEq(t.calls[i].data, calls[i].data);
+        }
+        if (useOpData) {
+            assertEq(t.opData, opData);
+        }
+
+        if (calls.length > 0 && _randomChance(8)) {
+            uint256 i = _bound(_randomUniform(), 0, calls.length - 1);
+            (t.target, t.value, t.data) = this.decodeBatchAndGetExecution(abi.encode(calls), i);
+            assertEq(t.target, calls[i].target);
+            assertEq(t.value, calls[i].value);
+            assertEq(t.data, calls[i].data);
+        }
+    }
+
+    function decodeBatch(bytes calldata executionData) public pure returns (Call[] memory) {
+        Call[] calldata calls;
+        bytes32[] calldata pointers = LibERC7579.decodeBatch(executionData);
+        /// @solidity memory-safe-assembly
+        assembly {
+            calls.offset := pointers.offset
+            calls.length := pointers.length
+        }
+        return calls;
+    }
+
+    function decodeBatchAndOpData(bytes calldata executionData)
+        public
+        pure
+        returns (Call[] memory, bytes memory)
+    {
+        Call[] calldata calls;
+        (bytes32[] calldata pointers, bytes calldata opData) =
+            LibERC7579.decodeBatchAndOpData(executionData);
+        /// @solidity memory-safe-assembly
+        assembly {
+            calls.offset := pointers.offset
+            calls.length := pointers.length
+        }
+        return (calls, opData);
+    }
+
+    function testDecodeBatchAndOpDataReverts(bytes32) public {
+        bytes memory opData = hex"3232323232323232323232323232323232323232323232323232323232323232";
+        Call[] memory calls = new Call[](1);
+        calls[0].target = address(this);
+        calls[0].value = 1 ether;
+        calls[0].data = hex"5656565656565656565656565656565656565656565656565656565656565656";
+        bytes memory executionData = abi.encode(calls, opData);
+        if (_randomChance(128)) {
+            // Check that it works.
+            this.decodeBatchAndOpData(executionData);
+        }
+        // 0000000000000000000000000000000000000000000000000000000000000040 : 0x20
+        // 0000000000000000000000000000000000000000000000000000000000000120 : 0x40
+        // 0000000000000000000000000000000000000000000000000000000000000001 : 0x60
+        // 0000000000000000000000000000000000000000000000000000000000000020 : 0x80
+        // 0000000000000000000000007fa9385be102ac3eac297483dd6233d62b3e1496 : 0xa0
+        // 0000000000000000000000000000000000000000000000000de0b6b3a7640000 : 0xc0
+        // 0000000000000000000000000000000000000000000000000000000000000060 : 0xe0
+        // 0000000000000000000000000000000000000000000000000000000000000020 : 0x100
+        // 5656565656565656565656565656565656565656565656565656565656565656 : 0x120
+        // 0000000000000000000000000000000000000000000000000000000000000020 : 0x140
+        // 3232323232323232323232323232323232323232323232323232323232323232 : 0x160
+
+        if (_randomChance(4)) {
+            _testDecodeBatchAndOpDataRevert(executionData, 0x20, 0x140);
+        }
+        if (_randomChance(4)) {
+            _testDecodeBatchAndOpDataRevert(executionData, 0x60, 0x02);
+        }
+        if (_randomChance(4)) {
+            _testDecodeBatchAndOpDataRevert(executionData, 0x40, 0x140);
+        }
+        if (_randomChance(4)) {
+            _testDecodeBatchAndOpDataRevert(executionData, 0x140, 0x21);
+        }
+        if (_randomChance(4)) {
+            _testDecodeBatchAndOpDataRevert(executionData, 0x100, 0x61);
+        }
+        if (_randomChance(4)) {
+            _testDecodeBatchAndOpDataRevert(executionData, 0x80, 0x1c0);
+        }
+    }
+
+    function _testDecodeBatchAndOpDataRevert(
+        bytes memory executionData,
+        uint256 o,
+        uint256 startingFrom
+    ) internal {
+        uint256 r = _randomLengthOrOffset(startingFrom);
+        bytes memory cd = abi.encodeWithSignature("decodeBatchAndOpData(bytes)", executionData);
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(add(o, add(cd, 0x44)), r)
+        }
+        (bool success,) = address(this).staticcall(cd);
+        assertFalse(success);
+    }
+
+    function _randomLengthOrOffset(uint256 startingFrom) internal returns (uint256) {
+        if (_randomChance(2)) {
+            return _bound(_random(), startingFrom, startingFrom + 0x1ff);
+        }
+        return _bound(_random(), startingFrom, type(uint256).max);
+    }
+
+    function decodeBatchAndGetExecution(bytes calldata executionData, uint256 i)
+        public
+        pure
+        returns (address, uint256, bytes memory)
+    {
+        return LibERC7579.getExecution(LibERC7579.decodeBatch(executionData), i);
+    }
+}
