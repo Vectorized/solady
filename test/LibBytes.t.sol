3d0f41a9b83cbbddc0e2d3844efc0a63809d4db8
Vectorized <webby1111@hotmail.com>
2024-11-27 17:22:06 +0800

    ✨ LibBytes sliceCalldata and emptyCalldata (#1186)

--- test/LibBytes.t.sol
+++ test/LibBytes.t.sol
@@ -31,4 +31,19 @@ contract LibBytesTest is SoladyTest {
         bytes memory truncated = LibBytes.truncatedCalldata(a, n);
         assertEq(truncated, sliced);
     }
+
+    function testSliceCalldata(bytes calldata a, uint256 start, uint256 end) public {
+        bytes memory aCopy = a;
+        assertEq(LibBytes.sliceCalldata(a, start, end), LibBytes.slice(aCopy, start, end));
+        assertEq(LibBytes.sliceCalldata(a, start), LibBytes.slice(aCopy, start));
+    }
+
+    function testSliceCalldata() public {
+        bytes memory data = hex"12f712c77281c66267d947165237893ba5eca3e5481727fe76d4511ce1b564f5";
+        this.testSliceCalldata(data, 1, 11);
+    }
+
+    function testEmptyCalldata() public {
+        assertEq(LibBytes.emptyCalldata(), "");
+    }
 }
