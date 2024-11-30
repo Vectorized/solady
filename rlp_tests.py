716c236ede42af99621f9ffe81b99639e9aa9272
Vectorized <webby1111@hotmail.com>
2024-11-30 19:54:56 +0000

    T

--- rlp_tests.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-for i in range(56,58):
-	s = '00' * i
-	command = 'cast to-rlp "0x' + s + '"'
-	print(i)
-	print(command)
-	result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-	print(result.stdout)
