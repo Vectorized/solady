32b3942e5bd993934233ee95d3c22a914b33fa1e
Vectorized <webby1111@hotmail.com>
2024-12-02 09:22:07 +0800

    ♻️ Remove Pod (#1192)

--- src/Milady.sol
+++ src/Milady.sol
@@ -8,7 +8,6 @@ import "./accounts/ERC6551.sol";
 import "./accounts/ERC6551Proxy.sol";
 import "./accounts/ERC7821.sol";
 import "./accounts/LibERC6551.sol";
-import "./accounts/Pod.sol";
 import "./accounts/Receiver.sol";
 import "./auth/Ownable.sol";
 import "./auth/OwnableRoles.sol";
