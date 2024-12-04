0502c32e6c3c1ab543b1fcfe86478fbca53efb93
Vectorized <webby1111@hotmail.com>
2024-12-04 04:47:17 +0000

    T

--- src/utils/ext/ithaca/BLS.sol
+++ src/utils/ext/ithaca/BLS.sol
@@ -17,14 +17,14 @@ library BLS {
     // See: https://eips.ethereum.org/EIPS/eip-2537
 
     /// @dev A representation of a base field element (Fp) in the BLS12-381 curve.
-    /// Due to the size of `p`, 
+    /// Due to the size of `p`,
     /// `0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab`
     /// the top 16 bytes are always zeroes.
     struct Fp {
         bytes32 a; // Upper 32 bytes.
         bytes32 b; // Lower 32 bytes.
     }
-    
+
     /// @dev A representation of an extension field element (Fp2) in the BLS12-381 curve.
     struct Fp2 {
         bytes32 c0_a;
@@ -32,7 +32,7 @@ library BLS {
         bytes32 c1_a;
         bytes32 c1_b;
     }
-    
+
     /// @dev A representation of a point on the G1 curve of BLS12-381.
     struct G1Point {
         bytes32 x_a;
@@ -40,7 +40,7 @@ library BLS {
         bytes32 y_a;
         bytes32 y_b;
     }
-    
+
     struct G2Point {
         bytes32 x_c0_a;
         bytes32 x_c0_b;
@@ -61,31 +61,31 @@ library BLS {
 
     /// @dev For scalar multiplication of a point on the BLS12-381 G1 curve.
     address internal constant BLS12_G1MUL = 0x000000000000000000000000000000000000000C;
-    
+
     /// @dev For multi-scalar multiplication (MSM) on the BLS12-381 G1 curve.
     address internal constant BLS12_G1MSM = 0x000000000000000000000000000000000000000d;
-    
+
     /// @dev For addition of two points on the BLS12-381 G2 curve.
     address internal constant BLS12_G2ADD = 0x000000000000000000000000000000000000000E;
-    
+
     /// @dev For scalar multiplication of a point on the BLS12-381 G2 curve,
     address internal constant BLS12_G2MUL = 0x000000000000000000000000000000000000000F;
-    
+
     /// @dev For multi-scalar multiplication (MSM) on the BLS12-381 G2 curve.
     address internal constant BLS12_G2MSM = 0x0000000000000000000000000000000000000010;
-    
+
     /// @dev For performing a pairing check on the BLS12-381 curve.
     address internal constant BLS12_PAIRING_CHECK = 0x0000000000000000000000000000000000000011;
-    
+
     /// @dev For mapping a Fp to a point on the BLS12-381 G1 curve.
     address internal constant BLS12_MAP_FP_TO_G1 = 0x0000000000000000000000000000000000000012;
-    
+
     /// @dev For mapping a Fp2 to a point on the BLS12-381 G2 curve.
     address internal constant BLS12_MAP_FP2_TO_G2 = 0x0000000000000000000000000000000000000013;
-    
+
     /// @dev For modular exponentiation.
     address internal constant MOD_EXP = 0x0000000000000000000000000000000000000005;
-    
+
     /// @dev For sha2-256.
     address internal constant SHA2_256 = 0x0000000000000000000000000000000000000002;
 
@@ -95,28 +95,28 @@ library BLS {
 
     /// @dev The G1Add operation failed.
     error G1AddFailed();
-    
+
     /// @dev The G1Mul operation failed.
     error G1MulFailed();
-    
+
     /// @dev The G1MSM operation failed.
     error G1MSMFailed();
-    
+
     /// @dev The G2Add operation failed.
     error G2AddFailed();
-    
+
     /// @dev The G2Mul operation failed.
     error G2MulFailed();
-    
+
     /// @dev The G2MSM operation failed.
     error G2MSMFailed();
-    
+
     /// @dev The pairing operation failed.
     error PairingFailed();
-    
+
     /// @dev The MapFpToG1 operation failed.
     error MapFpToG1Failed();
-    
+
     /// @dev The MapFpToG2 operation failed.
     error MapFp2ToG2Failed();
 
@@ -124,11 +124,15 @@ library BLS {
     /*                         OPERATIONS                         */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    function add(G1Point memory point0, G1Point memory point1) internal view returns (G1Point memory result) {
+    function add(G1Point memory point0, G1Point memory point1)
+        internal
+        view
+        returns (G1Point memory result)
+    {
         /// @solidity memory-safe-assembly
         assembly {
             // G1 addition call expects 256 bytes as an input that is the
-            // byte concatenation of two G1 points (128 bytes each). 
+            // byte concatenation of two G1 points (128 bytes each).
             // Output is an encoding of a single G1 point (128 bytes).
             mstore(add(result, 0x00), mload(add(point0, 0x00)))
             mstore(add(result, 0x20), mload(add(point0, 0x20)))
@@ -150,12 +154,16 @@ library BLS {
         }
     }
 
-    function mul(G1Point memory point, bytes32 scalar) internal view returns (G1Point memory result) {
+    function mul(G1Point memory point, bytes32 scalar)
+        internal
+        view
+        returns (G1Point memory result)
+    {
         /// @solidity memory-safe-assembly
         assembly {
             // G1 multiplication call expects 160 bytes as an input that is the
-            // byte concatenation of encoding of a G1 point (128 bytes) and 
-            // encoding of a scalar value (32 bytes). 
+            // byte concatenation of encoding of a G1 point (128 bytes) and
+            // encoding of a scalar value (32 bytes).
             // Output is an encoding of a single G1 point (128 bytes).
             mstore(add(result, 0x00), mload(add(point, 0x00)))
             mstore(add(result, 0x20), mload(add(point, 0x20)))
@@ -174,7 +182,11 @@ library BLS {
         }
     }
 
-    function msm(G1Point[] memory points, uint256[] memory scalars) internal view returns (G1Point memory result) {
+    function msm(G1Point[] memory points, uint256[] memory scalars)
+        internal
+        view
+        returns (G1Point memory result)
+    {
         /// @solidity memory-safe-assembly
         assembly {
             let k := mload(points)
@@ -182,10 +194,10 @@ library BLS {
                 mstore(0x00, 0x5f776986) // `G1MSMFailed()`.
                 revert(0x1c, 0x04)
             }
-            // G1 MSM call expects `160 * k` (`k` being a positive integer) bytes as an input 
-            // that is the byte concatenation of `k` slices each of them being a 
-            // byte concatenation of encoding of a G1 point (128 bytes) 
-            // and encoding of a scalar value (32 bytes). 
+            // G1 MSM call expects `160 * k` (`k` being a positive integer) bytes as an input
+            // that is the byte concatenation of `k` slices each of them being a
+            // byte concatenation of encoding of a G1 point (128 bytes)
+            // and encoding of a scalar value (32 bytes).
             // Output is an encoding of a single G1 point (128 bytes).
             for { let i := 0 } iszero(eq(i, k)) { i := add(i, 1) } {
                 let o := add(result, mul(0xa0, i))
@@ -208,11 +220,15 @@ library BLS {
         }
     }
 
-    function add(G2Point memory point0, G2Point memory point1) internal view returns (G2Point memory result) {
+    function add(G2Point memory point0, G2Point memory point1)
+        internal
+        view
+        returns (G2Point memory result)
+    {
         /// @solidity memory-safe-assembly
         assembly {
             // G2 addition call expects 512 bytes as an input that is the
-            // byte concatenation of two G2 points (256 bytes each). 
+            // byte concatenation of two G2 points (256 bytes each).
             // Output is an encoding of a single G2 point (256 bytes).
             mstore(add(result, 0x00), mload(add(point0, 0x00)))
             mstore(add(result, 0x20), mload(add(point0, 0x20)))
@@ -242,12 +258,16 @@ library BLS {
         }
     }
 
-    function mul(G2Point memory point, bytes32 scalar) internal view returns (G2Point memory result) {
+    function mul(G2Point memory point, bytes32 scalar)
+        internal
+        view
+        returns (G2Point memory result)
+    {
         /// @solidity memory-safe-assembly
         assembly {
             // G2 multiplication call expects 160 bytes as an input that is the
-            // byte concatenation of encoding of a G2 point (256 bytes) and 
-            // encoding of a scalar value (32 bytes). 
+            // byte concatenation of encoding of a G2 point (256 bytes) and
+            // encoding of a scalar value (32 bytes).
             // Output is an encoding of a single G2 point (256 bytes).
             mstore(add(result, 0x00), mload(add(point, 0x00)))
             mstore(add(result, 0x20), mload(add(point, 0x20)))
@@ -270,7 +290,11 @@ library BLS {
         }
     }
 
-    function msm(G2Point[] memory points, uint256[] memory scalars) internal view returns (G2Point memory result) {
+    function msm(G2Point[] memory points, uint256[] memory scalars)
+        internal
+        view
+        returns (G2Point memory result)
+    {
         /// @solidity memory-safe-assembly
         assembly {
             let k := mload(points)
@@ -278,10 +302,10 @@ library BLS {
                 mstore(0x00, 0xe3dc5425) // `G2MSMFailed()`.
                 revert(0x1c, 0x04)
             }
-            // G2 MSM call expects `288 * k` (`k` being a positive integer) bytes as an input 
-            // that is the byte concatenation of `k` slices each of them being a 
-            // byte concatenation of encoding of a G2 point (256 bytes) 
-            // and encoding of a scalar value (32 bytes). 
+            // G2 MSM call expects `288 * k` (`k` being a positive integer) bytes as an input
+            // that is the byte concatenation of `k` slices each of them being a
+            // byte concatenation of encoding of a G2 point (256 bytes)
+            // and encoding of a scalar value (32 bytes).
             // Output is an encoding of a single G2 point (256 bytes).
             for { let i := 0 } iszero(eq(i, k)) { i := add(i, 1) } {
                 let o := add(result, mul(0x120, i))
@@ -308,7 +332,11 @@ library BLS {
         }
     }
 
-    function pairing(G1Point[] memory g1Points, G2Point[] memory g2Points) internal view returns (bool result) {
+    function pairing(G1Point[] memory g1Points, G2Point[] memory g2Points)
+        internal
+        view
+        returns (bool result)
+    {
         /// @solidity memory-safe-assembly
         assembly {
             let k := mload(g1Points)
