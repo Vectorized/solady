8ec6feb5d24b7f17ec627a8d69a09f4ccb6ada42
Vectorized <webby1111@hotmail.com>
2024-12-18 18:57:00 +0000

    Add extra check for decodeBatch

--- src/accounts/LibERC7579.sol
+++ src/accounts/LibERC7579.sol
@@ -160,6 +160,10 @@ library LibERC7579 {
                 revert(0x1c, 0x04)
             }
             if pointers.length {
+                if iszero(lt(add(pointers.offset, shl(5, pointers.length)), add(e, 0x21))) {
+                    mstore(0x00, 0xba597e7e) // `DecodingError()`.
+                    revert(0x1c, 0x04)
+                }
                 // Perform bounds checks on the decoded `pointers`.
                 // Loop runs out-of-gas if `pointers.length` is big enough to cause overflows.
                 for { let i := pointers.length } 1 {} {
