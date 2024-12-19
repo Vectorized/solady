46abacf98544d197d814d61748bdbdd127ef8d51
Vectorized <webby1111@hotmail.com>
2024-12-19 16:12:07 +0000

    T

--- test/TimedRoles.t.sol
+++ test/TimedRoles.t.sol
@@ -2,14 +2,11 @@
 pragma solidity ^0.8.4;
 
 import {LibSort} from "../src/utils/LibSort.sol";
-import {LibPRNG} from "../src/utils/LibPRNG.sol";
-import {DynamicArrayLib} from "../src/utils/DynamicArrayLib.sol";
-import {EnumerableSetLib} from "../src/utils/EnumerableSetLib.sol";
 import "./utils/SoladyTest.sol";
 import "./utils/mocks/MockTimedRoles.sol";
 
 contract TimedRolesTest is SoladyTest {
-    using DynamicArrayLib for *;
+    event TimedRoleSet(address indexed holder, uint256 indexed timedRole, uint40 start, uint40 end);
 
     MockTimedRoles mockTimedRoles;
 
@@ -26,16 +23,27 @@ contract TimedRolesTest is SoladyTest {
         uint40 end;
     }
 
+    function _sampleTimedRoleConfig() internal returns (TimedRoleConfig memory c) {
+        uint256 m = 0xf00000000000000000000000000000000000000000000000000000000000000f;
+        c.holder = _randomNonZeroAddress();
+        c.role = _randomUniform() & m;
+        (c.start, c.end) = _sampleValidActiveTimeRange();
+    }
+
+    function _hasDuplicateKeys(TimedRoleConfig[] memory a) internal pure returns (bool) {
+        bytes32[] memory hashes = new bytes32[](a.length);
+        for (uint256 i; i != a.length; ++i) {
+            hashes[i] = keccak256(abi.encode(a[i].holder, a[i].role));
+        }
+        LibSort.insertionSort(hashes);
+        LibSort.uniquifySorted(hashes);
+        return hashes.length != a.length;
+    }
+
     function _sampleTimedRoleConfigs() internal returns (TimedRoleConfig[] memory a) {
-        a = new TimedRoleConfig[](_randomUniform() & 7);
-        unchecked {
-            uint256 m = 0xf00000000000000000000000000000000000000000000000000000000000000f;
-            for (uint256 i; i != a.length; ++i) {
-                TimedRoleConfig memory c = a[i];
-                c.holder = _randomUniqueHashedAddress();
-                c.role = _randomUniform() & m;
-                (c.start, c.end) = _sampleValidActiveTimeRange();
-            }
+        a = new TimedRoleConfig[](_randomUniform() & 3);
+        for (uint256 i; i != a.length; ++i) {
+            a[i] = _sampleTimedRoleConfig();
         }
     }
 
@@ -46,84 +54,66 @@ contract TimedRolesTest is SoladyTest {
         } while (end < start);
     }
 
+    function _sampleInvalidActiveTimeRange() internal returns (uint40 start, uint40 end) {
+        do {
+            start = uint40(_random());
+            end = uint40(_random());
+        } while (!(end < start));
+    }
+
     function testSetAndGetRoles(bytes32) public {
         TimedRoleConfig[] memory a = _sampleTimedRoleConfigs();
+
+        uint256 targetTimestamp = _bound(_random(), 0, 2 ** 41 - 1);
+        vm.warp(targetTimestamp);
+
         for (uint256 i; i != a.length; ++i) {
             TimedRoleConfig memory c = a[i];
-            mockTimedRoles.setTimedRoleDirect(c.holder, c.role, c.start, c.end);
+            vm.expectEmit(true, true, true, true);
+            emit TimedRoleSet(c.holder, c.role, c.start, c.end);
+            mockTimedRoles.setTimedRole(c.holder, c.role, c.start, c.end);
+            (bool isActive, uint40 start, uint40 end) =
+                mockTimedRoles.timedRoleActive(c.holder, c.role);
+            assertEq(start, c.start);
+            assertEq(end, c.end);
+            assertEq(isActive, start <= targetTimestamp && targetTimestamp < end);
         }
-    }
-    //     _TestTemps memory t;
-    //     t.holders = _sampleUniqueAddresses(_randomUniform() & 7);
-    //     t.roles = new uint256[][](t.holders.length);
-    //     t.rolesLookup = new uint256[][](t.holders.length);
-    //     unchecked {
-    //         for (uint256 i; i != t.holders.length; ++i) {
-    //             uint256[] memory roles = _sampleRoles(_randomUniform() & 7);
-    //             t.roles[i] = roles;
-    //             roles = LibSort.copy(roles);
-    //             LibSort.insertionSort(roles);
-    //             LibSort.uniquifySorted(roles);
-    //             t.rolesLookup[i] = roles;
-    //             t.combinedRoles = LibSort.union(roles, t.combinedRoles);
-    //         }
-
-    //         for (uint256 i; i != t.holders.length; ++i) {
-    //             uint256[] memory roles = t.roles[i];
-    //             for (uint256 j; j != roles.length; ++j) {
-    //                 mockEnumerableRoles.setRoleDirect(t.holders[i], roles[j], true);
-    //             }
-    //         }
-
-    //         if (_randomChance(2)) {
-    //             for (uint256 i; i < t.combinedRoles.length; ++i) {
-    //                 if (!_randomChance(8)) continue;
-    //                 uint256 role = t.combinedRoles.get(i);
-    //                 DynamicArrayLib.DynamicArray memory expected;
-    //                 for (uint256 j; j != t.holders.length; ++j) {
-    //                     if (LibSort.inSorted(t.rolesLookup[j], role)) {
-    //                         expected.p(t.holders[j]);
-    //                     }
-    //                 }
-    //                 LibSort.insertionSort(expected.data);
-    //                 assertEq(abi.encode(expected.data), abi.encode(_sortedRoleHolders(role)));
-    //             }
-    //         }
-
-    //         if (_randomChance(2)) {
-    //             for (uint256 i; i != t.holders.length; ++i) {
-    //                 uint256[] memory roles = t.roles[i];
-    //                 for (uint256 j; j != roles.length; ++j) {
-    //                     mockEnumerableRoles.setRoleDirect(t.holders[i], roles[j], false);
-    //                 }
-    //             }
-
-    //             for (uint256 i; i < t.combinedRoles.length; ++i) {
-    //                 uint256 role = t.combinedRoles.get(i);
-    //                 assertEq(mockEnumerableRoles.roleHolders(role).length, 0);
-    //                 assertEq(mockEnumerableRoles.roleHolderCount(role), 0);
-    //             }
-    //         }
-    //     }
-    // }
-
-    function _sampleUniqueAddresses(uint256 n) internal returns (address[] memory) {
-        unchecked {
-            uint256[] memory a = DynamicArrayLib.malloc(n);
-            for (uint256 i; i != n; ++i) {
-                a.set(i, _randomUniqueHashedAddress());
+        if (!_hasDuplicateKeys(a)) {
+            for (uint256 i; i != a.length; ++i) {
+                TimedRoleConfig memory c = a[i];
+                (bool isActive, uint40 start, uint40 end) =
+                    mockTimedRoles.timedRoleActive(c.holder, c.role);
+                assertEq(start, c.start);
+                assertEq(end, c.end);
+                assertEq(isActive, start <= targetTimestamp && targetTimestamp < end);
             }
-            return a.asAddressArray();
         }
-    }
-
-    function _sampleRoles(uint256 n) internal returns (uint256[] memory roles) {
-        unchecked {
-            uint256 m = 0xf00000000000000000000000000000000000000000000000000000000000000f;
-            roles = DynamicArrayLib.malloc(n);
-            for (uint256 i; i != n; ++i) {
-                roles.set(i, _randomUniform() & m);
+        if (_randomChance(16)) {
+            TimedRoleConfig memory c = _sampleTimedRoleConfig();
+            (c.start, c.end) = _sampleInvalidActiveTimeRange();
+            vm.expectRevert(TimedRoles.InvalidTimedRoleRange.selector);
+            mockTimedRoles.setTimedRole(c.holder, c.role, c.start, c.end);
+        }
+        if (_randomChance(16)) {
+            TimedRoleConfig memory c = _sampleTimedRoleConfig();
+            mockTimedRoles.setOwner(_randomUniqueHashedAddress());
+            vm.expectRevert(TimedRoles.TimedRolesUnauthorized.selector);
+            mockTimedRoles.setTimedRole(c.holder, c.role, c.start, c.end);
+            mockTimedRoles.setOwner(address(this));
+            if (_randomChance(16)) {
+                c.holder = address(0);
+                vm.expectRevert(TimedRoles.TimedRoleHolderIsZeroAddress.selector);
             }
+            mockTimedRoles.setTimedRole(c.holder, c.role, c.start, c.end);
+        }
+        if (_randomChance(16)) {
+            uint256 maxTimedRole = _random();
+            mockTimedRoles.setMaxTimedRole(maxTimedRole);
+            TimedRoleConfig memory c = _sampleTimedRoleConfig();
+            if (c.role > maxTimedRole) {
+                vm.expectRevert(TimedRoles.InvalidTimedRole.selector);
+            }
+            mockTimedRoles.setTimedRole(c.holder, c.role, c.start, c.end);
         }
     }
 }
