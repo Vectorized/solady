4587788e968aa5c2b539693b36593df9fced3c60
Vectorized <webby1111@hotmail.com>
2025-01-10 17:40:12 +0000

    Edit comments

--- src/utils/ext/zksync/UpgradeableBeacon.sol
+++ src/utils/ext/zksync/UpgradeableBeacon.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-/// @notice A sufficiently minimal upgradeable beacon tailored made for ZKsync.
+/// @notice A sufficiently minimal upgradeable beacon tailor-made for ZKsync.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/ext/zksync/UpgradeableBeacon.sol)
 contract UpgradeableBeacon {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
