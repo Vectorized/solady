a3bf5a414f9081efb8c1b0aee4f5562773d632da
Vectorized <webby1111@hotmail.com>
2025-01-14 19:35:38 +0000

    T

--- src/utils/ext/zksync/SingleUseETHVault.sol
+++ src/utils/ext/zksync/SingleUseETHVault.sol
@@ -31,14 +31,14 @@ contract SingleUseETHVault {
                 mstore(0x00, 0x82b42900) // `Unauthorized()`.
                 revert(0x1c, 0x04)
             }
-            // Initialize `to` to the starting word of the calldata.
             let to := calldataload(0x00)
-            // If the calldatasize is less than 32 bytes, left-pad it to 32 bytes with zeroes.
+            // If the calldata is less than 32 bytes, zero-left-pad it to 32 bytes.
             // Then use the rightmost 20 bytes of the word as the `to` address.
             // This allows for the calldata to be `abi.encode(to)` or `abi.encodePacked(to)`.
             to := shr(mul(lt(calldatasize(), 0x20), shl(3, sub(0x20, calldatasize()))), to)
             // If `to` is `address(0)`, set it to `msg.sender`.
             to := xor(mul(xor(to, caller()), iszero(to)), to)
+            // Transfer all the Ether in this contract to `to`.
             if iszero(call(gas(), to, selfbalance(), 0x00, 0x00, 0x00, 0x00)) {
                 mstore(0x00, 0x651aee10) // `WithdrawAllFailed()`.
                 revert(0x1c, 0x04)
