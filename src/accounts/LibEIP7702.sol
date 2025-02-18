cccbb2b12f978a1212fb633df74de4707253423f
Vectorized <webby1111@hotmail.com>
2025-02-18 20:08:05 +0000

    Use back full bytes32

--- src/accounts/LibEIP7702.sol
+++ src/accounts/LibEIP7702.sol
@@ -27,9 +27,11 @@ library LibEIP7702 {
         0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     /// @dev The transient storage slot for requesting the proxy to initialize the implementation.
-    /// `uint72(bytes9(keccak256("_EIP7702_PROXY_DELEGATION_INITIALIZATION_REQUEST_SLOT")))`.
-    uint256 internal constant EIP7702_PROXY_DELEGATION_INITIALIZATION_REQUEST_SLOT =
-        0x1b537e0b6adf9d6a20;
+    /// `uint256(keccak256("eip7702.proxy.delegation.initialization.request")) - 1`.
+    /// While we would love to use a smaller constant, this slot is used in both the proxy
+    /// and the delegation, so we shall just use bytes32 in case we want to standardize this.
+    bytes32 internal constant EIP7702_PROXY_DELEGATION_INITIALIZATION_REQUEST_SLOT =
+        0x94e11c6e41e7fb92cb8bb65e13fdfbd4eba8b831292a1a220f7915c78c7c078f;
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                    AUTHORITY OPERATIONS                    */
