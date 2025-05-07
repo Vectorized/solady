a6958186569036f895276a13097d0a42f0938e29
Vectorized <webby1111@hotmail.com>
2025-05-07 04:09:24 +0000

    T

--- src/utils/SemVerLib.sol
+++ src/utils/SemVerLib.sol
@@ -25,7 +25,7 @@ library SemVerLib {
                 mstore(0x00, 0)
                 for { _o := i_ } 1 { _o := add(1, _o) } {
                     let c_ := byte(_o, a_)
-                    if and(1, shr(c_, 0x480000000001)) { break } // `0x00`, '.', '+'.
+                    if and(1, shr(c_, 0x480000000001)) { break } // `0x00`, '.', '+'
                     let digit_ := sub(c_, 48)
                     hasNonDigit_ := or(hasNonDigit_, gt(digit_, 9))
                     _r := add(mul(10, _r), digit_)
@@ -34,33 +34,30 @@ library SemVerLib {
                 mstore(shl(5, hasNonDigit_), _r) // Overwrite if it's numeric.
                 _r := mload(0x00)
             }
-            let x, i := mmp(or(eq(byte(0, a), 118), eq(byte(0, a), 86)), a) // 'v', 'V'.
-            let y, j := mmp(or(eq(byte(0, b), 118), eq(byte(0, b), 86)), b) // 'v', 'V'.
+            let x, i := mmp(or(eq(byte(0, a), 118), eq(byte(0, a), 86)), a) // 'v', 'V'
+            let y, j := mmp(or(eq(byte(0, b), 118), eq(byte(0, b), 86)), b) // 'v', 'V'
             result := sub(gt(x, y), lt(x, y))
             for {} lt(result, or(eq(byte(i, a), 46), eq(byte(j, b), 46))) {} {
                 x := 0
                 y := 0
-                if eq(byte(i, a), 46) { x, i := mmp(add(i, 1), a) }
-                if eq(byte(j, b), 46) { y, j := mmp(add(j, 1), b) }
+                if eq(byte(i, a), 46) { x, i := mmp(add(i, 1), a) } // `.`
+                if eq(byte(j, b), 46) { y, j := mmp(add(j, 1), b) } // `.`
                 result := sub(gt(x, y), lt(x, y))
             }
             if iszero(result) {
-                let u := eq(byte(i, a), 45)
-                let v := eq(byte(j, b), 45)
+                let u := eq(byte(i, a), 45) // `-`
+                let v := eq(byte(j, b), 45) // `-`
                 result := sub(lt(u, v), gt(u, v))
-                if lt(result, eq(1, and(u, v))) {
-                    for {} 1 {} {
-                        u, x, i := pre(add(i, 1), a)
-                        v, y, j := pre(add(j, 1), b)
-                        result := sub(gt(u, v), lt(u, v))
-                        if result { break }
-                        result := sub(gt(x, y), lt(x, y))
-                        if result { break }
-                        u := eq(byte(i, a), 46)
-                        v := eq(byte(j, b), 46)
-                        result := sub(gt(u, v), lt(u, v))
-                        if or(result, iszero(u)) { break }
-                    }
+                for {} lt(result, u) {} {
+                    u, x, i := pre(add(i, 1), a)
+                    v, y, j := pre(add(j, 1), b)
+                    result := sub(gt(u, v), lt(u, v))
+                    if result { break }
+                    result := sub(gt(x, y), lt(x, y))
+                    if result { break }
+                    u := eq(byte(i, a), 46) // `.`
+                    v := eq(byte(j, b), 46) // `.`
+                    result := sub(gt(u, v), lt(u, v))
                 }
             }
         }
