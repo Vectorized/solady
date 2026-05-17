9f046e0241b33104111859b447e7b650bffe47dc
atarpara <akpatel0618@gmail.com>
2026-05-17 12:31:08 +0530

    Optimize more

--- src/utils/clz/FixedPointMathLib.sol
+++ src/utils/clz/FixedPointMathLib.sol
@@ -806,14 +806,15 @@ library FixedPointMathLib {
     function cbrt(uint256 x) internal pure returns (uint256 z) {
         /// @solidity memory-safe-assembly
         assembly {
-            // Initial guess z ≈ c · 2^q where b = ⌊log₂(x)⌋, q = ⌊b / 3⌋. The
-            // 8-bit fixed-point multipliers `c`: 140/128, 172/128, and 236/128
+            // Initial guess z ≈ c · 2^q where b = ⌊log₂(x) + 2⌋, q = ⌊b / 3⌋. The
+            // 8-bit fixed-point multipliers `c`: 89/128, 115/128, and 141/128
             // are selected by `b mod 3` to balance each octave's worst-case
-            // final error. This gives >88 bits of precision after only 5
-            // Newton-Raphson iterations. The `or(..., 1)` keeps z ≥ 1 when the
-            // shifted estimate is 0.
-            let b := sub(255, clz(x))
-            z := or(shr(7, shl(div(b, 3), add(121, shl(mod(b, 3), 28)))), 1)
+            // final error. This gives >97 bits of precision after only 5
+            // Newton-Raphson iterations.
+
+            let b := sub(257, clz(x))
+            z := shr(7, shl(div(b, 3), add(89, mul(26, mod(b, 3)))))
+
             // 5 Newton-Raphson iterations
             z := div(add(add(div(x, mul(z, z)), z), z), 3)
             z := div(add(add(div(x, mul(z, z)), z), z), 3)
